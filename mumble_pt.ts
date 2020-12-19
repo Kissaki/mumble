50a7b386a9c6a66aa3690866e248998caa7e0dcb
Jan Klass <kissaki@posteo.de>
2020-12-19 19:19:32 +0100

    New translations PathListWidget.h (Portuguese, Brazilian)

--- mumble_pt.ts
+++ mumble_pt.ts
@@ -3,28 +3,38 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include "SharedMemory.h"
+#ifndef MUMBLE_MUMBLE_PATHLISTWIDGET_H_
+#define MUMBLE_MUMBLE_PATHLISTWIDGET_H_
 
-unsigned int SharedMemory2::uiIndex = 0;
+#include <QListWidget>
 
-QString SharedMemory2::name() const {
-	return a_ucData ? qsName : QString();
-}
+class QGraphicsSceneDragDropEvent;
+class QDropEvent;
 
-unsigned int SharedMemory2::size() const {
-	return a_ucData ? uiSize : 0;
-}
+/**
+ * ListWidget that adds functionality for being able to drop files or folders to add them to the list.
+ *
+ * It makes use of OverlayAppInfo to display file/app information (e.g. the appropriate icon).
+ */
+class PathListWidget : public QListWidget {
+public:
+	enum PathType { FILE_EXE, FOLDER };
 
-unsigned char *SharedMemory2::data() {
-	return a_ucData;
-}
+	PathListWidget(QWidget *parent = 0);
+	void setPathType(PathType type);
+	virtual void dragEnterEvent(QDragEnterEvent *event) Q_DECL_OVERRIDE;
+	virtual void dragMoveEvent(QDragMoveEvent *event) Q_DECL_OVERRIDE;
+	virtual void dropEvent(QDropEvent *event) Q_DECL_OVERRIDE;
 
-const unsigned char *SharedMemory2::data() const {
-	return a_ucData;
-}
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(PathListWidget)
 
-void SharedMemory2::erase() {
-	if (a_ucData) {
-		memset(a_ucData, 0, uiSize);
-	}
-}
+	PathType pathType;
+
+	void addFilePath(const QString &path);
+	void addFolderPath(const QString &path);
+	void checkAcceptDragEvent(QDropEvent *event, bool store);
+};
+
+#endif
