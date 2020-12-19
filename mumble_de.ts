cf91cc2cac63b30ce5ec51d2b74fb9a8280e6ab9
Jan Klass <kissaki@posteo.de>
2020-12-19 18:13:30 +0100

    New translations TextToSpeech_macx.mm (German)

--- mumble_de.ts
+++ mumble_de.ts
@@ -3,70 +3,166 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include "Usage.h"
+/* Copyright (C) 2005-2011, Thorvald Natvig <thorvald@natvig.com>
+   Copyright (C) 2007, Sebastian Schlingmann <mit_service@users.sourceforge.net>
+   Copyright (C) 2008-2011, Mikkel Krautz <mikkel@krautz.dk>
+   Copyright (C) 2014, Mayur Pawashe <zorgiepoo@gmail.com>
 
-#include "ClientUser.h"
-#include "LCD.h"
-#include "NetworkConfig.h"
-#include "OSInfo.h"
-#include "Version.h"
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+   - Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+   - Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+   - Neither the name of the Mumble Developers nor the names of its
+     contributors may be used to endorse or promote products derived from this
+     software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#import <AppKit/AppKit.h>
+
 #include "Global.h"
+#include "TextToSpeech.h"
 
-#include <QtCore/QTimer>
-#include <QtNetwork/QHostAddress>
-#include <QtNetwork/QNetworkAccessManager>
-#include <QtNetwork/QNetworkReply>
-#include <QtXml/QDomElement>
+@interface MUSpeechSynthesizerPrivateHelper : NSObject {
+	NSMutableArray *m_messages;
+	NSSpeechSynthesizer *m_synthesizer;
+}
+- (NSSpeechSynthesizer *)synthesizer;
+- (void)appendMessage:(NSString *)message;
+- (void)processSpeech;
+@end
 
-Usage::Usage(QObject *p) : QObject(p) {
-	qbReport.open(QBuffer::ReadWrite);
-	qdsReport.setDevice(&qbReport);
-	qdsReport.setVersion(QDataStream::Qt_4_4);
-	qdsReport << static_cast< unsigned int >(2);
+#if !defined(USE_MAC_UNIVERSAL) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6
+@interface MUSpeechSynthesizerPrivateHelper () <NSSpeechSynthesizerDelegate>
+@end
+#endif
 
-	// Wait 10 minutes (so we know they're actually using this), then...
-	QTimer::singleShot(60 * 10 * 1000, this, SLOT(registerUsage()));
+@implementation MUSpeechSynthesizerPrivateHelper
+
+- (id)init {
+	if ((self = [super init])) {
+		m_synthesizer = [[NSSpeechSynthesizer alloc] initWithVoice:nil];
+		m_messages = [[NSMutableArray alloc] init];
+		[m_synthesizer setDelegate:self];
+	}
+	return self;
 }
 
-void Usage::registerUsage() {
-	if (!g.s.bUsage
-		|| g.s.uiUpdateCounter == 0) // Only register usage if allowed by the user and first wizard run has finished
+- (void)dealloc {
+	[m_synthesizer release];
+	[m_messages release];
+	[super dealloc];
+}
+
+- (NSSpeechSynthesizer *)synthesizer {
+	return m_synthesizer;
+}
+
+- (void)appendMessage:(NSString *)message {
+	[m_messages insertObject:message atIndex:0];
+}
+
+- (void)processSpeech {
+	Q_ASSERT([m_messages count] == 0);
+	
+	NSString *poppedMessage = [m_messages lastObject];
+	[m_synthesizer startSpeakingString:poppedMessage];
+	[m_messages removeLastObject];
+}
+
+- (void)speechSynthesizer:(NSSpeechSynthesizer *)synthesizer didFinishSpeaking:(BOOL)success {
+	Q_UNUSED(synthesizer);
+	Q_UNUSED(success);
+
+	if ([m_messages count] != 0) {
+		[self processSpeech];
+	}
+}
+
+@end
+
+class TextToSpeechPrivate {
+	public:
+		MUSpeechSynthesizerPrivateHelper *m_synthesizerHelper;
+
+		TextToSpeechPrivate();
+		~TextToSpeechPrivate();
+		void say(const QString &text);
+		void setVolume(int v);
+};
+
+TextToSpeechPrivate::TextToSpeechPrivate() {
+	m_synthesizerHelper = [[MUSpeechSynthesizerPrivateHelper alloc] init];
+}
+
+TextToSpeechPrivate::~TextToSpeechPrivate() {
+	[m_synthesizerHelper release];
+}
+
+void TextToSpeechPrivate::say(const QString &text) {
+	QByteArray byteArray = text.toUtf8();
+	NSString *message = [[NSString alloc] initWithBytes:byteArray.constData() length:byteArray.size() encoding:NSUTF8StringEncoding];
+
+	if (message == nil) {
 		return;
+	}
 
-	QDomDocument doc;
-	QDomElement root = doc.createElement(QLatin1String("usage"));
-	doc.appendChild(root);
+	[m_synthesizerHelper appendMessage:message];
+	[message release];
 
-	QDomElement tag;
-	QDomText t;
+	if (![[m_synthesizerHelper synthesizer] isSpeaking]) {
+		[m_synthesizerHelper processSpeech];
+	}
+}
 
-	OSInfo::fillXml(doc, root);
+void TextToSpeechPrivate::setVolume(int volume) {
+	// Check for setVolume: availability. It's only available on 10.5+.
+	if ([[m_synthesizerHelper synthesizer] respondsToSelector:@selector(setVolume:)]) {
+		[[m_synthesizerHelper synthesizer] setVolume:volume / 100.0];
+	}
+}
 
-	tag = doc.createElement(QLatin1String("in"));
-	root.appendChild(tag);
-	t = doc.createTextNode(g.s.qsAudioInput);
-	tag.appendChild(t);
+TextToSpeech::TextToSpeech(QObject *) {
+	enabled = true;
+	d = new TextToSpeechPrivate();
+}
 
-	tag = doc.createElement(QLatin1String("out"));
-	root.appendChild(tag);
-	t = doc.createTextNode(g.s.qsAudioOutput);
-	tag.appendChild(t);
+TextToSpeech::~TextToSpeech() {
+	delete d;
+}
 
-	tag = doc.createElement(QLatin1String("lcd"));
-	root.appendChild(tag);
-	t = doc.createTextNode(QString::number(g.lcd->hasDevices() ? 1 : 0));
-	tag.appendChild(t);
+void TextToSpeech::say(const QString &text) {
+	if (d && enabled)
+		d->say(text);
+}
 
-	QBuffer *qb = new QBuffer();
-	qb->setData(doc.toString().toUtf8());
-	qb->open(QIODevice::ReadOnly);
+void TextToSpeech::setEnabled(bool e) {
+	enabled = e;
+}
 
-	QNetworkRequest req(QUrl(QLatin1String("https://usage-report.mumble.info/v1/report")));
-	Network::prepareRequest(req);
-	req.setHeader(QNetworkRequest::ContentTypeHeader, QLatin1String("text/xml"));
+void TextToSpeech::setVolume(int volume) {
+	if (d && enabled)
+		d->setVolume(volume);
+}
 
-	QNetworkReply *rep = g.nam->post(req, qb);
-	qb->setParent(rep);
-
-	connect(rep, SIGNAL(finished()), rep, SLOT(deleteLater()));
+bool TextToSpeech::isEnabled() const {
+	return enabled;
 }
