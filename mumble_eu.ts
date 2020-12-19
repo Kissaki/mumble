8957564cd6f3d34dfbfc57c1b5c5d5e629dd7bf3
Jan Klass <kissaki@posteo.de>
2020-12-19 18:32:39 +0100

    New translations OverlayPositionableItem.h (Basque)

--- mumble_eu.ts
+++ mumble_eu.ts
@@ -3,123 +3,31 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_OVERLAY_H_
-#define MUMBLE_MUMBLE_OVERLAY_H_
+#ifndef MUMBLE_MUMBLE_OVERLAYPOSITIONABLEITEM_H
+#define MUMBLE_MUMBLE_OVERLAYPOSITIONABLEITEM_H
 
-#include <QtCore/QUrl>
 #include <QtCore/QtGlobal>
-#include <QtNetwork/QLocalSocket>
 #include <QtWidgets/QGraphicsItem>
 
-#include "../../overlay/overlay.h"
-#include "ConfigDialog.h"
-#include "OverlayText.h"
-
-#include <atomic>
-
-#include "ui_OverlayEditor.h"
-
-#ifdef Q_OS_MAC
-// The function is defined in Overlay_macx.mm because it uses Apple's Objective-C API.
-pid_t getForegroundProcessId();
-#endif
-
-class ClientUser;
-class Overlay;
-class QLocalServer;
-class OverlayClient;
-class OverlayConfig;
-
-struct OverlayAppInfo {
-	static QString applicationIdentifierForPath(const QString &path);
-	static OverlayAppInfo applicationInfoForId(const QString &identifier);
-
-	QString qsDisplayName;
-	QIcon qiIcon;
-
-private:
-	OverlayAppInfo(QString name, QIcon icon = QIcon());
-};
-
-class OverlayGroup : public QGraphicsItem {
-private:
-	Q_DISABLE_COPY(OverlayGroup);
-
-public:
-	enum { Type = UserType + 5 };
-
-public:
-	OverlayGroup();
-
-	QRectF boundingRect() const Q_DECL_OVERRIDE;
-	void paint(QPainter *, const QStyleOptionGraphicsItem *, QWidget *) Q_DECL_OVERRIDE;
-	int type() const Q_DECL_OVERRIDE;
-
-	template< typename T > QRectF boundingRect() const;
-};
-
-class OverlayMouse : public QGraphicsPixmapItem {
-private:
-	Q_DISABLE_COPY(OverlayMouse)
-public:
-	bool contains(const QPointF &) const Q_DECL_OVERRIDE;
-	bool collidesWithPath(const QPainterPath &, Qt::ItemSelectionMode = Qt::IntersectsItemShape) const Q_DECL_OVERRIDE;
-	OverlayMouse(QGraphicsItem * = nullptr);
-};
-
-class OverlayPrivate : public QObject {
-private:
+class OverlayPositionableItem : public QObject, public QGraphicsPixmapItem {
 	Q_OBJECT
-	Q_DISABLE_COPY(OverlayPrivate)
-public:
-	OverlayPrivate(QObject *p) : QObject(p){};
-};
+	Q_DISABLE_COPY(OverlayPositionableItem);
 
-class Overlay : public QObject {
-	friend class OverlayConfig;
-	friend class OverlayClient;
-	friend class OverlayUser;
+public:
+	OverlayPositionableItem(QRectF *posPtr, const bool isPositionable = false);
+	virtual ~OverlayPositionableItem();
+	void updateRender();
+	void setItemVisible(const bool &visible);
 
 private:
-	Q_OBJECT
-	Q_DISABLE_COPY(Overlay)
-protected:
-	OverlayPrivate *d;
-
-	QSet< unsigned int > qsQueried;
-	QSet< unsigned int > qsQuery;
-	/// A flag indicating if the platformInit has been called already
-	std::atomic< bool > m_initialized;
-
-	/// Initializes the overlay, if it hasn't been initialized yet
-	void initialize();
-
-	// These 2 functions (among others) are implemented by the system-specific backend
-	void platformInit();
-	void setActiveInternal(bool act);
-
-	void createPipe();
-
-	QMap< QString, QString > qmOverlayHash;
-	QLocalServer *qlsServer;
-	QList< OverlayClient * > qlClients;
-protected slots:
-	void disconnected();
-	void error(QLocalSocket::LocalSocketError);
-	void newConnection();
-
-public:
-	Overlay();
-	~Overlay() Q_DECL_OVERRIDE;
-	bool isActive() const;
-	void verifyTexture(ClientUser *cp, bool allowupdate = true);
-	void requestTexture(ClientUser *);
-
-public slots:
-	void updateOverlay();
-	void setActive(bool act);
-	void toggleShow();
-	void forceSettings();
+	/// Float value between 0 and 1 where 0,0 is top left, and 1,1 is bottom right
+	QRectF *m_position;
+	const bool m_isPositionEditable;
+	QGraphicsEllipseItem *m_qgeiHandle;
+	void createPositioningHandle();
+	bool sceneEventFilter(QGraphicsItem *, QEvent *) Q_DECL_OVERRIDE;
+private slots:
+	void onMove();
 };
 
 #endif
