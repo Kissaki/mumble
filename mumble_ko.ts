ceb93ce718c3d6773cf55ec679a65612590fee7a
Jan Klass <kissaki@posteo.de>
2020-12-19 18:41:21 +0100

    New translations SvgIcon.h (Korean)

--- mumble_ko.ts
+++ mumble_ko.ts
@@ -3,52 +3,23 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_SOCKETRPC_H_
-#define MUMBLE_MUMBLE_SOCKETRPC_H_
+#ifndef MUMBLE_MUMBLE_SVGICON_H
+#define MUMBLE_MUMBLE_SVGICON_H
 
-#include <QtCore/QObject>
-#include <QtCore/QString>
-#include <QtCore/QVariant>
-#include <QtCore/QXmlStreamReader>
-#include <QtNetwork/QLocalSocket>
+#include <QIcon>
+#include <QString>
 
-class QBuffer;
-class QLocalServer;
-
-class SocketRPCClient : public QObject {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(SocketRPCClient)
-protected:
-	QLocalSocket *qlsSocket;
-	QXmlStreamReader qxsrReader;
-	QXmlStreamWriter qxswWriter;
-	QBuffer *qbBuffer;
-	QByteArray qbaOutput;
-
-	void processXml();
-
+class SvgIcon {
 public:
-	SocketRPCClient(QLocalSocket *s, QObject *p = nullptr);
-public slots:
-	void disconnected();
-	void error(QLocalSocket::LocalSocketError);
-	void readyRead();
-};
-
-class SocketRPC : public QObject {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(SocketRPC)
-protected:
-	QLocalServer *qlsServer;
-
-public:
-	typedef QMap< QString, QVariant > ParameterMap;
-	SocketRPC(const QString &basename, QObject *p = nullptr);
-	static bool send(const QString &basename, const QString &request, const ParameterMap &param = ParameterMap());
-public slots:
-	void newConnection();
+	/// addSvgPixmapsToIcon renders the SVG file at |fn| in various
+	/// common sizes from 8x8 up to 256x256 and adds the resulting
+	/// rasterized pixmaps to |icon|.
+	///
+	/// This method is only needed due to an odd interaction between
+	/// Qt and Plasma/KStatusNotifierItem. See
+	/// https://github.com/mumble-voip/mumble/issues/3374
+	/// for more information.
+	static void addSvgPixmapsToIcon(QIcon &icon, QString fn);
 };
 
 #endif
