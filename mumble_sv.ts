2ce1fa78faa3f2d2fea6864bdb3215110d62f075
Jan Klass <kissaki@posteo.de>
2020-12-19 19:02:49 +0100

    New translations VersionCheck.h (Swedish)

--- mumble_sv.ts
+++ mumble_sv.ts
@@ -3,30 +3,28 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_VIEWCERT_H_
-#define MUMBLE_MUMBLE_VIEWCERT_H_
+#ifndef MUMBLE_MUMBLE_VERSIONCHECK_H_
+#define MUMBLE_MUMBLE_VERSIONCHECK_H_
 
-#include <QtCore/QList>
+#include <QFutureWatcher>
+#include <QtCore/QByteArray>
 #include <QtCore/QObject>
-#include <QtCore/QtGlobal>
-#include <QtNetwork/QSslCertificate>
-#include <QtWidgets/QDialog>
+#include <QtCore/QUrl>
 
-class QListWidget;
-
-class ViewCert : public QDialog {
+class VersionCheck : public QObject {
 private:
 	Q_OBJECT
-	Q_DISABLE_COPY(ViewCert)
-protected:
-	QList< QSslCertificate > qlCerts;
-	QListWidget *qlwChain, *qlwCert;
+	Q_DISABLE_COPY(VersionCheck)
+
+	QFutureWatcher< void > m_preparationWatcher;
+	QUrl m_requestURL;
 protected slots:
-	void on_Chain_currentRowChanged(int);
+	void performRequest();
+public slots:
+	void fetched(QByteArray data, QUrl url);
 
 public:
-	ViewCert(QList< QSslCertificate > c, QWidget *p);
-	static QString prettifyDigest(QString);
+	VersionCheck(bool autocheck, QObject *parent = nullptr, bool focus = false);
 };
 
 #endif
