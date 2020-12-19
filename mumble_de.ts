a006c6faa1ff726d959ab55c7c1aaea3242ea9c7
Jan Klass <kissaki@posteo.de>
2020-12-19 18:05:39 +0100

    New translations SharedMemory.h (German)

--- mumble_de.ts
+++ mumble_de.ts
@@ -1,134 +1,37 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>PluginConfig</class>
- <widget class="QWidget" name="PluginConfig">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>321</width>
-    <height>235</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>Plugins</string>
-  </property>
-  <layout class="QVBoxLayout">
-   <item>
-    <widget class="QGroupBox" name="qgbOptions">
-     <property name="title">
-      <string>Options</string>
-     </property>
-     <layout class="QVBoxLayout">
-      <item>
-       <widget class="QCheckBox" name="qcbTransmit">
-        <property name="toolTip">
-         <string>Enable plugins and transmit positional information</string>
-        </property>
-        <property name="whatsThis">
-         <string>This allows plugins for supported games to fetch your in-game position and transmit it with each voice packet. This enables other users to hear your voice in-game from the direction your character is in relation to their own.</string>
-        </property>
-        <property name="text">
-         <string>Link to Game and Transmit Position</string>
-        </property>
-       </widget>
-      </item>
-     </layout>
-    </widget>
-   </item>
-   <item>
-    <widget class="QGroupBox" name="qgbPlugins">
-     <property name="title">
-      <string>Plugins</string>
-     </property>
-     <layout class="QVBoxLayout">
-      <item>
-       <widget class="QTreeWidget" name="qtwPlugins">
-        <property name="rootIsDecorated">
-         <bool>false</bool>
-        </property>
-        <property name="headerHidden">
-         <bool>false</bool>
-        </property>
-        <attribute name="headerStretchLastSection">
-         <bool>false</bool>
-        </attribute>
-        <attribute name="headerStretchLastSection">
-         <bool>false</bool>
-        </attribute>
-        <column>
-         <property name="text">
-          <string>Name</string>
-         </property>
-        </column>
-        <column>
-         <property name="text">
-          <string>Enabled</string>
-         </property>
-        </column>
-       </widget>
-      </item>
-      <item>
-       <layout class="QHBoxLayout">
-        <item>
-         <widget class="QPushButton" name="qpbReload">
-          <property name="toolTip">
-           <string>Reloads all plugins</string>
-          </property>
-          <property name="whatsThis">
-           <string>This rescans and reloads plugins. Use this if you just added or changed a plugin to the plugins directory.</string>
-          </property>
-          <property name="text">
-           <string>&amp;Reload plugins</string>
-          </property>
-         </widget>
-        </item>
-        <item>
-         <spacer>
-          <property name="orientation">
-           <enum>Qt::Horizontal</enum>
-          </property>
-          <property name="sizeHint" stdset="0">
-           <size>
-            <width>40</width>
-            <height>20</height>
-           </size>
-          </property>
-         </spacer>
-        </item>
-        <item>
-         <widget class="QPushButton" name="qpbAbout">
-          <property name="toolTip">
-           <string>Information about plugin</string>
-          </property>
-          <property name="whatsThis">
-           <string>This shows a small information message about the plugin.</string>
-          </property>
-          <property name="text">
-           <string>&amp;About</string>
-          </property>
-         </widget>
-        </item>
-        <item>
-         <widget class="QPushButton" name="qpbConfig">
-          <property name="toolTip">
-           <string>Show configuration page of plugin</string>
-          </property>
-          <property name="whatsThis">
-           <string>This shows the configuration page of the plugin, if any.</string>
-          </property>
-          <property name="text">
-           <string>&amp;Configure</string>
-          </property>
-         </widget>
-        </item>
-       </layout>
-      </item>
-     </layout>
-    </widget>
-   </item>
-  </layout>
- </widget>
- <resources/>
- <connections/>
-</ui>
+// Copyright 2005-2020 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#ifndef MUMBLE_MUMBLE_SHAREDMEMORY_H_
+#define MUMBLE_MUMBLE_SHAREDMEMORY_H_
+
+#include <QtCore/QObject>
+#include <QtCore/QString>
+
+struct SharedMemory2Private;
+class SharedMemory2 : QObject {
+private:
+	Q_DISABLE_COPY(SharedMemory2)
+	Q_OBJECT
+protected:
+	QString qsName;
+	SharedMemory2Private *d;
+	unsigned char *a_ucData;
+	unsigned int uiSize;
+	static unsigned int uiIndex;
+
+public:
+	SharedMemory2(QObject *p, unsigned int minsize, const QString &name = QString());
+	~SharedMemory2() Q_DECL_OVERRIDE;
+
+	void erase();
+	void systemRelease();
+
+	QString name() const;
+	unsigned int size() const;
+	unsigned char *data();
+	const unsigned char *data() const;
+};
+
+#endif
