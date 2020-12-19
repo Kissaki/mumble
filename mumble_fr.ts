9ad7e85de1f7c29381396f929d92789614d3bb0f
Jan Klass <kissaki@posteo.de>
2020-12-19 17:58:20 +0100

    New translations OverlayUser.h (French)

--- mumble_fr.ts
+++ mumble_fr.ts
@@ -3,47 +3,47 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_OVERLAYUSERGROUP_H_
-#define MUMBLE_MUMBLE_OVERLAYUSERGROUP_H_
+#ifndef MUMBLE_MUMBLE_OVERLAYUSER_H_
+#define MUMBLE_MUMBLE_OVERLAYUSER_H_
+
+#include <QtCore/QtGlobal>
 
 #include "Overlay.h"
 
-class OverlayUser;
-
-class OverlayUserGroup : public QObject, public OverlayGroup {
+class OverlayUser : public OverlayGroup {
 private:
-	Q_OBJECT
-	Q_DISABLE_COPY(OverlayUserGroup);
-
+	Q_DISABLE_COPY(OverlayUser)
 public:
-	enum { Type = UserType + 3 };
+	enum { Type = UserType + 1 };
 
 protected:
+	QGraphicsPixmapItem *qgpiMuted, *qgpiDeafened;
+	QGraphicsPixmapItem *qgpiAvatar;
+	QGraphicsPixmapItem *qgpiName[4];
+	QGraphicsPixmapItem *qgpiChannel;
+	QGraphicsPathItem *qgpiBox;
+
 	OverlaySettings *os;
 
-	QMap< QObject *, OverlayUser * > qmUsers;
-	QList< OverlayUser * > qlExampleUsers;
+	unsigned int uiSize;
+	ClientUser *cuUser;
+	Settings::TalkState tsColor;
 
-	QGraphicsEllipseItem *qgeiHandle;
+	QString qsName;
+	QString qsChannelName;
+	QByteArray qbaAvatar;
 
-	void contextMenuEvent(QGraphicsSceneContextMenuEvent *e) Q_DECL_OVERRIDE;
-	void wheelEvent(QGraphicsSceneWheelEvent *e) Q_DECL_OVERRIDE;
-	bool sceneEventFilter(QGraphicsItem *, QEvent *e) Q_DECL_OVERRIDE;
-protected slots:
-	void userDestroyed(QObject *);
-	void moveUsers();
+	void setup();
 
 public:
-	bool bShowExamples;
-
-	OverlayUserGroup(OverlaySettings *);
-	~OverlayUserGroup() Q_DECL_OVERRIDE;
+	OverlayUser(ClientUser *cu, unsigned int uiSize, OverlaySettings *osptr);
+	OverlayUser(Settings::TalkState ts, unsigned int uiSize, OverlaySettings *osptr);
+	void updateUser();
+	void updateLayout();
 
 	int type() const Q_DECL_OVERRIDE;
-public slots:
-	void reset();
-	void updateUsers();
-	void updateLayout();
+	static QRectF scaledRect(const QRectF &qr, qreal scale);
+	static QPointF alignedPosition(const QRectF &box, const QRectF &item, Qt::Alignment a);
 };
 
 #endif
