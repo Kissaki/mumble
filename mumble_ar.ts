9d91231d7de5dd62ae7dec0d4e2d61fd2ec01f48
Jan Klass <kissaki@posteo.de>
2020-12-19 18:15:51 +0100

    New translations ConfigDialog.ui (Arabic)

--- mumble_ar.ts
+++ mumble_ar.ts
@@ -1,50 +1,128 @@
-// Copyright 2005-2020 The Mumble Developers. All rights reserved.
-// Use of this source code is governed by a BSD-style license
-// that can be found in the LICENSE file at the root of the
-// Mumble source tree or at <https://www.mumble.info/LICENSE>.
-
-#ifndef MUMBLE_MUMBLE_CONFIGDIALOG_H_
-#define MUMBLE_MUMBLE_CONFIGDIALOG_H_
-
-#include "ConfigWidget.h"
-#include "Settings.h"
-
-#include "ui_ConfigDialog.h"
-
-#include <QtCore/QMutex>
-
-class ConfigDialog : public QDialog, public Ui::ConfigDialog {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(ConfigDialog)
-protected:
-	static QMutex s_existingWidgetsMutex;
-	static QHash< QString, ConfigWidget * > s_existingWidgets;
-	QHash< ConfigWidget *, QWidget * > qhPages;
-	QMap< unsigned int, ConfigWidget * > qmWidgets;
-	QMap< QListWidgetItem *, ConfigWidget * > qmIconWidgets;
-	void updateListView();
-	void addPage(ConfigWidget *aw, unsigned int idx);
-	Settings s;
-
-public:
-	ConfigDialog(QWidget *p = nullptr);
-	~ConfigDialog() Q_DECL_OVERRIDE;
-
-	/// @returns The pointer to the existing ConfigWidget with the given name or nullptr,
-	/// 	if no such widget exists.
-	static ConfigWidget *getConfigWidget(const QString &name);
-
-signals:
-	/// Emitted whenever the settings dialog has been accepted. For potential slots this
-	/// means that the settings potentially have changed.
-	void settingsAccepted();
-public slots:
-	void on_pageButtonBox_clicked(QAbstractButton *);
-	void on_dialogButtonBox_clicked(QAbstractButton *);
-	void on_qlwIcons_currentItemChanged(QListWidgetItem *current, QListWidgetItem *previous);
-	void apply();
-	void accept() Q_DECL_OVERRIDE;
-};
-
-#endif
+<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>ConfigDialog</class>
+ <widget class="QDialog" name="ConfigDialog">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>709</width>
+    <height>505</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>Mumble Configuration</string>
+  </property>
+  <property name="sizeGripEnabled">
+   <bool>true</bool>
+  </property>
+  <layout class="QGridLayout" name="gridLayout">
+   <item row="0" column="0">
+    <widget class="QListWidget" name="qlwIcons">
+     <property name="sizePolicy">
+      <sizepolicy hsizetype="Minimum" vsizetype="Expanding">
+       <horstretch>0</horstretch>
+       <verstretch>0</verstretch>
+      </sizepolicy>
+     </property>
+     <property name="iconSize">
+      <size>
+       <width>24</width>
+       <height>24</height>
+      </size>
+     </property>
+     <property name="resizeMode">
+      <enum>QListView::Adjust</enum>
+     </property>
+     <property name="layoutMode">
+      <enum>QListView::Batched</enum>
+     </property>
+     <property name="uniformItemSizes">
+      <bool>true</bool>
+     </property>
+    </widget>
+   </item>
+   <item row="1" column="0" colspan="3">
+    <layout class="QHBoxLayout" name="horizontalLayout">
+     <item>
+      <widget class="QDialogButtonBox" name="pageButtonBox">
+       <property name="orientation">
+        <enum>Qt::Horizontal</enum>
+       </property>
+      </widget>
+     </item>
+     <item>
+      <spacer>
+       <property name="orientation">
+        <enum>Qt::Horizontal</enum>
+       </property>
+       <property name="sizeHint" stdset="0">
+        <size>
+         <width>474</width>
+         <height>22</height>
+        </size>
+       </property>
+      </spacer>
+     </item>
+     <item>
+      <widget class="QDialogButtonBox" name="dialogButtonBox">
+       <property name="orientation">
+        <enum>Qt::Horizontal</enum>
+       </property>
+       <property name="standardButtons">
+        <set>QDialogButtonBox::Apply|QDialogButtonBox::Cancel|QDialogButtonBox::Ok</set>
+       </property>
+      </widget>
+     </item>
+    </layout>
+   </item>
+   <item row="0" column="1" rowspan="1">
+    <widget class="QStackedWidget" name="qswPages">
+     <property name="sizePolicy">
+      <sizepolicy hsizetype="Expanding" vsizetype="Expanding">
+       <horstretch>1</horstretch>
+       <verstretch>0</verstretch>
+      </sizepolicy>
+     </property>
+    </widget>
+   </item>
+  </layout>
+ </widget>
+ <resources>
+  <include location="mumble.qrc"/>
+ </resources>
+ <connections>
+  <connection>
+   <sender>dialogButtonBox</sender>
+   <signal>accepted()</signal>
+   <receiver>ConfigDialog</receiver>
+   <slot>accept()</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>167</x>
+     <y>344</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>191</x>
+     <y>319</y>
+    </hint>
+   </hints>
+  </connection>
+  <connection>
+   <sender>dialogButtonBox</sender>
+   <signal>rejected()</signal>
+   <receiver>ConfigDialog</receiver>
+   <slot>reject()</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>260</x>
+     <y>356</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>284</x>
+     <y>319</y>
+    </hint>
+   </hints>
+  </connection>
+ </connections>
+</ui>
