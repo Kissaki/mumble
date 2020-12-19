8773ec2a05501a3a37802efa6051c6f3f6e894b2
Jan Klass <kissaki@posteo.de>
2020-12-19 19:09:01 +0100

    New translations PAAudio.h (Polish)

--- mumble_pl.ts
+++ mumble_pl.ts
@@ -1,134 +1,102 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>PluginConfig</class>
- <widget class="QWidget" name="PluginConfig">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>321</width>
-    <height>235</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>Plugins</string>
-  </property>
-  <layout class="QVBoxLayout">
-   <item>
-    <widget class="QGroupBox" name="qgbOptions">
-     <property name="title">
-      <string>Options</string>
-     </property>
-     <layout class="QVBoxLayout">
-      <item>
-       <widget class="QCheckBox" name="qcbTransmit">
-        <property name="toolTip">
-         <string>Enable plugins and transmit positional information</string>
-        </property>
-        <property name="whatsThis">
-         <string>This allows plugins for supported games to fetch your in-game position and transmit it with each voice packet. This enables other users to hear your voice in-game from the direction your character is in relation to their own.</string>
-        </property>
-        <property name="text">
-         <string>Link to Game and Transmit Position</string>
-        </property>
-       </widget>
-      </item>
-     </layout>
-    </widget>
-   </item>
-   <item>
-    <widget class="QGroupBox" name="qgbPlugins">
-     <property name="title">
-      <string>Plugins</string>
-     </property>
-     <layout class="QVBoxLayout">
-      <item>
-       <widget class="QTreeWidget" name="qtwPlugins">
-        <property name="rootIsDecorated">
-         <bool>false</bool>
-        </property>
-        <property name="headerHidden">
-         <bool>false</bool>
-        </property>
-        <attribute name="headerStretchLastSection">
-         <bool>false</bool>
-        </attribute>
-        <attribute name="headerStretchLastSection">
-         <bool>false</bool>
-        </attribute>
-        <column>
-         <property name="text">
-          <string>Name</string>
-         </property>
-        </column>
-        <column>
-         <property name="text">
-          <string>Enabled</string>
-         </property>
-        </column>
-       </widget>
-      </item>
-      <item>
-       <layout class="QHBoxLayout">
-        <item>
-         <widget class="QPushButton" name="qpbReload">
-          <property name="toolTip">
-           <string>Reloads all plugins</string>
-          </property>
-          <property name="whatsThis">
-           <string>This rescans and reloads plugins. Use this if you just added or changed a plugin to the plugins directory.</string>
-          </property>
-          <property name="text">
-           <string>&amp;Reload plugins</string>
-          </property>
-         </widget>
-        </item>
-        <item>
-         <spacer>
-          <property name="orientation">
-           <enum>Qt::Horizontal</enum>
-          </property>
-          <property name="sizeHint" stdset="0">
-           <size>
-            <width>40</width>
-            <height>20</height>
-           </size>
-          </property>
-         </spacer>
-        </item>
-        <item>
-         <widget class="QPushButton" name="qpbAbout">
-          <property name="toolTip">
-           <string>Information about plugin</string>
-          </property>
-          <property name="whatsThis">
-           <string>This shows a small information message about the plugin.</string>
-          </property>
-          <property name="text">
-           <string>&amp;About</string>
-          </property>
-         </widget>
-        </item>
-        <item>
-         <widget class="QPushButton" name="qpbConfig">
-          <property name="toolTip">
-           <string>Show configuration page of plugin</string>
-          </property>
-          <property name="whatsThis">
-           <string>This shows the configuration page of the plugin, if any.</string>
-          </property>
-          <property name="text">
-           <string>&amp;Configure</string>
-          </property>
-         </widget>
-        </item>
-       </layout>
-      </item>
-     </layout>
-    </widget>
-   </item>
-  </layout>
- </widget>
- <resources/>
- <connections/>
-</ui>
+// Copyright 2005-2020 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#ifndef MUMBLE_MUMBLE_PAAUDIO_H_
+#define MUMBLE_MUMBLE_PAAUDIO_H_
+
+#include "AudioInput.h"
+#include "AudioOutput.h"
+
+#include <QtCore/QLibrary>
+#include <QtCore/QWaitCondition>
+
+#include <portaudio.h>
+
+class PortAudioInit;
+
+class PortAudioSystem : public QObject {
+	friend PortAudioInit;
+
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(PortAudioSystem)
+protected:
+	bool bOk;
+	QMutex qmWait;
+	QLibrary qlPortAudio;
+	QWaitCondition qwcWait;
+
+	static int streamCallback(const void *input, void *output, unsigned long frames, const PaStreamCallbackTimeInfo *,
+							  PaStreamCallbackFlags statusFlags, void *isInput);
+
+	const char *(*Pa_GetVersionText)();
+	const char *(*Pa_GetErrorText)(PaError errorCode);
+	PaError (*Pa_Initialize)();
+	PaError (*Pa_Terminate)();
+	PaError (*Pa_OpenStream)(PaStream **stream, const PaStreamParameters *inputParameters,
+							 const PaStreamParameters *outputParameters, double sampleRate,
+							 unsigned long framesPerBuffer, PaStreamFlags streamFlags, PaStreamCallback *streamCallback,
+							 void *userData);
+	PaError (*Pa_CloseStream)(PaStream *stream);
+	PaError (*Pa_StartStream)(PaStream *stream);
+	PaError (*Pa_StopStream)(PaStream *stream);
+	PaError (*Pa_IsStreamActive)(PaStream *stream);
+	PaDeviceIndex (*Pa_GetDefaultInputDevice)();
+	PaDeviceIndex (*Pa_GetDefaultOutputDevice)();
+	PaDeviceIndex (*Pa_HostApiDeviceIndexToDeviceIndex)(PaHostApiIndex hostApi, int hostApiDeviceIndex);
+	PaHostApiIndex (*Pa_GetHostApiCount)();
+	const PaHostApiInfo *(*Pa_GetHostApiInfo)(PaHostApiIndex hostApi);
+	const PaDeviceInfo *(*Pa_GetDeviceInfo)(PaDeviceIndex device);
+
+public:
+	const QList< audioDevice > enumerateDevices(const bool input, const PaDeviceIndex current);
+
+	bool isStreamRunning(PaStream *stream);
+
+	int openStream(PaStream **stream, PaDeviceIndex device, const uint32_t frameSize, const bool isInput);
+	bool closeStream(PaStream *stream);
+
+	bool startStream(PaStream *stream);
+	bool stopStream(PaStream *stream);
+
+	PortAudioSystem();
+	~PortAudioSystem();
+};
+
+
+class PortAudioInput : public AudioInput {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(PortAudioInput)
+protected:
+	QMutex qmWait;
+	QWaitCondition qwcSleep;
+	PaStream *stream;
+
+public:
+	void process(const uint32_t frames, const void *buffer);
+	void run() Q_DECL_OVERRIDE;
+	PortAudioInput();
+	~PortAudioInput() Q_DECL_OVERRIDE;
+};
+
+
+class PortAudioOutput : public AudioOutput {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(PortAudioOutput)
+protected:
+	QMutex qmWait;
+	QWaitCondition qwcSleep;
+	PaStream *stream;
+
+public:
+	void process(const uint32_t frames, void *buffer);
+	void run() Q_DECL_OVERRIDE;
+	PortAudioOutput();
+	~PortAudioOutput() Q_DECL_OVERRIDE;
+};
+
+#endif
