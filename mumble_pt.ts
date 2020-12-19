0d46f7dc9372a585d1648483bfaf09b0ed89816c
Jan Klass <kissaki@posteo.de>
2020-12-19 19:26:39 +0100

    New translations TalkingUISelection.cpp (Portuguese, Brazilian)

--- mumble_pt.ts
+++ mumble_pt.ts
@@ -1,21 +1,108 @@
-<RCC>
-    <qresource prefix="/">
-        <file alias="rec.svg">../../icons/rec.svg</file>
-        <file alias="mumble.svg">../../icons/mumble.svg</file>
-        <file alias="mumble.icns">../../icons/mumble.icns</file>
-        <file alias="wb_male.oga">../../samples/wb_male.oga</file>
-        <file alias="on.ogg">../../samples/on.ogg</file>
-        <file alias="off.ogg">../../samples/off.ogg</file>
-        <file alias="Critical.ogg">../../samples/Critical.ogg</file>
-        <file alias="PermissionDenied.ogg">../../samples/PermissionDenied.ogg</file>
-        <file alias="SelfMutedDeafened.ogg">../../samples/SelfMutedDeafened.ogg</file>
-        <file alias="ServerConnected.ogg">../../samples/ServerConnected.ogg</file>
-        <file alias="ServerDisconnected.ogg">../../samples/ServerDisconnected.ogg</file>
-        <file alias="TextMessage.ogg">../../samples/TextMessage.ogg</file>
-        <file alias="UserJoinedChannel.ogg">../../samples/UserJoinedChannel.ogg</file>
-        <file alias="UserKickedYouOrByYou.ogg">../../samples/UserKickedYouOrByYou.ogg</file>
-        <file alias="UserLeftChannel.ogg">../../samples/UserLeftChannel.ogg</file>
-        <file alias="UserMutedYouOrByYou.ogg">../../samples/UserMutedYouOrByYou.ogg</file>
-        <file alias="RecordingStateChanged.ogg">../../samples/RecordingStateChanged.ogg</file>
-    </qresource>
-</RCC>
+// Copyright 2020 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#include "TalkingUISelection.h"
+#include "MainWindow.h"
+#include "UserModel.h"
+
+#include <QVariant>
+#include <QWidget>
+
+// We define a global macro called 'g'. This can lead to issues when included code uses 'g' as a type or parameter name
+// (like protobuf 3.7 does). As such, for now, we have to make this our last include.
+#include "Global.h"
+
+TalkingUISelection::TalkingUISelection(QWidget *widget) : m_widget(widget) {
+}
+
+
+void TalkingUISelection::setActive(bool active) {
+	if (m_widget) {
+		m_widget->setProperty("selected", active);
+		// Unpolish the widget's style so that the new property can take effect
+		m_widget->style()->unpolish(m_widget);
+	}
+}
+
+void TalkingUISelection::apply() {
+	setActive(true);
+}
+
+void TalkingUISelection::discard() {
+	setActive(false);
+}
+
+bool TalkingUISelection::operator==(const TalkingUISelection &other) const {
+	return m_widget == other.m_widget;
+}
+
+bool TalkingUISelection::operator!=(const TalkingUISelection &other) const {
+	return m_widget != other.m_widget;
+}
+
+bool TalkingUISelection::operator==(const QWidget *widget) const {
+	return m_widget == widget;
+}
+
+bool TalkingUISelection::operator!=(const QWidget *widget) const {
+	return m_widget != widget;
+}
+
+
+UserSelection::UserSelection(QWidget *widget, unsigned int userSession)
+	: TalkingUISelection(widget), m_userSession(userSession) {
+}
+
+void UserSelection::syncToMainWindow() const {
+	if (g.mw && g.mw->pmModel) {
+		g.mw->pmModel->setSelectedUser(m_userSession);
+	}
+}
+
+std::unique_ptr< TalkingUISelection > UserSelection::cloneToHeap() const {
+	return std::make_unique< UserSelection >(*this);
+}
+
+
+
+ChannelSelection::ChannelSelection(QWidget *widget, int channelID)
+	: TalkingUISelection(widget), m_channelID(channelID) {
+}
+
+void ChannelSelection::syncToMainWindow() const {
+	if (g.mw && g.mw->pmModel) {
+		g.mw->pmModel->setSelectedChannel(m_channelID);
+	}
+}
+
+std::unique_ptr< TalkingUISelection > ChannelSelection::cloneToHeap() const {
+	return std::make_unique< ChannelSelection >(*this);
+}
+
+
+
+ListenerSelection::ListenerSelection(QWidget *widget, unsigned int userSession, int channelID)
+	: TalkingUISelection(widget), m_userSession(userSession), m_channelID(channelID) {
+}
+
+void ListenerSelection::syncToMainWindow() const {
+	if (g.mw && g.mw->pmModel) {
+		g.mw->pmModel->setSelectedChannelListener(m_userSession, m_channelID);
+	}
+}
+
+std::unique_ptr< TalkingUISelection > ListenerSelection::cloneToHeap() const {
+	return std::make_unique< ListenerSelection >(*this);
+}
+
+
+
+void EmptySelection::syncToMainWindow() const {
+	// Do nothing
+}
+
+std::unique_ptr< TalkingUISelection > EmptySelection::cloneToHeap() const {
+	return std::make_unique< EmptySelection >(*this);
+}
