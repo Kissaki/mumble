8b06cceca00f59f7e60371439ed4e81c020e35b2
Jan Klass <kissaki@posteo.de>
2020-12-19 19:03:32 +0100

    New translations CrashReporter.h (Swedish)

--- mumble_sv.ts
+++ mumble_sv.ts
@@ -3,90 +3,37 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include "PathListWidget.h"
+#ifndef MUMBLE_MUMBLE_CRASHREPORTER_H_
+#	define MUMBLE_MUMBLE_CRASHREPORTER_H_
 
-#include "Overlay.h"
+#	include <QtCore/QEventLoop>
+#	include <QtCore/QObject>
+#	include <QtNetwork/QNetworkReply>
+#	include <QtWidgets/QDialog>
+#	include <QtWidgets/QLineEdit>
+#	include <QtWidgets/QProgressDialog>
+#	include <QtWidgets/QTextEdit>
 
-#include <QtCore/QDir>
-#include <QtCore/QFile>
-#include <QtCore/QMimeData>
-#include <QtGui/QDragEnterEvent>
-#include <QtGui/QDragMoveEvent>
-#include <QtGui/QDropEvent>
+class CrashReporter : QDialog {
+	Q_OBJECT
+	Q_DISABLE_COPY(CrashReporter)
 
-PathListWidget::PathListWidget(QWidget *parent) : QListWidget(parent), pathType(FILE_EXE) {
-	setAcceptDrops(true);
-}
+public:
+	CrashReporter(QWidget *p = 0);
+	~CrashReporter() Q_DECL_OVERRIDE;
+	void run();
 
-void PathListWidget::setPathType(PathType type) {
-	pathType = type;
-}
+protected:
+	QEventLoop *qelLoop;
+	QProgressDialog *qpdProgress;
+	QNetworkReply *qnrReply;
+	QLineEdit *qleEmail;
+	QTextEdit *qteDescription;
+public slots:
+	void uploadFinished();
+	void uploadProgress(qint64, qint64);
+};
 
-void PathListWidget::addFilePath(const QString &path) {
-	QString qsAppIdentifier = OverlayAppInfo::applicationIdentifierForPath(path);
-	QStringList qslIdentifiers;
-	for (int i = 0; i < count(); i++) {
-		qslIdentifiers << item(i)->data(Qt::UserRole).toString();
-	}
-	if (!qslIdentifiers.contains(qsAppIdentifier)) {
-		OverlayAppInfo oai               = OverlayAppInfo::applicationInfoForId(qsAppIdentifier);
-		QListWidgetItem *qlwiApplication = new QListWidgetItem(oai.qiIcon, oai.qsDisplayName, this);
-		qlwiApplication->setData(Qt::UserRole, QVariant(qsAppIdentifier));
-		setCurrentItem(qlwiApplication);
-	}
-}
-
-void PathListWidget::addFolderPath(const QString &path) {
-	QString dir = QDir::toNativeSeparators(path);
-	QStringList qslIdentifiers;
-	for (int i = 0; i < count(); i++) {
-		qslIdentifiers << item(i)->data(Qt::UserRole).toString();
-	}
-	if (!dir.isEmpty() && !qslIdentifiers.contains(dir)) {
-		QListWidgetItem *qlwiPath = new QListWidgetItem(QIcon(), QDir(dir).path(), this);
-		qlwiPath->setData(Qt::UserRole, QVariant(dir));
-		setCurrentItem(qlwiPath);
-	}
-}
-
-void PathListWidget::checkAcceptDragEvent(QDropEvent *event, bool store) {
-	if (event->mimeData()->hasUrls()) {
-		foreach (QUrl url, event->mimeData()->urls()) {
-			if (url.isLocalFile()) {
-				QFileInfo info(url.toLocalFile());
-				switch (pathType) {
-					case FILE_EXE:
-						if (info.isFile() && info.isExecutable()) {
-							if (store) {
-								addFilePath(info.filePath());
-							}
-							event->setDropAction(Qt::LinkAction);
-							event->accept();
-						}
-						break;
-					case FOLDER:
-						if (info.isDir()) {
-							if (store) {
-								addFolderPath(url.toLocalFile());
-							}
-							event->setDropAction(Qt::LinkAction);
-							event->accept();
-						}
-						break;
-				}
-			}
-		}
-	}
-}
-
-void PathListWidget::dragEnterEvent(QDragEnterEvent *event) {
-	checkAcceptDragEvent(event, false);
-}
-
-void PathListWidget::dragMoveEvent(QDragMoveEvent *event) {
-	checkAcceptDragEvent(event, false);
-}
-
-void PathListWidget::dropEvent(QDropEvent *event) {
-	checkAcceptDragEvent(event, true);
-}
+#else
+class CrashReporter;
+#endif
