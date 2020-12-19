106751d7d4cb5d663f2e3895db1b05d5bfb24ea3
Jan Klass <kissaki@posteo.de>
2020-12-19 19:09:58 +0100

    New translations Overlay_unix.cpp (Polish)

--- mumble_pl.ts
+++ mumble_pl.ts
@@ -3,70 +3,32 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_OVERLAYEDITORSCENE_H_
-#define MUMBLE_MUMBLE_OVERLAYEDITORSCENE_H_
+#include "Overlay.h"
+#include "OverlayConfig.h"
 
-#include <QtCore/QtGlobal>
-#include <QtWidgets/QGraphicsScene>
+void Overlay::platformInit() {
+	d = nullptr;
+}
 
-#include "Settings.h"
+void Overlay::setActiveInternal(bool) {
+}
 
-class OverlayEditorScene : public QGraphicsScene {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(OverlayEditorScene)
+bool OverlayConfig::supportsInstallableOverlay() {
+	return false;
+}
 
-protected:
-	QGraphicsItem *qgiGroup;
+bool OverlayConfig::isInstalled() {
+	return true;
+}
 
-	QGraphicsPixmapItem *qgpiMuted;
-	QGraphicsPixmapItem *qgpiAvatar;
-	QGraphicsPixmapItem *qgpiName;
-	QGraphicsPixmapItem *qgpiChannel;
-	QGraphicsPathItem *qgpiBox;
-	QGraphicsRectItem *qgriSelected;
-	QGraphicsPixmapItem *qgpiSelected;
-	int iDragCorner;
+bool OverlayConfig::needsUpgrade() {
+	return false;
+}
 
-	Qt::WindowFrameSection wfsHover;
+bool OverlayConfig::installFiles() {
+	return false;
+}
 
-	unsigned int uiSize;
-
-	void setup();
-
-	void contextMenuEvent(QGraphicsSceneContextMenuEvent *e) Q_DECL_OVERRIDE;
-	void mousePressEvent(QGraphicsSceneMouseEvent *e) Q_DECL_OVERRIDE;
-	void mouseMoveEvent(QGraphicsSceneMouseEvent *e) Q_DECL_OVERRIDE;
-	void mouseReleaseEvent(QGraphicsSceneMouseEvent *e) Q_DECL_OVERRIDE;
-	void updateCursorShape(const QPointF &point);
-
-	void drawBackground(QPainter *, const QRectF &) Q_DECL_OVERRIDE;
-
-	QGraphicsPixmapItem *childAt(const QPointF &);
-	QRectF selectedRect() const;
-
-	static Qt::WindowFrameSection rectSection(const QRectF &rect, const QPointF &point, qreal dist = 3.0f);
-
-public:
-	Settings::TalkState tsColor;
-	unsigned int uiZoom;
-	OverlaySettings os;
-
-	OverlayEditorScene(const OverlaySettings &, QObject *p = nullptr);
-public slots:
-	void resync();
-	void updateSelected();
-
-	void updateMuted();
-	void updateUserName();
-	void updateChannel();
-	void updateAvatar();
-
-	void moveMuted();
-	void moveUserName();
-	void moveChannel();
-	void moveAvatar();
-	void moveBox();
-};
-
-#endif
+bool OverlayConfig::uninstallFiles() {
+	return false;
+}
