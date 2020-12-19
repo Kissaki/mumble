d62d74c2a89ef258bc957ae471da6479948adbaa
Jan Klass <kissaki@posteo.de>
2020-12-19 18:42:44 +0100

    New translations Global.h (Polish)

--- mumble_pl.ts
+++ mumble_pl.ts
@@ -3,62 +3,144 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_GKEY_H
-#define MUMBLE_MUMBLE_GKEY_H
+#ifndef MUMBLE_MUMBLE_GLOBAL_H_
+#define MUMBLE_MUMBLE_GLOBAL_H_
 
-#include "win.h"
+#include <QtCore/QDir>
+#include <boost/shared_ptr.hpp>
 
-#include <QtCore/QLibrary>
-#include <QtCore/QString>
-#include <QtCore/QUuid>
+#include "ACL.h"
+#include "Settings.h"
+#include "Timer.h"
+#include "Version.h"
 
-#define GKEY_LOGITECH_DLL_REG_HKEY HKEY_CLASSES_ROOT
-#ifdef _M_X64
-#	define GKEY_LOGITECH_DLL_REG_PATH L"Wow6432Node\\CLSID\\{7bded654-f278-4977-a20f-6e72a0d07859}\\ServerBinary"
-#	define GKEY_LOGITECH_DLL_DEFAULT_LOCATION \
-		"C:/Program Files/Logitech Gaming Software/SDK/G-key/x64/LogitechGkey.dll"
-#else
-#	define GKEY_LOGITECH_DLL_REG_PATH L"CLSID\\{7bded654-f278-4977-a20f-6e72a0d07859}\\ServerBinary"
-#	define GKEY_LOGITECH_DLL_DEFAULT_LOCATION \
-		"C:/Program Files/Logitech Gaming Software/SDK/G-key/x86/LogitechGkey.dll"
-#endif
+// Global helper class to spread variables around across threads.
 
-#define GKEY_MIN_MOUSE_BUTTON 6
-#define GKEY_MAX_MOUSE_BUTTON 20
-#define GKEY_MIN_KEYBOARD_BUTTON 1
-#define GKEY_MAX_KEYBOARD_BUTTON 29
-#define GKEY_MIN_KEYBOARD_MODE 1
-#define GKEY_MAX_KEYBOARD_MODE 3
+class MainWindow;
+class ServerHandler;
+class AudioInput;
+class AudioOutput;
+class Database;
+class Log;
+class Plugins;
+class QSettings;
+class Overlay;
+class LCD;
+class Zeroconf;
+class OverlayClient;
+class CELTCodec;
+class OpusCodec;
+class LogEmitter;
+class DeveloperConsole;
+class TalkingUI;
 
-#define GKEY_BUTTON_MOUSE 1
-#define GKEY_BUTTON_KEYBOARD 2
+class QNetworkAccessManager;
 
-#define GKEY_MOUSE_GUID "c41e60af-9022-46cf-bc39-37981082d716"
-#define GKEY_KEYBOARD_GUID "153e64e6-98c8-4e03-80ef-5ffd33d25b8a"
-
-class GKeyLibrary {
+struct Global Q_DECL_FINAL {
+private:
+	Q_DISABLE_COPY(Global)
 public:
-	GKeyLibrary();
-	virtual ~GKeyLibrary();
-	bool isValid() const;
-	static const QUuid quMouse;
-	static const QUuid quKeyboard;
+	static Global *g_global_struct;
+	MainWindow *mw;
+	Settings s;
+	boost::shared_ptr< ServerHandler > sh;
+	boost::shared_ptr< AudioInput > ai;
+	boost::shared_ptr< AudioOutput > ao;
+	/**
+	 * @remark Must only be accessed from the main event loop
+	 */
+	Database *db;
+	Log *l;
+	Plugins *p;
+	QSettings *qs;
+#ifdef USE_OVERLAY
+	Overlay *o;
+#endif
+	LCD *lcd;
+	Zeroconf *zeroconf;
+	QNetworkAccessManager *nam;
+	QSharedPointer< LogEmitter > le;
+	DeveloperConsole *c;
+	TalkingUI *talkingUI;
+	int iPushToTalk;
+	Timer tDoublePush;
+	quint64 uiDoublePush;
+	/// Holds the current VoiceTarget ID to send audio to
+	int iTarget;
+	/// Holds the value of iTarget before its last change until the current
+	/// audio-stream ends (and it has a value > 0). See the comment in
+	/// AudioInput::flushCheck for further details on this.
+	int iPrevTarget;
+	bool bPushToMute;
+	bool bCenterPosition;
+	bool bPosTest;
+	bool bInAudioWizard;
+#ifdef USE_OVERLAY
+	OverlayClient *ocIntercept;
+#endif
+	int iAudioPathTime;
+	/// A unique ID for the current user. It is being assigned by the server right
+	/// after connecting to it. An ID of 0 indicates that the user currently isn't
+	/// connected to a server.
+	unsigned int uiSession;
+	ChanACL::Permissions pPermissions;
+	int iMaxBandwidth;
+	int iAudioBandwidth;
+	QDir qdBasePath;
+	QMap< int, CELTCodec * > qmCodecs;
+	OpusCodec *oCodec;
+	int iCodecAlpha, iCodecBeta;
+	bool bPreferAlpha;
+	bool bOpus;
+	bool bAttenuateOthers;
+	/// If set the AudioOutput::mix will forcefully adjust the volume of all
+	/// non-priority speakers.
+	bool prioritySpeakerActiveOverride;
+	bool bAllowHTML;
+	unsigned int uiMessageLength;
+	unsigned int uiImageLength;
+	unsigned int uiMaxUsers;
+	bool bQuit;
+	QString windowTitlePostfix;
+	bool bDebugDumpInput;
+	bool bDebugPrintQueue;
 
-	bool isMouseButtonPressed(int button);
-	bool isKeyboardGkeyPressed(int key, int mode);
-	QString getMouseButtonString(int button);
-	QString getKeyboardGkeyString(int key, int mode);
+	bool bHappyEaster;
+	static const char ccHappyEaster[];
 
-protected:
-	QLibrary qlLogiGkey;
-	bool bValid;
-
-	BOOL (*LogiGkeyInit)(void *);
-	void (*LogiGkeyShutdown)();
-	BOOL (*LogiGkeyIsMouseButtonPressed)(int button);
-	BOOL (*LogiGkeyIsKeyboardGkeyPressed)(int key, int mode);
-	wchar_t *(*LogiGkeyGetMouseButtonString)(int button);
-	wchar_t *(*LogiGkeyGetKeyboardGkeyString)(int key, int mode);
+	Global(const QString &qsConfigPath = QString());
+	~Global();
 };
 
-#endif // MUMBLE_MUMBLE_GKEY_H
+// Class to handle ordered initialization of globals.
+// This allows the same link-time magic as used everywhere else
+// for globals that need an init before the GUI starts, but
+// after we reach main().
+
+class DeferInit {
+private:
+	Q_DISABLE_COPY(DeferInit)
+protected:
+	static QMultiMap< int, DeferInit * > *qmDeferers;
+	void add(int priority);
+
+public:
+	DeferInit(int priority) { add(priority); };
+	DeferInit() { add(0); };
+	virtual ~DeferInit();
+	virtual void initialize(){};
+	virtual void destroy(){};
+	static void run_initializers();
+	static void run_destroyers();
+};
+
+/// Special exit code which causes mumble to restart itself. The outward facing return code with be 0
+const int MUMBLE_EXIT_CODE_RESTART = 64738;
+
+// -Wshadow is bugged. If an inline function of a class uses a variable or
+// parameter named 'g', that will generate a warning even if the class header
+// is included long before this definition.
+
+#define g (*Global::g_global_struct)
+
+#endif
