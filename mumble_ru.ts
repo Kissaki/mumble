2ecfb5e350f5b781e9451e3154514aeea13fc137
Jan Klass <kissaki@posteo.de>
2020-12-19 18:59:02 +0100

    New translations ThemeInfo.h (Russian)

--- mumble_ru.ts
+++ mumble_ru.ts
@@ -3,72 +3,87 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include "TextToSpeech.h"
+#ifndef MUMBLE_MUMBLE_THEMEINFO_H_
+#define MUMBLE_MUMBLE_THEMEINFO_H_
 
-// As the include order seems to make a difference, disable clang-format for them
-// clang-format off
-#include <servprov.h>
-#include <sapi.h>
-// clang-format on
+#include <QMetaType>
+#include <QtCore/QFileInfo>
+#include <QtCore/QMap>
+#include <QtCore/QString>
+#ifndef Q_MOC_RUN
+#	include <boost/optional.hpp>
+#endif
 
-#undef FAILED
-#define FAILED(Status) (static_cast< HRESULT >(Status) < 0)
+class QSettings;
+class QDir;
 
-class TextToSpeechPrivate {
+class ThemeInfo;
+typedef QMap< QString, ThemeInfo > ThemeMap;
+
+/// Description of a Mumble theme with multiple styles
+class ThemeInfo {
 public:
-	ISpVoice *pVoice;
-	TextToSpeechPrivate();
-	~TextToSpeechPrivate();
-	void say(const QString &text);
-	void setVolume(int v);
+	/// A specific style of a Mumble theme
+	///
+	/// Multiple styles can for example be used to differentiate light/dark
+	/// variants of a theme.
+	///
+	/// A single style can refer to multiple run-time platform specific qss
+	/// theme files.
+	class StyleInfo {
+	public:
+		/// Name of the theme containing this style
+		QString themeName;
+		/// Name for the style
+		QString name;
+
+		/// @return Returns platform specific qss or defaultQss if none available
+		QFileInfo getPlatformQss() const;
+
+		/// Default QSS file for the style
+		QFileInfo defaultQss;
+		/// Platform specific QSS files available
+		QMap< QString, QFileInfo > qssFiles;
+	};
+
+	typedef QMap< QString, StyleInfo > StylesMap;
+
+	/// Takes stock of all mumble themes in the given folders.
+	///
+	/// If a theme with the same name is available in multiple directories
+	/// only the last occurance will be returned.
+	///
+	/// @param themesDirectories List of directories to search for theme directories.
+	/// @return Map of theme name to Theme
+	static ThemeMap scanDirectories(const QVector< QDir > &themesDirectories);
+
+	/// Takes stock of all mumble themes in the given folder
+	///
+	/// Uses loadThemeInfoFromDirectory on each directory in the folder
+	/// to find themes. Themes with the same names will override each other.
+	///
+	/// @param themesDirectory Directory to scan for theme directories
+	/// @return Map of theme name to Theme
+	static ThemeMap scanDirectory(const QDir &themesDirectory);
+
+	/// Loads the theme description from a given directory
+	///
+	/// @param themeDirectory
+	/// @return Theme if description was correctly loaded. boost::none if not.
+	static boost::optional< ThemeInfo > load(const QDir &themeDirectory);
+
+	/// @return Style with given name or default
+	StyleInfo getStyle(QString name_) const;
+
+	/// Ideally unique theme name. A theme with identical name can override.
+	QString name;
+	/// Style name to style mapping.
+	StylesMap styles;
+	/// Default style
+	QString defaultStyle;
 };
 
-TextToSpeechPrivate::TextToSpeechPrivate() {
-	pVoice = nullptr;
+Q_DECLARE_METATYPE(ThemeInfo);
+Q_DECLARE_METATYPE(ThemeInfo::StyleInfo);
 
-	HRESULT hr = CoCreateInstance(CLSID_SpVoice, nullptr, CLSCTX_ALL, IID_ISpVoice, (void **) &pVoice);
-	if (FAILED(hr))
-		qWarning("TextToSpeechPrivate: Failed to allocate TTS Voice");
-}
-
-TextToSpeechPrivate::~TextToSpeechPrivate() {
-	if (pVoice)
-		pVoice->Release();
-}
-
-void TextToSpeechPrivate::say(const QString &text) {
-	if (pVoice) {
-		pVoice->Speak((const wchar_t *) text.utf16(), SPF_ASYNC, nullptr);
-	}
-}
-
-void TextToSpeechPrivate::setVolume(int volume) {
-	if (pVoice)
-		pVoice->SetVolume(volume);
-}
-
-TextToSpeech::TextToSpeech(QObject *p) : QObject(p) {
-	enabled = true;
-	d       = new TextToSpeechPrivate();
-}
-
-TextToSpeech::~TextToSpeech() {
-	delete d;
-}
-
-void TextToSpeech::say(const QString &text) {
-	if (enabled)
-		d->say(text);
-}
-
-void TextToSpeech::setEnabled(bool e) {
-	enabled = e;
-}
-
-void TextToSpeech::setVolume(int volume) {
-	d->setVolume(volume);
-}
-
-bool TextToSpeech::isEnabled() const {
-	return enabled;
-}
+#endif // MUMBLE_MUMBLE_THEMEINFO_H_
