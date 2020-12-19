dbb8d22fde18f39cfbba5015bc66ae2635f88d9f
Jan Klass <kissaki@posteo.de>
2020-12-19 19:15:34 +0100

    New translations About.cpp (Russian)

--- mumble_ru.ts
+++ mumble_ru.ts
@@ -3,88 +3,77 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_ASIOINPUT_H_
-#define MUMBLE_MUMBLE_ASIOINPUT_H_
+#include "About.h"
 
-#include "ui_ASIOInput.h"
+#include "License.h"
+#include "MainWindow.h"
 
-#include "AudioInput.h"
-#include "ConfigDialog.h"
+#include "Utils.h"
 
-#include "win.h"
+#include <QtWidgets/QPushButton>
 
-#include <QtCore/QList>
-#include <QtCore/QObject>
-#include <QtCore/QString>
-#include <QtCore/QWaitCondition>
+// We define a global macro called 'g'. This can lead to issues when included code uses 'g' as a type or parameter name
+// (like protobuf 3.7 does). As such, for now, we have to make this our last include.
+#include "Global.h"
 
-#include <combaseapi.h>
+AboutDialog::AboutDialog(QWidget *p) : QDialog(p) {
+	setWindowTitle(tr("About Mumble"));
 
-#include <asiodrvr.h>
+	QTabWidget *qtwTab   = new QTabWidget(this);
+	QVBoxLayout *vblMain = new QVBoxLayout(this);
 
-typedef QPair< QString, QString > ASIODev;
+	QTextEdit *qteLicense = new QTextEdit(qtwTab);
+	qteLicense->setReadOnly(true);
+	qteLicense->setPlainText(License::license());
+	qteLicense->setAccessibleName(tr("License agreement"));
 
-class ASIOConfig : public ConfigWidget, public Ui::ASIOConfig {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(ASIOConfig)
-protected:
-	QList< ASIODev > qlDevs;
-	bool bOk;
+	QTextEdit *qteAuthors = new QTextEdit(qtwTab);
+	qteAuthors->setReadOnly(true);
+	qteAuthors->setPlainText(License::authors());
+	qteAuthors->setAccessibleName(tr("Authors"));
 
-public:
-	/// The unique name of this ConfigWidget
-	static const QString name;
-	ASIOConfig(Settings &st);
-	virtual QString title() const Q_DECL_OVERRIDE;
-	virtual const QString &getName() const Q_DECL_OVERRIDE;
-	virtual QIcon icon() const Q_DECL_OVERRIDE;
-public slots:
-	void save() const Q_DECL_OVERRIDE;
-	void load(const Settings &r) Q_DECL_OVERRIDE;
-	void clearQuery();
-	void on_qcbDevice_activated(int index);
-	void on_qpbQuery_clicked();
-	void on_qpbConfig_clicked();
-	void on_qpbAddMic_clicked();
-	void on_qpbRemMic_clicked();
-	void on_qpbAddSpeaker_clicked();
-	void on_qpbRemSpeaker_clicked();
-};
+	QTextBrowser *qtb3rdPartyLicense = new QTextBrowser(qtwTab);
+	qtb3rdPartyLicense->setReadOnly(true);
+	qtb3rdPartyLicense->setOpenExternalLinks(true);
+	qtb3rdPartyLicense->setAccessibleName(tr("Third-party license agreements"));
 
-#define IEEE754_64FLOAT 1
-#include "asio.h"
+	QList< LicenseInfo > thirdPartyLicenses = License::thirdPartyLicenses();
+	foreach (LicenseInfo li, thirdPartyLicenses) {
+		qtb3rdPartyLicense->append(QString::fromLatin1("<h3>%1 (<a href=\"%2\">%2</a>)</h3><pre>%3</pre>")
+									   .arg(li.name.toHtmlEscaped())
+									   .arg(li.url.toHtmlEscaped())
+									   .arg(li.license.toHtmlEscaped()));
+	}
 
-class ASIOInput : public AudioInput {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(ASIOInput)
-protected:
-	IASIO *iasio;
+	qtb3rdPartyLicense->moveCursor(QTextCursor::Start);
 
-	int iNumMic, iNumSpeaker;
-	long lBufSize;
-	ASIOBufferInfo *abiInfo;
-	ASIOChannelInfo *aciInfo;
+	QWidget *about = new QWidget(qtwTab);
 
-	// ASIO Callbacks
-	static ASIOInput *aiSelf;
+	QLabel *icon = new QLabel(about);
+	icon->setPixmap(g.mw->qiIcon.pixmap(g.mw->qiIcon.actualSize(QSize(128, 128))));
 
-	static void sampleRateChanged(ASIOSampleRate sRate);
-	static long asioMessages(long selector, long value, void *message, double *opt);
-	static void bufferSwitch(long index, ASIOBool processNow);
-	static ASIOTime *bufferSwitchTimeInfo(ASIOTime *timeInfo, long index, ASIOBool processNow);
+	QLabel *text = new QLabel(about);
+	text->setTextInteractionFlags(Qt::TextBrowserInteraction);
+	text->setOpenExternalLinks(true);
+	text->setText(tr("<h3>Mumble (%1)</h3>"
+					 "<p>%3</p>"
+					 "<p><b>A voice-chat utility for gamers</b></p>"
+					 "<p><tt><a href=\"%2\">%2</a></tt></p>")
+					  .arg(QLatin1String(MUMBLE_RELEASE))
+					  .arg(QLatin1String("https://www.mumble.info/"))
+					  .arg(QLatin1String("Copyright 2005-2020 The Mumble Developers")));
+	QHBoxLayout *qhbl = new QHBoxLayout(about);
+	qhbl->addWidget(icon);
+	qhbl->addWidget(text);
 
-	void addBuffer(long sampType, int interleave, void *src, float *RESTRICT dst);
-	void bufferReady(long index);
-	bool initializeDriver();
+	qtwTab->addTab(about, tr("&About Mumble"));
+	qtwTab->addTab(qteLicense, tr("&License"));
+	qtwTab->addTab(qteAuthors, tr("A&uthors"));
+	qtwTab->addTab(qtb3rdPartyLicense, tr("&Third-Party Licenses"));
 
-	QWaitCondition qwDone;
+	QPushButton *okButton = new QPushButton(tr("OK"), this);
+	connect(okButton, SIGNAL(clicked()), this, SLOT(accept()));
 
-public:
-	ASIOInput();
-	~ASIOInput() Q_DECL_OVERRIDE;
-	void run() Q_DECL_OVERRIDE;
-};
-
-#endif
+	vblMain->addWidget(qtwTab);
+	vblMain->addWidget(okButton);
+}
