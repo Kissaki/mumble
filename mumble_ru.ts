2197028c462c677c94e0ff6b5a913d0eba59f65f
Jan Klass <kissaki@posteo.de>
2020-12-19 19:00:23 +0100

    New translations XboxInput.h (Russian)

--- mumble_ru.ts
+++ mumble_ru.ts
@@ -3,67 +3,82 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include "XboxInput.h"
+#ifndef MUMBLE_MUMBLE_XBOXINPUT_H_
+#define MUMBLE_MUMBLE_XBOXINPUT_H_
 
-#include <QtCore/QStringList>
+#include "win.h"
 
-const QUuid XboxInput::s_XboxInputGuid = QUuid(QString::fromLatin1("ca3937e3-640c-4d9e-9ef3-903f8b4fbcab"));
+#include <QUuid>
 
-XboxInput::XboxInput()
-	: GetState(nullptr), m_getStateFunc(nullptr), m_getStateExFunc(nullptr), m_xinputlib(nullptr), m_valid(false) {
-	// Load the most suitable XInput DLL available.
-	//
-	// We prefer 1_4 and 1_3 over the others because they provide
-	// XInputGetStateEx(), which allows us to query the state of
-	// the guide button.
-	//
-	// See https://msdn.microsoft.com/en-us/library/windows/desktop/hh405051(v=vs.85).aspx
-	// for more information.
-	QStringList alternatives;
-	alternatives << QLatin1String("XInput1_4.dll");
-	alternatives << QLatin1String("xinput1_3.dll");
-	alternatives << QLatin1String("XInput9_1_0.dll");
-	alternatives << QLatin1String("xinput1_2.dll");
-	alternatives << QLatin1String("xinput1_1.dll");
+#include <stdint.h>
 
-	foreach (const QString &lib, alternatives) {
-		m_xinputlib = LoadLibraryW(reinterpret_cast< const wchar_t * >(lib.utf16()));
-		if (m_xinputlib) {
-			qWarning("XboxInput: using XInput DLL '%s'", qPrintable(lib));
-			m_valid = true;
-			break;
-		}
-	}
+/// XBOXINPUT_MAX_DEVICES defines the maximum
+/// number of devices that can be connected
+/// to the system at once.
+///
+/// In typical operation, one calls GetState()
+/// in a loop bounded by XBOXINPUT_MAX_DEVICES.
+#define XBOXINPUT_MAX_DEVICES 4
 
-	if (!m_valid) {
-		qWarning("XboxInput: no valid XInput DLL was found on the system.");
-		return;
-	}
+/// XBOXINPUT_TRIGGER_THRESHOLD defines the thresold
+/// that an analog trigger (leftTrigger and
+/// rightTrigger of XboxInputState) must have exceeded
+/// in order to count as a button press.
+#define XBOXINPUT_TRIGGER_THRESHOLD 30
 
-	m_getStateFunc = reinterpret_cast< XboxInputGetStateFunc >(GetProcAddress(m_xinputlib, "XInputGetState"));
-	// Undocumented XInputGetStateEx -- ordinal 100. It is available in XInput 1.3 and greater.
-	// It provides access to the state of the guide button.
-	// For reference, see SDL's XInput support: http://www.libsdl.org/tmp/SDL/src/core/windows/SDL_xinput.c
-	m_getStateExFunc = reinterpret_cast< XboxInputGetStateFunc >(GetProcAddress(m_xinputlib, (char *) 100));
+/// XboxInputState represents the state of an
+/// Xbox controller as returned by GetState().
+struct XboxInputState {
+	uint32_t packetNumber;
+	uint16_t buttons;
+	uint8_t leftTrigger;
+	uint8_t rightTrigger;
+	uint16_t leftThumbX;
+	uint16_t leftThumbY;
+	uint16_t rightThumbY;
+	uint16_t rightThumbX;
+	uint32_t paddingReserved; // Required for XInputGetStateEx. Not required for XInputGetState.
+};
 
-	if (m_getStateExFunc) {
-		GetState = m_getStateExFunc;
-		qWarning("XboxInput: using XInputGetStateEx() as querying function.");
-	} else if (m_getStateFunc) {
-		GetState = m_getStateFunc;
-		qWarning("XboxInput: using XInputGetState() as querying function.");
-	} else {
-		m_valid = false;
-		qWarning("XboxInput: no valid querying function found.");
-	}
-}
+typedef uint32_t(WINAPI *XboxInputGetStateFunc)(uint32_t deviceIndex, XboxInputState *state);
 
-XboxInput::~XboxInput() {
-	if (m_xinputlib) {
-		FreeLibrary(m_xinputlib);
-	}
-}
+/// XboxInput is an XInput wrapper that dynamically loads an appropriate
+/// xinput*.dll on construction and provides access to its GetState(Ex)
+/// function.
+class XboxInput {
+public:
+	XboxInput();
+	virtual ~XboxInput();
 
-bool XboxInput::isValid() const {
-	return m_valid;
-}
+	/// s_XboxInputGuid is the GUID used by GlobalShortcut_win
+	/// to distinguish XboxInputLibrary's events from other event
+	/// soures.
+	static const QUuid s_XboxInputGuid;
+
+	/// isValid determines wheter the XboxInputLibrary
+	/// is usable.
+	bool isValid() const;
+
+	/// Query the state of the Xbox controller at deviceIndex.
+	/// If the function succeeds, it returns 0 (Windows's ERROR_SUCCESS).
+	/// If no device is connected, it returns 0x48F (Windows's ERROR_DEVICE_NOT_CONNECTED).
+	XboxInputGetStateFunc GetState;
+
+protected:
+	/// m_getStateFunc represents XInputGetState from the XInput DLL.
+	XboxInputGetStateFunc m_getStateFunc;
+
+	/// m_getStateFuncEx represents XInputGetStateEx, which is optionally
+	/// available in the XInput DLL.
+	XboxInputGetStateFunc m_getStateExFunc;
+
+	/// m_xinputlib is the handle to the XInput DLL as returned by
+	/// LoadLibrary.
+	HMODULE m_xinputlib;
+
+	/// m_valid determines whether or not the XboxInputLibrary
+	/// is valid for use.
+	bool m_valid;
+};
+
+#endif
