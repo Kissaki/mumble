e15f49cd5d2810e1496e23cac84f48c3b576392b
Jan Klass <kissaki@posteo.de>
2020-12-19 18:43:38 +0100

    New translations MumbleApplication.cpp (Polish)

--- mumble_pl.ts
+++ mumble_pl.ts
@@ -3,48 +3,99 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_CONFIGDIALOG_H_
-#define MUMBLE_MUMBLE_CONFIGDIALOG_H_
+#include "MumbleApplication.h"
 
-#include "ConfigWidget.h"
-#include "Settings.h"
+#include "EnvUtils.h"
+#include "MainWindow.h"
+#include "GlobalShortcut.h"
 
-#include "ui_ConfigDialog.h"
+#if defined(Q_OS_WIN)
+#	include "GlobalShortcut_win.h"
+#endif
 
-#include <QtCore/QMutex>
+#include <QtGui/QFileOpenEvent>
 
-class ConfigDialog : public QDialog, public Ui::ConfigDialog {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(ConfigDialog)
-protected:
-	static QMutex s_existingWidgetsMutex;
-	static QHash< QString, ConfigWidget * > s_existingWidgets;
-	QHash< ConfigWidget *, QWidget * > qhPages;
-	QMap< unsigned int, ConfigWidget * > qmWidgets;
-	QMap< QListWidgetItem *, ConfigWidget * > qmIconWidgets;
-	void updateListView();
-	void addPage(ConfigWidget *aw, unsigned int idx);
-	Settings s;
+// We define a global macro called 'g'. This can lead to issues when included code uses 'g' as a type or parameter name
+// (like protobuf 3.7 does). As such, for now, we have to make this our last include.
+#include "Global.h"
 
-public:
-	ConfigDialog(QWidget *p = nullptr);
-	~ConfigDialog() Q_DECL_OVERRIDE;
+MumbleApplication *MumbleApplication::instance() {
+	return static_cast< MumbleApplication * >(QCoreApplication::instance());
+}
 
-	/// @returns The pointer to the existing ConfigWidget with the given name or nullptr,
-	/// 	if no such widget exists.
-	static ConfigWidget *getConfigWidget(const QString &name);
+MumbleApplication::MumbleApplication(int &pargc, char **pargv) : QApplication(pargc, pargv) {
+	connect(this, SIGNAL(commitDataRequest(QSessionManager &)), SLOT(onCommitDataRequest(QSessionManager &)),
+			Qt::DirectConnection);
+}
 
-signals:
-	/// Emitted whenever the settings dialog has been accepted. For potential slots this
-	/// means that the settings potentially have changed.
-	void settingsAccepted();
-public slots:
-	void on_pageButtonBox_clicked(QAbstractButton *);
-	void on_dialogButtonBox_clicked(QAbstractButton *);
-	void on_qlwIcons_currentItemChanged(QListWidgetItem *current, QListWidgetItem *previous);
-	void apply();
-	void accept() Q_DECL_OVERRIDE;
-};
+QString MumbleApplication::applicationVersionRootPath() {
+	QString versionRoot = EnvUtils::getenv(QLatin1String("MUMBLE_VERSION_ROOT"));
+	if (versionRoot.count() > 0) {
+		return versionRoot;
+	}
+	return this->applicationDirPath();
+}
 
+void MumbleApplication::onCommitDataRequest(QSessionManager &) {
+	// Make sure the config is saved and supress the ask on quit message
+	if (g.mw) {
+		g.s.save();
+		g.mw->bSuppressAskOnQuit = true;
+		qWarning() << "Session likely ending. Suppressing ask on quit";
+	}
+}
+
+bool MumbleApplication::event(QEvent *e) {
+	if (e->type() == QEvent::FileOpen) {
+		QFileOpenEvent *foe = static_cast< QFileOpenEvent * >(e);
+		if (!g.mw) {
+			this->quLaunchURL = foe->url();
+		} else {
+			g.mw->openUrl(foe->url());
+		}
+		return true;
+	}
+	return QApplication::event(e);
+}
+
+#ifdef Q_OS_WIN
+/// gswForward forwards a native Windows keyboard/mouse message
+/// into GlobalShortcutWin's event stream.
+///
+/// @return  Returns true if the forwarded event was suppressed
+///          by GlobalShortcutWin. Otherwise, returns false.
+static bool gswForward(MSG *msg) {
+	GlobalShortcutWin *gsw = static_cast< GlobalShortcutWin * >(GlobalShortcutEngine::engine);
+	if (!gsw) {
+		return false;
+	}
+	switch (msg->message) {
+		case WM_LBUTTONDOWN:
+		case WM_LBUTTONUP:
+		case WM_RBUTTONDOWN:
+		case WM_RBUTTONUP:
+		case WM_MBUTTONDOWN:
+		case WM_MBUTTONUP:
+		case WM_XBUTTONDOWN:
+		case WM_XBUTTONUP:
+			return gsw->injectMouseMessage(msg);
+		case WM_KEYDOWN:
+		case WM_KEYUP:
+		case WM_SYSKEYDOWN:
+		case WM_SYSKEYUP:
+			return gsw->injectKeyboardMessage(msg);
+	}
+	return false;
+}
+
+bool MumbleApplication::nativeEventFilter(const QByteArray &, void *message, long *) {
+	MSG *msg = reinterpret_cast< MSG * >(message);
+	if (QThread::currentThread() == thread()) {
+		bool suppress = gswForward(msg);
+		if (suppress) {
+			return true;
+		}
+	}
+	return false;
+}
 #endif
