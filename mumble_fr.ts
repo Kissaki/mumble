ae5c299e50b3da4ea80323951735b51307c50a04
Jan Klass <kissaki@posteo.de>
2020-12-19 18:00:20 +0100

    New translations TextToSpeech_win.cpp (French)

--- mumble_fr.ts
+++ mumble_fr.ts
@@ -1,146 +1,74 @@
-// Copyright 2020 The Mumble Developers. All rights reserved.
+// Copyright 2005-2020 The Mumble Developers. All rights reserved.
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_TALKINGUIENTRY_H_
-#define MUMBLE_MUMBLE_TALKINGUIENTRY_H_
+#include "TextToSpeech.h"
 
-#include "Settings.h"
-#include "TalkingUIComponent.h"
+// As the include order seems to make a difference, disable clang-format for them
+// clang-format off
+#include <servprov.h>
+#include <sapi.h>
+// clang-format on
 
-#include <QIcon>
-#include <QLatin1String>
-#include <QString>
-#include <QTimer>
+#undef FAILED
+#define FAILED(Status) (static_cast< HRESULT >(Status) < 0)
 
-#include <vector>
-
-class QWidget;
-class QLabel;
-class TalkingUIContainer;
-class ClientUser;
-class Channel;
-
-enum class EntryType { USER, LISTENER };
-
-enum class EntryPriority { LOWEST, LOW, DEFAULT, HIGH };
-
-
-class TalkingUIEntry : public TalkingUIComponent {
-	// Needed in order for the container class to be able to modify m_container
-	friend class TalkingUIContainer;
-
-protected:
-	unsigned int m_associatedUserSession;
-
-	TalkingUIContainer *m_container = nullptr;
-
-	EntryPriority m_priority = EntryPriority::DEFAULT;
-
+class TextToSpeechPrivate {
 public:
-	TalkingUIEntry(unsigned int associatedUserSession);
-	virtual ~TalkingUIEntry() = default;
-
-	virtual EntryType getType() const = 0;
-
-	virtual unsigned int getAssociatedUserSession() const;
-
-	virtual TalkingUIContainer *getContainer();
-	virtual const TalkingUIContainer *getContainer() const;
-
-	virtual void setPriority(EntryPriority priority);
-	virtual EntryPriority getPriority() const;
-
-	virtual void setIconSize(int size) = 0;
-
-	virtual void setDisplayString(const QString &displayString) = 0;
-
-	virtual int compare(const TalkingUIEntry &other) const;
-
-	bool operator==(const TalkingUIEntry &other) const;
-	bool operator!=(const TalkingUIEntry &other) const;
-	bool operator<(const TalkingUIEntry &other) const;
-	bool operator<=(const TalkingUIEntry &other) const;
-	bool operator>(const TalkingUIEntry &other) const;
-	bool operator>=(const TalkingUIEntry &other) const;
+	ISpVoice *pVoice;
+	TextToSpeechPrivate();
+	~TextToSpeechPrivate();
+	void say(const QString &text);
+	void setVolume(int v);
 };
 
+TextToSpeechPrivate::TextToSpeechPrivate() {
+	pVoice = nullptr;
 
-class TalkingUIUser : public TalkingUIEntry {
-protected:
-	QWidget *m_backgroundWidget = nullptr;
+	HRESULT hr = CoCreateInstance(CLSID_SpVoice, nullptr, CLSCTX_ALL, IID_ISpVoice, (void **) &pVoice);
+	if (FAILED(hr))
+		qWarning("TextToSpeechPrivate: Failed to allocate TTS Voice");
+}
 
-	QLabel *m_talkingIcon = nullptr;
-	QLabel *m_nameLabel   = nullptr;
-	QLabel *m_statusIcons = nullptr;
+TextToSpeechPrivate::~TextToSpeechPrivate() {
+	if (pVoice)
+		pVoice->Release();
+}
 
-	QString m_name;
+void TextToSpeechPrivate::say(const QString &text) {
+	if (pVoice) {
+		pVoice->Speak((const wchar_t *) text.utf16(), SPF_ASYNC, nullptr);
+	}
+}
 
-	int m_iconSize                     = 5;
-	Settings::TalkState m_talkingState = Settings::Passive;
+void TextToSpeechPrivate::setVolume(int volume) {
+	if (pVoice)
+		pVoice->SetVolume(volume);
+}
 
-	QTimer m_timer;
-	bool m_restrictLifetime = false;
+TextToSpeech::TextToSpeech(QObject *p) : QObject(p) {
+	enabled = true;
+	d       = new TextToSpeechPrivate();
+}
 
-	const QIcon &getTalkingIcon(Settings::TalkState talkState) const;
+TextToSpeech::~TextToSpeech() {
+	delete d;
+}
 
-	virtual void updateTalkingIcon();
+void TextToSpeech::say(const QString &text) {
+	if (enabled)
+		d->say(text);
+}
 
-public:
-	struct UserStatus {
-		bool muted, selfMuted, localMuted, deafened, selfDeafened;
-	};
+void TextToSpeech::setEnabled(bool e) {
+	enabled = e;
+}
 
-	TalkingUIUser(const ClientUser &user);
-	virtual ~TalkingUIUser() override;
+void TextToSpeech::setVolume(int volume) {
+	d->setVolume(volume);
+}
 
-	virtual QWidget *getWidget() override;
-	virtual const QWidget *getWidget() const override;
-
-	virtual EntryType getType() const override;
-
-	virtual QString getName() const;
-
-	virtual int compare(const TalkingUIEntry &other) const override;
-
-	virtual void setTalkingState(Settings::TalkState talkState);
-
-	virtual void setIconSize(int size) override;
-
-	virtual void setDisplayString(const QString &displayString) override;
-
-	virtual void setLifeTime(unsigned int time);
-	virtual void restrictLifetime(bool restrict);
-
-	virtual void setStatus(UserStatus status);
-};
-
-
-class TalkingUIChannelListener : public TalkingUIEntry {
-protected:
-	QWidget *m_backgroundWidget = nullptr;
-
-	QLabel *m_icon      = nullptr;
-	QLabel *m_nameLabel = nullptr;
-
-	int m_channelID;
-	QString m_name;
-
-public:
-	TalkingUIChannelListener(const ClientUser &user, const Channel &channel);
-	virtual ~TalkingUIChannelListener() override;
-
-	virtual EntryType getType() const override;
-
-	virtual QWidget *getWidget() override;
-	virtual const QWidget *getWidget() const override;
-
-	virtual void setIconSize(int size) override;
-
-	virtual void setDisplayString(const QString &displayString) override;
-
-	virtual int getAssociatedChannelID() const;
-};
-
-#endif // MUMBLE_MUMBLE_TALKINGUIENTRY_H_
+bool TextToSpeech::isEnabled() const {
+	return enabled;
+}
