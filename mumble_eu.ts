b964f60273d271e1529e21166933581769e1113e
Jan Klass <kissaki@posteo.de>
2020-12-19 18:36:44 +0100

    New translations UserLocalVolumeDialog.ui (Basque)

--- mumble_eu.ts
+++ mumble_eu.ts
@@ -1,308 +1,106 @@
-// Copyright 2005-2020 The Mumble Developers. All rights reserved.
-// Use of this source code is governed by a BSD-style license
-// that can be found in the LICENSE file at the root of the
-// Mumble source tree or at <https://www.mumble.info/LICENSE>.
-
-#include "Zeroconf.h"
-
-#define SYMBOL_EXISTS(symbol) (GetProcAddress(handle, symbol))
-
-Zeroconf::Zeroconf() : m_ok(false) {
-	qRegisterMetaType< uint16_t >("uint16_t");
-	qRegisterMetaType< BonjourRecord >("BonjourRecord");
-	qRegisterMetaType< QList< BonjourRecord > >("QList<BonjourRecord>");
-#ifdef Q_OS_WIN64
-	static bool winDnsUnavailable = false;
-	if (!winDnsUnavailable) {
-		auto handle = GetModuleHandle(L"dnsapi.dll");
-		if (handle) {
-			if (SYMBOL_EXISTS("DnsServiceBrowse") && SYMBOL_EXISTS("DnsServiceBrowseCancel")
-				&& SYMBOL_EXISTS("DnsServiceResolve") && SYMBOL_EXISTS("DnsServiceResolveCancel")
-				&& SYMBOL_EXISTS("DnsServiceFreeInstance")) {
-				m_ok = true;
-				return;
-			}
-		}
-
-		winDnsUnavailable = true;
-		qWarning("Zeroconf: Native mDNS/DNS-SD API not available, falling back to third-party API");
-	}
-
-	static bool dnssdLoadFailed = false;
-	if (!dnssdLoadFailed) {
-		auto handle = LoadLibrary(L"dnssd.DLL");
-		if (!handle) {
-			dnssdLoadFailed = true;
-			qWarning("Zeroconf: Failed to load dnssd.dll, assuming third-party API is not available");
-			return;
-		}
-		FreeLibrary(handle);
-	}
-#endif
-	resetHelperBrowser();
-	resetHelperResolver();
-
-	m_ok = true;
-}
-
-Zeroconf::~Zeroconf() {
-	if (!m_helperBrowser) {
-		stopBrowser();
-	}
-
-	if (!m_helperResolver) {
-		cleanupResolvers();
-	}
-}
-
-void Zeroconf::resetHelperBrowser() {
-	m_helperBrowser.reset(new BonjourServiceBrowser(this));
-	connect(m_helperBrowser.get(), &BonjourServiceBrowser::currentBonjourRecordsChanged, this,
-			&Zeroconf::helperBrowserRecordsChanged);
-	connect(m_helperBrowser.get(), &BonjourServiceBrowser::error, this, &Zeroconf::helperBrowserError);
-}
-
-void Zeroconf::resetHelperResolver() {
-	m_helperResolver.reset(new BonjourServiceResolver(this));
-	connect(m_helperResolver.get(), &BonjourServiceResolver::bonjourRecordResolved, this,
-			&Zeroconf::helperResolverRecordResolved);
-	connect(m_helperResolver.get(), &BonjourServiceResolver::error, this, &Zeroconf::helperResolverError);
-}
-
-bool Zeroconf::startBrowser(const QString &serviceType) {
-	if (!m_ok) {
-		return false;
-	}
-
-	stopBrowser();
-
-	if (m_helperBrowser) {
-		m_helperBrowser->browseForServiceType(serviceType);
-		return true;
-	}
-#ifdef Q_OS_WIN64
-	const QString queryName = serviceType + QLatin1String(".local");
-
-	DNS_SERVICE_BROWSE_REQUEST request{};
-	request.Version         = DNS_QUERY_REQUEST_VERSION1;
-	request.QueryName       = reinterpret_cast< LPCWSTR >(queryName.utf16());
-	request.pBrowseCallback = callbackBrowseComplete;
-	request.pQueryContext   = this;
-
-	m_cancelBrowser.reset(new DNS_SERVICE_CANCEL{});
-	const auto ret = DnsServiceBrowse(&request, m_cancelBrowser.get());
-	if (ret == DNS_REQUEST_PENDING) {
-		return true;
-	}
-
-	m_cancelBrowser.reset();
-	qWarning("Zeroconf: DnsServiceBrowse() failed with error %u!", ret);
-#endif
-	return false;
-}
-
-bool Zeroconf::stopBrowser() {
-	if (!m_ok) {
-		return false;
-	}
-
-	if (m_helperBrowser) {
-		resetHelperBrowser();
-		return true;
-	}
-#ifdef Q_OS_WIN64
-	if (m_cancelBrowser) {
-		const auto ret = DnsServiceBrowseCancel(m_cancelBrowser.get());
-		if (ret == ERROR_SUCCESS || ret == ERROR_CANCELLED) {
-			m_cancelBrowser.reset();
-			return true;
-		}
-
-		qWarning("Zeroconf: DnsServiceBrowseCancel() failed with error %u!", ret);
-		return false;
-	}
-#endif
-	return true;
-}
-
-bool Zeroconf::startResolver(const BonjourRecord &record) {
-	if (!m_ok) {
-		return false;
-	}
-
-	if (m_helperResolver) {
-		m_helperResolver->resolveBonjourRecord(record);
-		return true;
-	}
-#ifdef Q_OS_WIN64
-	stopResolver(record);
-
-	auto qualifiedHostname = record.serviceName.toStdWString() + L"." + record.registeredType.toStdWString()
-							 + record.replyDomain.toStdWString();
-
-	m_resolvers.append(Resolver(this, record));
-	auto context = &m_resolvers.last();
-
-	DNS_SERVICE_RESOLVE_REQUEST request{};
-	request.Version                    = DNS_QUERY_REQUEST_VERSION1;
-	request.QueryName                  = &qualifiedHostname[0];
-	request.pResolveCompletionCallback = &Zeroconf::callbackResolveComplete;
-	request.pQueryContext              = context;
-
-	const auto ret = DnsServiceResolve(&request, &context->m_cancel);
-	if (ret == DNS_REQUEST_PENDING) {
-		return true;
-	}
-
-	m_resolvers.removeLast();
-
-	qWarning("Zeroconf: DnsServiceResolve() failed with error %u!", ret);
-#endif
-	return false;
-}
-#ifdef Q_OS_WIN64
-bool Zeroconf::stopResolver(const BonjourRecord &record) {
-	if (!m_ok) {
-		return false;
-	}
-
-	Resolver tmp(this, record);
-	if (!m_resolvers.contains(tmp)) {
-		return true;
-	}
-
-	auto resolver = m_resolvers[m_resolvers.indexOf(tmp)];
-	return stopResolver(resolver);
-}
-
-bool Zeroconf::stopResolver(Resolver &resolver) {
-	if (!m_ok) {
-		return false;
-	}
-
-	const auto ret = DnsServiceResolveCancel(&resolver.m_cancel);
-	if (ret == ERROR_SUCCESS || ret == ERROR_CANCELLED) {
-		return true;
-	}
-
-	qWarning("Zeroconf: DnsServiceResolveCancel() failed with error %u!", ret);
-	return false;
-}
-#endif
-bool Zeroconf::cleanupResolvers() {
-	if (!m_ok) {
-		return false;
-	}
-
-	if (m_helperResolver) {
-		resetHelperResolver();
-		return true;
-	}
-
-	auto result = true;
-#ifdef Q_OS_WIN64
-	for (auto i = 0; i < m_resolvers.size(); ++i) {
-		if (!stopResolver(m_resolvers[i])) {
-			result = false;
-		}
-	}
-
-	m_resolvers.clear();
-#endif
-	return result;
-}
-
-void Zeroconf::helperBrowserRecordsChanged(const QList< BonjourRecord > &records) {
-	emit recordsChanged(records);
-}
-
-void Zeroconf::helperResolverRecordResolved(const BonjourRecord record, const QString hostname, const int port) {
-	emit recordResolved(record, hostname, port);
-}
-
-void Zeroconf::helperBrowserError(const DNSServiceErrorType error) const {
-	qWarning("Zeroconf: Third-party browser API reports error %d", error);
-}
-
-void Zeroconf::helperResolverError(const BonjourRecord record, const DNSServiceErrorType error) {
-	qWarning("Zeroconf: Third-party resolver API reports error %d", error);
-	emit resolveError(record);
-}
-#ifdef Q_OS_WIN64
-void WINAPI Zeroconf::callbackBrowseComplete(const DWORD status, void *context, DNS_RECORD *records) {
-	auto zeroconf = static_cast< Zeroconf * >(context);
-	zeroconf->m_cancelBrowser.reset();
-
-	if (status != ERROR_SUCCESS) {
-		if (records) {
-			DnsRecordListFree(records, DnsFreeRecordList);
-		}
-
-		if (status != ERROR_CANCELLED) {
-			qWarning("Zeroconf: DnsServiceBrowse() reports status code %u, ignoring results", status);
-		}
-
-		return;
-	}
-
-	if (!records) {
-		return;
-	}
-
-	bool changed = false;
-
-	for (auto cur = records; cur; cur = cur->pNext) {
-		if (cur->wType != DNS_TYPE_PTR) {
-			continue;
-		}
-
-		// Example: "_mumble._tcp.local".
-		const auto domain = QString::fromWCharArray(cur->pName);
-		// Example: "Test._mumble._tcp.local".
-		const auto hostname = QString::fromWCharArray(cur->Data.PTR.pNameHost);
-
-		BonjourRecord record;
-		record.serviceName = hostname.left(hostname.lastIndexOf(domain));
-		// Trim trailing ".".
-		record.serviceName.resize(record.serviceName.size() - 1);
-
-		record.replyDomain    = domain.mid(domain.lastIndexOf('.'));
-		record.registeredType = domain.left(domain.lastIndexOf(record.replyDomain));
-
-		if (!zeroconf->m_records.contains(record)) {
-			zeroconf->m_records.append(record);
-			changed = true;
-		}
-	}
-
-	DnsRecordListFree(records, DnsFreeRecordList);
-
-	if (changed) {
-		emit zeroconf->recordsChanged(zeroconf->m_records);
-	}
-}
-
-void WINAPI Zeroconf::callbackResolveComplete(const DWORD status, void *context, DNS_SERVICE_INSTANCE *instance) {
-	if (status != ERROR_SUCCESS) {
-		if (instance) {
-			DnsServiceFreeInstance(instance);
-		}
-
-		if (status != ERROR_CANCELLED) {
-			qWarning("Zeroconf: DnsServiceResolve() reports status code %u, ignoring result", status);
-		}
-
-		return;
-	}
-
-	if (!instance) {
-		return;
-	}
-
-	auto resolverContext = static_cast< Resolver * >(context);
-	emit resolverContext->m_zeroconf->recordResolved(resolverContext->m_record,
-													 QString::fromWCharArray(instance->pszHostName), instance->wPort);
-
-	DnsServiceFreeInstance(instance);
-}
-#endif
+<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>UserLocalVolumeDialog</class>
+ <widget class="QDialog" name="UserLocalVolumeDialog">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>500</width>
+    <height>224</height>
+   </rect>
+  </property>
+  <property name="minimumSize">
+   <size>
+    <width>500</width>
+    <height>224</height>
+   </size>
+  </property>
+  <property name="maximumSize">
+   <size>
+    <width>16777215</width>
+    <height>224</height>
+   </size>
+  </property>
+  <layout class="QGridLayout">
+   <item row="0" column="0">
+    <widget class="QSlider" name="qsUserLocalVolume">
+     <property name="toolTip">
+      <string>Local volume for other users</string>
+     </property>
+     <property name="whatsThis">
+      <string>&lt;b&gt;Adjust the volume of other users locally&lt;/b&gt;&lt;br /&gt;Mumble supports adjusting the volume of other users locally.</string>
+     </property>
+     <property name="styleSheet">
+      <string notr="true"/>
+     </property>
+     <property name="minimum">
+      <number>-60</number>
+     </property>
+     <property name="maximum">
+      <number>30</number>
+     </property>
+     <property name="value">
+      <number>0</number>
+     </property>
+     <property name="orientation">
+      <enum>Qt::Horizontal</enum>
+     </property>
+     <property name="tickPosition">
+      <enum>QSlider::TicksBelow</enum>
+     </property>
+     <property name="tickInterval">
+      <number>10</number>
+     </property>
+    </widget>
+   </item>
+   <item row="2" column="0" colspan="2" alignment="Qt::AlignBottom">
+    <widget class="QDialogButtonBox" name="qbbUserLocalVolume">
+     <property name="orientation">
+      <enum>Qt::Horizontal</enum>
+     </property>
+     <property name="standardButtons">
+      <set>QDialogButtonBox::Cancel|QDialogButtonBox::Ok|QDialogButtonBox::Reset</set>
+     </property>
+     <property name="centerButtons">
+      <bool>false</bool>
+     </property>
+    </widget>
+   </item>
+   <item row="0" column="1">
+    <widget class="QSpinBox" name="qsbUserLocalVolume">
+     <property name="toolTip">
+      <string>Local volume for other users</string>
+     </property>
+     <property name="whatsThis">
+      <string>&lt;b&gt;Adjust the volume of other users locally&lt;/b&gt;&lt;br /&gt;Mumble supports adjusting the volume of other users locally.</string>
+     </property>
+     <property name="suffix">
+      <string> dB</string>
+     </property>
+     <property name="minimum">
+      <number>-60</number>
+     </property>
+     <property name="maximum">
+      <number>30</number>
+     </property>
+    </widget>
+   </item>
+   <item row="1" column="0" colspan="2">
+    <widget class="QLabel" name="qlUserLocalVolume">
+     <property name="text">
+      <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Use the slider or the text box to change the volume of the user.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot;font-weight:600;&quot;&gt;Attention!&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Increasing the volume of a user too much can permanently damage your hearing. It may also increase the background noise of the user.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+     </property>
+     <property name="alignment">
+      <set>Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter</set>
+     </property>
+     <property name="wordWrap">
+      <bool>true</bool>
+     </property>
+    </widget>
+   </item>
+  </layout>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>
