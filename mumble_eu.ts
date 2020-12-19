37adb10664761aedd2134aeb591e6111aa86565a
Jan Klass <kissaki@posteo.de>
2020-12-19 18:37:52 +0100

    New translations Themes.h (Basque)

--- mumble_eu.ts
+++ mumble_eu.ts
@@ -3,31 +3,66 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_USAGE_H_
-#define MUMBLE_MUMBLE_USAGE_H_
+#ifndef MUMBLE_MUMBLE_THEMES_H_
+#define MUMBLE_MUMBLE_THEMES_H_
 
-#include <QtCore/QBuffer>
-#include <QtCore/QDataStream>
-#include <QtCore/QObject>
+#include <Settings.h>
+#include <ThemeInfo.h>
+#ifndef Q_MOC_RUN
+#	include <boost/optional.hpp>
+#endif
 
-class ClientUser;
-
-/// This class is used to send usage information to the mumble.info website.
-///
-/// During instantiation of this class a ten minute timer will be started. After that
-/// the object's registerUsage function will be called that sends the respective
-/// information to the server.
-class Usage : public QObject {
-	Q_OBJECT
-protected:
-	QBuffer qbReport;
-	QDataStream qdsReport;
-
+class Themes {
 public:
-	Usage(QObject *p = nullptr);
-public slots:
-	/// Performs the actual registration.
-	void registerUsage();
+	/// Returns the style configured in the given settings structure
+	static boost::optional< ThemeInfo::StyleInfo > getConfiguredStyle(const Settings &settings);
+
+	/// Updates the given settings object to be configured to the given style
+	///
+	/// @note Does not apply the theme @see apply
+	///
+	/// @param settings Settings object to update
+	/// @param style Style to set
+	/// @param outChanged Will be set to true if the style in settings actually changed. Will not be changed otherwise.
+	static void setConfiguredStyle(Settings &settings, boost::optional< ThemeInfo::StyleInfo > style, bool &outChanged);
+
+	/// Applies the theme
+	///
+	/// @note Can only apply a theme before MainWindow etc. is opened
+	static bool apply();
+
+	/// Return a theme name to theme map
+	static ThemeMap getThemes();
+
+	/// Returns the per user themes directory
+	static QDir getUserThemesDirectory();
+
+private:
+	/// Applies the fallback stylesheet
+	static void applyFallback();
+
+	/// Tries to apply the configured theme.
+	/// @return True on success. False on failure.
+	static bool applyConfigured();
+
+	// Sets the theme to a QSS theme
+	static void setTheme(QString &themeQss, QStringList &skinPaths);
+
+	/// Returns list of theme search directories ordered ascending by priorty (lowest first)
+	static QVector< QDir > getSearchDirectories();
+
+	/// Returns default style-sheet used for fall-backs
+	static QString getDefaultStylesheet();
+
+	/// userStylesheetPath returns the absolute path to the
+	/// user.qss file.
+	static QString userStylesheetPath();
+
+	/// readStylesheet fills stylesheetContent with the content
+	/// of the file at stylesheetFn, if available.
+	/// If a the file is is available, the function returns true.
+	/// If no file is available, it returns false.
+	static bool readStylesheet(const QString &stylesheetFn, QString &stylesheetContent);
 };
 
-#endif
+#endif // MUMBLE_MUMBLE_THEMES_H_
