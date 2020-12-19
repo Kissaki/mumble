b78a80590a7951f249f1d406105f2cc750497694
Jan Klass <kissaki@posteo.de>
2020-12-19 18:59:05 +0100

    New translations Themes.cpp (Russian)

--- mumble_ru.ts
+++ mumble_ru.ts
@@ -1,236 +1,151 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>UserEdit</class>
- <widget class="QDialog" name="UserEdit">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>542</width>
-    <height>470</height>
-   </rect>
-  </property>
-  <property name="minimumSize">
-   <size>
-    <width>381</width>
-    <height>470</height>
-   </size>
-  </property>
-  <property name="windowTitle">
-   <string/>
-  </property>
-  <layout class="QGridLayout" name="gridLayout_2">
-   <item row="1" column="0">
-    <widget class="QDialogButtonBox" name="qbbButtons">
-     <property name="sizePolicy">
-      <sizepolicy hsizetype="Expanding" vsizetype="Minimum">
-       <horstretch>0</horstretch>
-       <verstretch>0</verstretch>
-      </sizepolicy>
-     </property>
-     <property name="orientation">
-      <enum>Qt::Horizontal</enum>
-     </property>
-     <property name="standardButtons">
-      <set>QDialogButtonBox::Cancel|QDialogButtonBox::Ok</set>
-     </property>
-    </widget>
-   </item>
-   <item row="0" column="0">
-    <widget class="QGroupBox" name="qgbUserList">
-     <property name="title">
-      <string>Registered Users</string>
-     </property>
-     <layout class="QGridLayout" name="gridLayout">
-      <item row="2" column="0">
-       <widget class="QPushButton" name="qpbRename">
-        <property name="enabled">
-         <bool>false</bool>
-        </property>
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Minimum" vsizetype="Fixed">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-        <property name="text">
-         <string>Rename</string>
-        </property>
-       </widget>
-      </item>
-      <item row="2" column="1">
-       <widget class="QPushButton" name="qpbRemove">
-        <property name="enabled">
-         <bool>false</bool>
-        </property>
-        <property name="text">
-         <string>Remove</string>
-        </property>
-        <property name="flat">
-         <bool>false</bool>
-        </property>
-       </widget>
-      </item>
-      <item row="1" column="0" colspan="6">
-       <widget class="QLineEdit" name="qlSearch">
-        <property name="font">
-         <font>
-          <italic>true</italic>
-         </font>
-        </property>
-        <property name="text">
-         <string/>
-        </property>
-        <property name="alignment">
-         <set>Qt::AlignCenter</set>
-        </property>
-        <property name="placeholderText">
-         <string>Who are you looking for?</string>
-        </property>
-       </widget>
-      </item>
-      <item row="2" column="5">
-       <widget class="MUComboBox" name="qcbInactive">
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Minimum" vsizetype="Fixed">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-        <item>
-         <property name="text">
-          <string>Days</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>Weeks</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>Months</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>Years</string>
-         </property>
-        </item>
-       </widget>
-      </item>
-      <item row="2" column="2">
-       <spacer name="horizontalSpacer">
-        <property name="orientation">
-         <enum>Qt::Horizontal</enum>
-        </property>
-        <property name="sizeHint" stdset="0">
-         <size>
-          <width>40</width>
-          <height>20</height>
-         </size>
-        </property>
-       </spacer>
-      </item>
-      <item row="2" column="4">
-       <widget class="QSpinBox" name="qsbInactive">
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="MinimumExpanding" vsizetype="Fixed">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-        <property name="maximumSize">
-         <size>
-          <width>48</width>
-          <height>16777215</height>
-         </size>
-        </property>
-        <property name="frame">
-         <bool>true</bool>
-        </property>
-        <property name="maximum">
-         <number>9999</number>
-        </property>
-       </widget>
-      </item>
-      <item row="2" column="3">
-       <widget class="QLabel" name="qlInactive">
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Minimum" vsizetype="Preferred">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-        <property name="text">
-         <string>Inactive for</string>
-        </property>
-       </widget>
-      </item>
-      <item row="0" column="0" colspan="6">
-       <widget class="QTreeView" name="qtvUserList">
-        <property name="alternatingRowColors">
-         <bool>true</bool>
-        </property>
-        <property name="selectionMode">
-         <enum>QAbstractItemView::ExtendedSelection</enum>
-        </property>
-        <property name="rootIsDecorated">
-         <bool>false</bool>
-        </property>
-        <property name="itemsExpandable">
-         <bool>false</bool>
-        </property>
-        <property name="sortingEnabled">
-         <bool>true</bool>
-        </property>
-       </widget>
-      </item>
-     </layout>
-    </widget>
-   </item>
-  </layout>
- </widget>
- <resources/>
- <connections>
-  <connection>
-   <sender>qbbButtons</sender>
-   <signal>accepted()</signal>
-   <receiver>UserEdit</receiver>
-   <slot>accept()</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>248</x>
-     <y>254</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>157</x>
-     <y>274</y>
-    </hint>
-   </hints>
-  </connection>
-  <connection>
-   <sender>qbbButtons</sender>
-   <signal>rejected()</signal>
-   <receiver>UserEdit</receiver>
-   <slot>reject()</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>316</x>
-     <y>260</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>286</x>
-     <y>274</y>
-    </hint>
-   </hints>
-  </connection>
- </connections>
- <customwidgets>
-  <customwidget>
-   <class>MUComboBox</class>
-   <extends>QComboBox</extends>
-   <header>widgets/MUComboBox.h</header>
-  </customwidget>
- </customwidgets>
-</ui>
+// Copyright 2005-2020 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#include "Themes.h"
+#include "MainWindow.h"
+#include "MumbleApplication.h"
+
+// We define a global macro called 'g'. This can lead to issues when included code uses 'g' as a type or parameter name
+// (like protobuf 3.7 does). As such, for now, we have to make this our last include.
+#include "Global.h"
+
+boost::optional< ThemeInfo::StyleInfo > Themes::getConfiguredStyle(const Settings &settings) {
+	if (settings.themeName.isEmpty() && settings.themeStyleName.isEmpty()) {
+		return boost::none;
+	}
+
+	const ThemeMap themes            = getThemes();
+	ThemeMap::const_iterator themeIt = themes.find(settings.themeName);
+	if (themeIt == themes.end()) {
+		qWarning() << "Could not find configured theme" << settings.themeName;
+		return boost::none;
+	}
+
+	ThemeInfo::StylesMap::const_iterator styleIt = themeIt->styles.find(settings.themeStyleName);
+	if (styleIt == themeIt->styles.end()) {
+		qWarning() << "Configured theme" << settings.themeName << "does not have configured style"
+				   << settings.themeStyleName;
+		return boost::none;
+	}
+
+	return *styleIt;
+}
+
+void Themes::setConfiguredStyle(Settings &settings, boost::optional< ThemeInfo::StyleInfo > style, bool &outChanged) {
+	if (style) {
+		if (settings.themeName != style->themeName || settings.themeStyleName != style->name) {
+			settings.themeName      = style->themeName;
+			settings.themeStyleName = style->name;
+			outChanged              = true;
+		}
+	} else {
+		if (!settings.themeName.isEmpty() || !settings.themeStyleName.isEmpty()) {
+			settings.themeName = settings.themeStyleName = QString();
+			outChanged                                   = true;
+		}
+	}
+}
+
+void Themes::applyFallback() {
+	qWarning() << "Applying fallback style sheet";
+
+	QStringList skinPaths;
+	skinPaths << QLatin1String(":/themes/Mumble");
+	QString defaultTheme = getDefaultStylesheet();
+	setTheme(defaultTheme, skinPaths);
+}
+
+bool Themes::applyConfigured() {
+	boost::optional< ThemeInfo::StyleInfo > style = Themes::getConfiguredStyle(g.s);
+	if (!style) {
+		return false;
+	}
+
+	const QFileInfo qssFile(style->getPlatformQss());
+
+	qWarning() << "Theme:" << style->themeName;
+	qWarning() << "Style:" << style->name;
+	qWarning() << "--> qss:" << qssFile.absoluteFilePath();
+
+	QFile file(qssFile.absoluteFilePath());
+	if (!file.open(QFile::ReadOnly)) {
+		qWarning() << "Failed to open theme stylesheet:" << file.errorString();
+		return false;
+	}
+
+	QStringList skinPaths;
+	skinPaths << qssFile.path();
+	skinPaths << QLatin1String(":/themes/Mumble"); // Some skins might want to fall-back on our built-in resources
+
+	QString themeQss = QString::fromUtf8(file.readAll());
+	setTheme(themeQss, skinPaths);
+	return true;
+}
+
+void Themes::setTheme(QString &themeQss, QStringList &skinPaths) {
+	QDir::setSearchPaths(QLatin1String("skin"), skinPaths);
+
+	QString userStylesheetFn = userStylesheetPath();
+	QString userStylesheetContent;
+	if (readStylesheet(userStylesheetFn, userStylesheetContent)) {
+		qWarning("Themes: allowing user stylesheet at '%s' to override the stylesheet", qPrintable(userStylesheetFn));
+	}
+
+	qApp->setStyleSheet(themeQss + QLatin1String("\n") + userStylesheetContent);
+}
+
+bool Themes::apply() {
+	const bool result = applyConfigured();
+	if (!result) {
+		applyFallback();
+	}
+
+	if (g.mw) {
+		g.mw->qteLog->document()->setDefaultStyleSheet(qApp->styleSheet());
+	}
+	return result;
+}
+
+ThemeMap Themes::getThemes() {
+	return ThemeInfo::scanDirectories(getSearchDirectories());
+}
+
+QDir Themes::getUserThemesDirectory() {
+	return QDir(g.qdBasePath.absolutePath() + QLatin1String("/Themes"));
+}
+
+QVector< QDir > Themes::getSearchDirectories() {
+	QVector< QDir > themeSearchDirectories;
+
+	// Built-in themes contained in the binary have the lowest priority
+	themeSearchDirectories << QDir(QLatin1String(":themes"));
+	// Next come themes found in the applications Themes directory
+	themeSearchDirectories << QDir(MumbleApplication::instance()->applicationVersionRootPath()
+								   + QLatin1String("/Themes"));
+	// Highest priorty have themes located in the user directory
+	themeSearchDirectories << getUserThemesDirectory();
+
+	return themeSearchDirectories;
+}
+
+QString Themes::userStylesheetPath() {
+	return g.qdBasePath.absolutePath() + QLatin1String("/user.qss");
+}
+
+bool Themes::readStylesheet(const QString &stylesheetFn, QString &stylesheetContent) {
+	QFile file(stylesheetFn);
+	if (!file.open(QFile::ReadOnly)) {
+		stylesheetContent = QString();
+		return false;
+	}
+
+	stylesheetContent = QString::fromUtf8(file.readAll());
+	return true;
+}
+
+QString Themes::getDefaultStylesheet() {
+	return QLatin1String(".log-channel{text-decoration:none;}.log-user{text-decoration:none;}p{margin:0;}#qwMacWarning,"
+						 "#qwInlineNotice{background-color:#FFFEDC;border-radius:5px;border:1px solid #B5B59E;}");
+}
