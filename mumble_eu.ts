474f1415f57c5c1d5a466f3c178c569b8fdcf9ae
Jan Klass <kissaki@posteo.de>
2020-12-19 18:33:48 +0100

    New translations G15LCDEngine_unix.cpp (Basque)

--- mumble_eu.ts
+++ mumble_eu.ts
@@ -1,95 +1,79 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>RichTextEditorLink</class>
- <widget class="QDialog" name="RichTextEditorLink">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>295</width>
-    <height>95</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>Add Link</string>
-  </property>
-  <layout class="QGridLayout" name="gridLayout">
-   <item row="0" column="0">
-    <widget class="QLabel" name="qlUrl">
-     <property name="text">
-      <string>URL</string>
-     </property>
-     <property name="buddy">
-      <cstring>qleUrl</cstring>
-     </property>
-    </widget>
-   </item>
-   <item row="0" column="1">
-    <widget class="QLineEdit" name="qleUrl"/>
-   </item>
-   <item row="1" column="0">
-    <widget class="QLabel" name="qlText">
-     <property name="text">
-      <string>Text</string>
-     </property>
-     <property name="buddy">
-      <cstring>qleText</cstring>
-     </property>
-    </widget>
-   </item>
-   <item row="1" column="1">
-    <widget class="QLineEdit" name="qleText"/>
-   </item>
-   <item row="2" column="0" colspan="2">
-    <widget class="QDialogButtonBox" name="buttonBox">
-     <property name="orientation">
-      <enum>Qt::Horizontal</enum>
-     </property>
-     <property name="standardButtons">
-      <set>QDialogButtonBox::Cancel|QDialogButtonBox::Ok</set>
-     </property>
-    </widget>
-   </item>
-  </layout>
- </widget>
- <tabstops>
-  <tabstop>qleUrl</tabstop>
-  <tabstop>qleText</tabstop>
-  <tabstop>buttonBox</tabstop>
- </tabstops>
- <resources/>
- <connections>
-  <connection>
-   <sender>buttonBox</sender>
-   <signal>accepted()</signal>
-   <receiver>RichTextEditorLink</receiver>
-   <slot>accept()</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>227</x>
-     <y>228</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>157</x>
-     <y>274</y>
-    </hint>
-   </hints>
-  </connection>
-  <connection>
-   <sender>buttonBox</sender>
-   <signal>rejected()</signal>
-   <receiver>RichTextEditorLink</receiver>
-   <slot>reject()</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>295</x>
-     <y>234</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>286</x>
-     <y>274</y>
-    </hint>
-   </hints>
-  </connection>
- </connections>
-</ui>
+// Copyright 2005-2020 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#include <G15LCDEngine_unix.h>
+
+static LCDEngine *G15LCDEngineNew() {
+	return new G15LCDEngineUnix();
+}
+
+static LCDEngineRegistrar registrar(G15LCDEngineNew);
+
+G15LCDEngineUnix::G15LCDEngineUnix() {
+	sock = new_g15_screen(G15_PIXELBUF);
+	if (sock < 0) {
+		qWarning("G15LCDEngineUnix: Unable to connect to G15Daemon.");
+		return;
+	}
+
+	qlDevices << new G15LCDDeviceUnix(this);
+}
+
+G15LCDEngineUnix::~G15LCDEngineUnix() {
+}
+
+QList< LCDDevice * > G15LCDEngineUnix::devices() const {
+	return qlDevices;
+}
+
+/* -- */
+
+G15LCDDeviceUnix::G15LCDDeviceUnix(G15LCDEngineUnix *e) : LCDDevice() {
+	bEnabled = false;
+	engine   = e;
+}
+
+G15LCDDeviceUnix::~G15LCDDeviceUnix() {
+}
+
+bool G15LCDDeviceUnix::enabled() {
+	return bEnabled;
+}
+
+void G15LCDDeviceUnix::setEnabled(bool b) {
+	bEnabled = b;
+}
+
+void G15LCDDeviceUnix::blitImage(QImage *img, bool) {
+	Q_ASSERT(img);
+
+	const unsigned int len = 6880;
+	uchar buf[len];
+	uchar *tmp = img->bits();
+
+	for (unsigned int i = 0; i < len / 8; ++i) {
+		unsigned int idx = i * 8;
+		buf[idx + 7]     = tmp[i] & 0x80 ? 1 : 0;
+		buf[idx + 6]     = tmp[i] & 0x40 ? 1 : 0;
+		buf[idx + 5]     = tmp[i] & 0x20 ? 1 : 0;
+		buf[idx + 4]     = tmp[i] & 0x10 ? 1 : 0;
+		buf[idx + 3]     = tmp[i] & 0x08 ? 1 : 0;
+		buf[idx + 2]     = tmp[i] & 0x04 ? 1 : 0;
+		buf[idx + 1]     = tmp[i] & 0x02 ? 1 : 0;
+		buf[idx + 0]     = tmp[i] & 0x01 ? 1 : 0;
+	}
+
+	int ret = g15_send(engine->sock, reinterpret_cast< char * >(buf), len);
+	if (ret < 0)
+		qWarning("G15LCDDeviceUnix: Unable to g15_send().");
+}
+
+QString G15LCDDeviceUnix::name() const {
+	return QString::fromLatin1("Logitech Gamepanel (G15Daemon)");
+}
+
+QSize G15LCDDeviceUnix::size() const {
+	return QSize(G15_WIDTH, G15_HEIGHT);
+}
