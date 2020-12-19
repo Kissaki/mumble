03633922eef00f79575d59bf490c02f340349169
Jan Klass <kissaki@posteo.de>
2020-12-19 18:01:31 +0100

    New translations AudioOutputSpeech.h (French)

--- mumble_fr.ts
+++ mumble_fr.ts
@@ -3,34 +3,79 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_AUDIOOUTPUTUSER_H_
-#define MUMBLE_MUMBLE_AUDIOOUTPUTUSER_H_
+#ifndef MUMBLE_MUMBLE_AUDIOOUTPUTSPEECH_H_
+#define MUMBLE_MUMBLE_AUDIOOUTPUTSPEECH_H_
 
-#include <QtCore/QObject>
+#include <celt.h>
+#include <speex/speex.h>
+#include <speex/speex_jitter.h>
+#include <speex/speex_resampler.h>
 
-class AudioOutputUser : public QObject {
+#include <QtCore/QMutex>
+
+#include "AudioOutputUser.h"
+#include "Message.h"
+
+class CELTCodec;
+class OpusCodec;
+class ClientUser;
+struct OpusDecoder;
+
+class AudioOutputSpeech : public AudioOutputUser {
 private:
 	Q_OBJECT
-	Q_DISABLE_COPY(AudioOutputUser)
+	Q_DISABLE_COPY(AudioOutputSpeech)
 protected:
-	unsigned int iBufferSize;
+	unsigned int iAudioBufferSize;
+	unsigned int iBufferOffset;
+	unsigned int iBufferFilled;
+	unsigned int iOutputSize;
+	unsigned int iLastConsume;
+	unsigned int iFrameSize;
+	unsigned int iFrameSizePerChannel;
+	unsigned int iSampleRate;
+	unsigned int iMixerFreq;
+	bool bLastAlive;
+	bool bHasTerminator;
 
-	/// Used to resize the buffer.
-	/// WARNING:
-	///          Audio callback is a dedicated place that can be executed
-	///          in a special thread or interrupt handler. Allocating
-	///          memory will probably crash the program!
-	void resizeBuffer(unsigned int newsize);
+	float *fFadeIn;
+	float *fFadeOut;
+	float *fResamplerBuffer;
+
+	SpeexResamplerState *srs;
+
+	QMutex qmJitter;
+	JitterBuffer *jbJitter;
+	int iMissCount;
+
+	CELTCodec *cCodec;
+	CELTDecoder *cdDecoder;
+
+	OpusCodec *oCodec;
+	OpusDecoder *opusState;
+
+	SpeexBits sbBits;
+	void *dsSpeex;
+
+	QList< QByteArray > qlFrames;
 
 public:
-	AudioOutputUser(const QString &name);
-	~AudioOutputUser() Q_DECL_OVERRIDE;
-	const QString qsName;
-	float *pfBuffer = nullptr;
-	float *pfVolume = nullptr;
-	float fPos[3]   = { 0.0, 0.0, 0.0 };
-	bool bStereo;
-	virtual bool prepareSampleBuffer(unsigned int snum) = 0;
+	unsigned char ucFlags;
+	MessageHandler::UDPMessageType umtType;
+	int iMissedFrames;
+	ClientUser *p;
+
+	/// Fetch and decode frames from the jitter buffer. Called in mix().
+	///
+	/// @param frameCount Number of frames to decode. frame means a bundle of one sample from each channel.
+	virtual bool prepareSampleBuffer(unsigned int frameCount) Q_DECL_OVERRIDE;
+
+	void addFrameToBuffer(const QByteArray &, unsigned int iBaseSeq);
+
+	/// @param systemMaxBufferSize maximum number of samples the system audio play back may request each time
+	AudioOutputSpeech(ClientUser *, unsigned int freq, MessageHandler::UDPMessageType type,
+					  unsigned int systemMaxBufferSize);
+	~AudioOutputSpeech() Q_DECL_OVERRIDE;
 };
 
-#endif // AUDIOOUTPUTUSER_H_
+#endif // AUDIOOUTPUTSPEECH_H_
