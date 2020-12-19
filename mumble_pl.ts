f46fc489534532fb6f0939a5de195b6e25895715
Jan Klass <kissaki@posteo.de>
2020-12-19 19:10:19 +0100

    New translations UserLocalVolumeDialog.h (Polish)

--- mumble_pl.ts
+++ mumble_pl.ts
@@ -3,28 +3,65 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include "SharedMemory.h"
+/* Copyright (C) 2015, Fredrik Nordin <freedick@ludd.ltu.se>
 
-unsigned int SharedMemory2::uiIndex = 0;
+   All rights reserved.
 
-QString SharedMemory2::name() const {
-	return a_ucData ? qsName : QString();
-}
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
 
-unsigned int SharedMemory2::size() const {
-	return a_ucData ? uiSize : 0;
-}
+   - Redistributions of source code must retain the above copyright notice,
+	 this list of conditions and the following disclaimer.
+   - Redistributions in binary form must reproduce the above copyright notice,
+	 this list of conditions and the following disclaimer in the documentation
+	 and/or other materials provided with the distribution.
+   - Neither the name of the Mumble Developers nor the names of its
+	 contributors may be used to endorse or promote products derived from this
+	 software without specific prior written permission.
 
-unsigned char *SharedMemory2::data() {
-	return a_ucData;
-}
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
-const unsigned char *SharedMemory2::data() const {
-	return a_ucData;
-}
+#ifndef MUMBLE_MUMBLE_USERVOLUME_H_
+#define MUMBLE_MUMBLE_USERVOLUME_H_
 
-void SharedMemory2::erase() {
-	if (a_ucData) {
-		memset(a_ucData, 0, uiSize);
-	}
-}
+#include <QMap>
+
+#include "ClientUser.h"
+#include "ui_UserLocalVolumeDialog.h"
+
+class UserLocalVolumeDialog : public QDialog, private Ui::UserLocalVolumeDialog {
+	Q_OBJECT
+	Q_DISABLE_COPY(UserLocalVolumeDialog);
+
+	/// The session ID for the user that the dialog is changing the volume for.
+	unsigned int m_clientSession;
+
+	/// The user's original adjustment (in dB) when entering the dialog.
+	int m_originalVolumeAdjustmentDecibel;
+	QMap< unsigned int, UserLocalVolumeDialog * > *m_qmUserVolTracker;
+
+public slots:
+	void closeEvent(QCloseEvent *event);
+	void on_qsUserLocalVolume_valueChanged(int value);
+	void on_qsbUserLocalVolume_valueChanged(int value);
+	void on_qbbUserLocalVolume_clicked(QAbstractButton *b);
+	void reject();
+
+public:
+	static void present(unsigned int sessionId, QMap< unsigned int, UserLocalVolumeDialog * > *qmUserVolTracker);
+	UserLocalVolumeDialog(unsigned int sessionId, QMap< unsigned int, UserLocalVolumeDialog * > *qmUserVolTracker);
+};
+
+#endif
