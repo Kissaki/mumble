28e498689c0d12219fdafe0a7838e63ded48707f
Jan Klass <kissaki@posteo.de>
2020-12-19 18:13:43 +0100

    New translations Tokens.h (German)

--- mumble_de.ts
+++ mumble_de.ts
@@ -3,52 +3,25 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include "Tokens.h"
+#ifndef MUMBLE_MUMBLE_TOKENS_H_
+#define MUMBLE_MUMBLE_TOKENS_H_
 
-#include "Database.h"
-#include "ServerHandler.h"
+#include "ui_Tokens.h"
 
-// We define a global macro called 'g'. This can lead to issues when included code uses 'g' as a type or parameter name
-// (like protobuf 3.7 does). As such, for now, we have to make this our last include.
-#include "Global.h"
+class Tokens : public QDialog, public Ui::Tokens {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(Tokens)
+protected:
+	QByteArray qbaDigest;
 
-Tokens::Tokens(QWidget *p) : QDialog(p) {
-	setupUi(this);
-	qlwTokens->setAccessibleName(tr("Tokens"));
+public:
+	Tokens(QWidget *p = nullptr);
+public slots:
+	void accept() Q_DECL_OVERRIDE;
+public slots:
+	void on_qpbAdd_clicked();
+	void on_qpbRemove_clicked();
+};
 
-	qbaDigest          = g.sh->qbaDigest;
-	QStringList tokens = g.db->getTokens(qbaDigest);
-	tokens.sort();
-	foreach (const QString &qs, tokens) {
-		QListWidgetItem *qlwi = new QListWidgetItem(qs);
-		qlwi->setFlags(qlwi->flags() | Qt::ItemIsEditable);
-		qlwTokens->addItem(qlwi);
-	}
-}
-
-void Tokens::accept() {
-	QStringList qsl;
-
-	QList< QListWidgetItem * > items = qlwTokens->findItems(QString(), Qt::MatchStartsWith);
-	foreach (QListWidgetItem *qlwi, items) {
-		const QString &text = qlwi->text().trimmed();
-		if (!text.isEmpty())
-			qsl << text;
-	}
-	g.db->setTokens(qbaDigest, qsl);
-	g.sh->setTokens(qsl);
-	QDialog::accept();
-}
-
-void Tokens::on_qpbAdd_clicked() {
-	QListWidgetItem *qlwi = new QListWidgetItem(tr("Empty Token"));
-	qlwi->setFlags(qlwi->flags() | Qt::ItemIsEditable);
-
-	qlwTokens->addItem(qlwi);
-	qlwTokens->editItem(qlwi);
-}
-
-void Tokens::on_qpbRemove_clicked() {
-	foreach (QListWidgetItem *qlwi, qlwTokens->selectedItems())
-		delete qlwi;
-}
+#endif
