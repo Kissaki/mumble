ef876e15c46789dd2df0eb9ccbc12e2dbc4b911c
Jan Klass <kissaki@posteo.de>
2020-12-19 19:02:31 +0100

    New translations ViewCert.cpp (Swedish)

--- mumble_sv.ts
+++ mumble_sv.ts
@@ -3,111 +3,159 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include "WebFetch.h"
+#include "ViewCert.h"
 
-#include "NetworkConfig.h"
+#include <QtCore/QUrl>
+#include <QtNetwork/QSslKey>
+#include <QtWidgets/QDialogButtonBox>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QListWidget>
+#include <QtWidgets/QVBoxLayout>
 
-#include <QtNetwork/QNetworkReply>
+static QString decode_utf8_qssl_string(const QString &input) {
+	QString i = input;
+	return QUrl::fromPercentEncoding(i.replace(QLatin1String("\\x"), QLatin1String("%")).toLatin1());
+}
 
-// We define a global macro called 'g'. This can lead to issues when included code uses 'g' as a type or parameter name
-// (like protobuf 3.7 does). As such, for now, we have to make this our last include.
-#include "Global.h"
+static QStringList processQSslCertificateInfo(QStringList in) {
+	QStringList list;
+	foreach (QString str, in) { list << decode_utf8_qssl_string(str); }
+	return list;
+}
 
-WebFetch::WebFetch(QString service, QUrl url, QObject *obj, const char *slot)
-	: QObject(), qoObject(obj), cpSlot(slot), m_service(service) {
-	url.setScheme(QLatin1String("https"));
+static void addQSslCertificateInfo(QStringList &l, const QString &label, const QStringList &items) {
+	foreach (const QString &item, items) { l << QString(QLatin1String("%1: %2")).arg(label, item); }
+}
 
-	if (!g.s.qsServicePrefix.isEmpty()) {
-		url.setHost(prefixedServiceHost());
-	} else {
-		url.setHost(serviceHost());
+static QString certificateFriendlyName(const QSslCertificate &cert) {
+	QStringList cnList  = processQSslCertificateInfo(cert.subjectInfo(QSslCertificate::CommonName));
+	QStringList orgList = processQSslCertificateInfo(cert.subjectInfo(QSslCertificate::Organization));
+
+	QString cn;
+	if (cnList.count() > 0) {
+		cn = cnList.at(0);
 	}
 
-	qnr = Network::get(url);
-	connect(qnr, SIGNAL(finished()), this, SLOT(finished()));
-	connect(this, SIGNAL(fetched(QByteArray, QUrl, QMap< QString, QString >)), obj, slot);
-}
-
-QString WebFetch::prefixedServiceHost() const {
-	if (g.s.qsServicePrefix.isEmpty()) {
-		return serviceHost();
+	QString org;
+	if (orgList.count() > 0) {
+		org = orgList.at(0);
 	}
-	return QString::fromLatin1("%1-%2.mumble.info").arg(g.s.qsServicePrefix, m_service);
+
+	return QString(QLatin1String("%1 %2")).arg(cn, org);
 }
 
-QString WebFetch::serviceHost() const {
-	return QString::fromLatin1("%1.mumble.info").arg(m_service);
-}
+ViewCert::ViewCert(QList< QSslCertificate > cl, QWidget *p) : QDialog(p) {
+	qlCerts = cl;
 
-static QString fromUtf8(const QByteArray &qba) {
-	if (qba.isEmpty())
-		return QString();
-	return QString::fromUtf8(qba.constData(), qba.length());
-}
+	setWindowTitle(tr("Certificate Chain Details"));
 
-void WebFetch::finished() {
-	// Note that if this functions succeeds, it should deleteLater() itself, as this is a temporary object.
-	Q_ASSERT(qobject_cast< QNetworkReply * >(sender()) == qnr);
-	qnr->disconnect();
-	qnr->deleteLater();
+	QHBoxLayout *h;
+	QVBoxLayout *v;
+	QGroupBox *qcbChain, *qcbDetails;
 
-	QUrl url = qnr->request().url();
+	qcbChain = new QGroupBox(tr("Certificate chain"), this);
+	h        = new QHBoxLayout(qcbChain);
+	qlwChain = new QListWidget(qcbChain);
+	qlwChain->setObjectName(QLatin1String("Chain"));
 
-	if (qnr->error() == QNetworkReply::NoError) {
-		QByteArray a = qnr->readAll();
-
-		// empty response is not an error
-		if (a.isNull())
-			a.append("");
-
-		QMap< QString, QString > headers;
-
-		foreach (const QByteArray &headerName, qnr->rawHeaderList()) {
-			QString name  = fromUtf8(headerName);
-			QString value = fromUtf8(qnr->rawHeader(headerName));
-			if (!name.isEmpty() && !value.isEmpty()) {
-				headers.insert(name, value);
-				if (name == QLatin1String("Use-Service-Prefix")) {
-					QRegExp servicePrefixRegExp(QLatin1String("^[a-zA-Z]+$"));
-					if (servicePrefixRegExp.exactMatch(value)) {
-						g.s.qsServicePrefix = value.toLower();
-					}
-				}
-			}
+	// load certs into a set as a hacky fix to #2141
+#if QT_VERSION >= 0x050400
+	QSet< QSslCertificate > qlCertSet;
+#else
+	QList< QSslCertificate > qlCertSet;
+#endif
+	foreach (QSslCertificate c, qlCerts) {
+		if (!qlCertSet.contains(c)) {
+			qlwChain->addItem(certificateFriendlyName(c));
+			qlCertSet << c;
 		}
-
-		emit fetched(a, url, headers);
-		deleteLater();
-	} else if (url.host() == prefixedServiceHost() && url.host() != serviceHost()) {
-		// We have tried to fetch from a local service domain (e.g. de-update.mumble.info)
-		// which has failed, so naturally we want to try the non-local one (update.mumble.info)
-		// as well as maybe that one will work.
-		// This of course only makes sense, if prefixedServiceHost() and serviceHost() are in fact
-		// different hosts.
-		url.setHost(serviceHost());
-
-		qnr = Network::get(url);
-		connect(qnr, SIGNAL(finished()), this, SLOT(finished()));
-	} else {
-		emit fetched(QByteArray(), url, QMap< QString, QString >());
-		deleteLater();
 	}
+	h->addWidget(qlwChain);
+
+	qcbDetails = new QGroupBox(tr("Certificate details"), this);
+	h          = new QHBoxLayout(qcbDetails);
+	qlwCert    = new QListWidget(qcbDetails);
+	h->addWidget(qlwCert);
+
+	QDialogButtonBox *qdbb = new QDialogButtonBox(QDialogButtonBox::Ok, Qt::Horizontal, this);
+
+	v = new QVBoxLayout(this);
+	v->addWidget(qcbChain);
+	v->addWidget(qcbDetails);
+	v->addWidget(qdbb);
+
+	QMetaObject::connectSlotsByName(this);
+	connect(qdbb, SIGNAL(accepted()), this, SLOT(accept()));
+
+	resize(510, 300);
 }
 
-/**
- * @brief Fetch URL from mumble servers.
- *
- * If fetching fails, the slot is invoked with a null QByteArray.
- * @param url URL to fetch. Hostname and scheme must be blank.
- * @param obj Object to invoke slot on.
- * @param slot Slot to be triggered, invoked with the signature of \link fetched.
- */
-void WebFetch::fetch(const QString &service, const QUrl &url, QObject *obj, const char *slot) {
-	Q_ASSERT(!service.isEmpty());
-	Q_ASSERT(url.scheme().isEmpty());
-	Q_ASSERT(url.host().isEmpty());
-	Q_ASSERT(obj);
-	Q_ASSERT(slot);
+QString ViewCert::prettifyDigest(QString digest) {
+	QString pretty_digest = digest.toUpper();
+	int step              = 2;
+	QChar separator       = QChar::fromLatin1(':');
+	for (int i = step; i < pretty_digest.size(); i += step + 1) {
+		pretty_digest.insert(i, separator);
+	}
+	return pretty_digest;
+}
 
-	new WebFetch(service, url, obj, slot);
+void ViewCert::on_Chain_currentRowChanged(int idx) {
+	qlwCert->clear();
+	if ((idx < 0) || (idx >= qlCerts.size()))
+		return;
+
+	QStringList l;
+	const QSslCertificate &c = qlCerts.at(idx);
+
+	addQSslCertificateInfo(l, tr("Common Name"),
+						   processQSslCertificateInfo(c.subjectInfo(QSslCertificate::CommonName)));
+	addQSslCertificateInfo(l, tr("Organization"),
+						   processQSslCertificateInfo(c.subjectInfo(QSslCertificate::Organization)));
+	addQSslCertificateInfo(l, tr("Subunit"),
+						   processQSslCertificateInfo(c.subjectInfo(QSslCertificate::OrganizationalUnitName)));
+	addQSslCertificateInfo(l, tr("Country"), processQSslCertificateInfo(c.subjectInfo(QSslCertificate::CountryName)));
+	addQSslCertificateInfo(l, tr("Locality"), processQSslCertificateInfo(c.subjectInfo(QSslCertificate::LocalityName)));
+	addQSslCertificateInfo(l, tr("State"),
+						   processQSslCertificateInfo(c.subjectInfo(QSslCertificate::StateOrProvinceName)));
+	l << tr("Valid from: %1").arg(c.effectiveDate().toString());
+	l << tr("Valid to: %1").arg(c.expiryDate().toString());
+	l << tr("Serial: %1").arg(QString::fromLatin1(c.serialNumber().toHex()));
+	l << tr("Public Key: %1 bits %2")
+			 .arg(c.publicKey().length())
+			 .arg((c.publicKey().algorithm() == QSsl::Rsa) ? tr("RSA") : tr("DSA"));
+	l << tr("Digest (SHA-1): %1").arg(prettifyDigest(QString::fromLatin1(c.digest(QCryptographicHash::Sha1).toHex())));
+	l << tr("Digest (SHA-256): %1")
+			 .arg(prettifyDigest(QString::fromLatin1(c.digest(QCryptographicHash::Sha256).toHex())));
+
+	const QMultiMap< QSsl::AlternativeNameEntryType, QString > &alts = c.subjectAlternativeNames();
+	QMultiMap< QSsl::AlternativeNameEntryType, QString >::const_iterator i;
+
+	for (i = alts.constBegin(); i != alts.constEnd(); ++i) {
+		switch (i.key()) {
+			case QSsl::EmailEntry: {
+				l << tr("Email: %1").arg(i.value());
+			} break;
+			case QSsl::DnsEntry: {
+				l << tr("DNS: %1").arg(i.value());
+			} break;
+			default:
+				break;
+		}
+	}
+
+	l << QString();
+	l << tr("Issued by:");
+	addQSslCertificateInfo(l, tr("Common Name"), processQSslCertificateInfo(c.issuerInfo(QSslCertificate::CommonName)));
+	addQSslCertificateInfo(l, tr("Organization"),
+						   processQSslCertificateInfo(c.issuerInfo(QSslCertificate::Organization)));
+	addQSslCertificateInfo(l, tr("Unit Name"),
+						   processQSslCertificateInfo(c.issuerInfo(QSslCertificate::OrganizationalUnitName)));
+	addQSslCertificateInfo(l, tr("Country"), processQSslCertificateInfo(c.issuerInfo(QSslCertificate::CountryName)));
+	addQSslCertificateInfo(l, tr("Locality"), processQSslCertificateInfo(c.issuerInfo(QSslCertificate::LocalityName)));
+	addQSslCertificateInfo(l, tr("State"),
+						   processQSslCertificateInfo(c.issuerInfo(QSslCertificate::StateOrProvinceName)));
+
+	qlwCert->addItems(l);
 }
