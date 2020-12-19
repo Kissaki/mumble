5f0e055d276f7d97a3777b3e068f7d3284d40fb3
Jan Klass <kissaki@posteo.de>
2020-12-19 18:42:13 +0100

    New translations TaskList.cpp (Korean)

--- mumble_ko.ts
+++ mumble_ko.ts
@@ -3,166 +3,89 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-/* Copyright (C) 2005-2011, Thorvald Natvig <thorvald@natvig.com>
-   Copyright (C) 2007, Sebastian Schlingmann <mit_service@users.sourceforge.net>
-   Copyright (C) 2008-2011, Mikkel Krautz <mikkel@krautz.dk>
-   Copyright (C) 2014, Mayur Pawashe <zorgiepoo@gmail.com>
+#include "TaskList.h"
 
-   All rights reserved.
+#include "MumbleApplication.h"
 
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions
-   are met:
+#include "win.h"
 
-   - Redistributions of source code must retain the above copyright notice,
-     this list of conditions and the following disclaimer.
-   - Redistributions in binary form must reproduce the above copyright notice,
-     this list of conditions and the following disclaimer in the documentation
-     and/or other materials provided with the distribution.
-   - Neither the name of the Mumble Developers nor the names of its
-     contributors may be used to endorse or promote products derived from this
-     software without specific prior written permission.
+#include <QtCore/QFileInfo>
+#include <QtCore/QSettings>
+#include <QtCore/QString>
+#include <QtCore/QUrl>
+#include <QtCore/QUrlQuery>
 
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR
-   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+// We disable clang-format for these includes as apparently the order in which they are
+// included is important.
+// clang-format off
+#include <shlobj.h>
+#include <shobjidl.h>
+#include <propkey.h>
+#include <propvarutil.h>
+// clang-format on
 
-#import <AppKit/AppKit.h>
+extern bool bIsWin7;
 
-#include "Global.h"
-#include "TextToSpeech.h"
-
-@interface MUSpeechSynthesizerPrivateHelper : NSObject {
-	NSMutableArray *m_messages;
-	NSSpeechSynthesizer *m_synthesizer;
-}
-- (NSSpeechSynthesizer *)synthesizer;
-- (void)appendMessage:(NSString *)message;
-- (void)processSpeech;
-@end
-
-#if !defined(USE_MAC_UNIVERSAL) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6
-@interface MUSpeechSynthesizerPrivateHelper () <NSSpeechSynthesizerDelegate>
-@end
-#endif
-
-@implementation MUSpeechSynthesizerPrivateHelper
-
-- (id)init {
-	if ((self = [super init])) {
-		m_synthesizer = [[NSSpeechSynthesizer alloc] initWithVoice:nil];
-		m_messages = [[NSMutableArray alloc] init];
-		[m_synthesizer setDelegate:self];
-	}
-	return self;
-}
-
-- (void)dealloc {
-	[m_synthesizer release];
-	[m_messages release];
-	[super dealloc];
-}
-
-- (NSSpeechSynthesizer *)synthesizer {
-	return m_synthesizer;
-}
-
-- (void)appendMessage:(NSString *)message {
-	[m_messages insertObject:message atIndex:0];
-}
-
-- (void)processSpeech {
-	Q_ASSERT([m_messages count] == 0);
-	
-	NSString *poppedMessage = [m_messages lastObject];
-	[m_synthesizer startSpeakingString:poppedMessage];
-	[m_messages removeLastObject];
-}
-
-- (void)speechSynthesizer:(NSSpeechSynthesizer *)synthesizer didFinishSpeaking:(BOOL)success {
-	Q_UNUSED(synthesizer);
-	Q_UNUSED(success);
-
-	if ([m_messages count] != 0) {
-		[self processSpeech];
-	}
-}
-
-@end
-
-class TextToSpeechPrivate {
-	public:
-		MUSpeechSynthesizerPrivateHelper *m_synthesizerHelper;
-
-		TextToSpeechPrivate();
-		~TextToSpeechPrivate();
-		void say(const QString &text);
-		void setVolume(int v);
-};
-
-TextToSpeechPrivate::TextToSpeechPrivate() {
-	m_synthesizerHelper = [[MUSpeechSynthesizerPrivateHelper alloc] init];
-}
-
-TextToSpeechPrivate::~TextToSpeechPrivate() {
-	[m_synthesizerHelper release];
-}
-
-void TextToSpeechPrivate::say(const QString &text) {
-	QByteArray byteArray = text.toUtf8();
-	NSString *message = [[NSString alloc] initWithBytes:byteArray.constData() length:byteArray.size() encoding:NSUTF8StringEncoding];
-
-	if (message == nil) {
+void TaskList::addToRecentList(QString name, QString user, QString host, int port) {
+	if (!bIsWin7)
 		return;
+
+	HRESULT hr;
+	IShellLink *link   = nullptr;
+	IPropertyStore *ps = nullptr;
+	PROPVARIANT pt;
+
+	hr = CoCreateInstance(CLSID_ShellLink, nullptr, CLSCTX_INPROC_SERVER, __uuidof(IShellLink),
+						  reinterpret_cast< void ** >(&link));
+	if (!link || FAILED(hr))
+		return;
+
+	QUrl url;
+	url.setScheme(QLatin1String("mumble"));
+	url.setUserName(user);
+	url.setHost(host);
+	url.setPort(port);
+
+	QUrlQuery query;
+	query.addQueryItem(QLatin1String("title"), name);
+	query.addQueryItem(QLatin1String("version"), QLatin1String("1.2.0"));
+	url.setQuery(query);
+
+	QSettings settings(QLatin1String("HKEY_CLASSES_ROOT"), QSettings::NativeFormat);
+
+	QString app = settings.value(QLatin1String("mumble/DefaultIcon/.")).toString();
+	if (app.isEmpty() || !QFileInfo(app).exists())
+		app = QCoreApplication::applicationFilePath();
+
+	link->SetPath(app.toStdWString().c_str());
+	link->SetArguments(QString::fromLatin1(url.toEncoded()).toStdWString().c_str());
+
+	hr = link->QueryInterface(__uuidof(IPropertyStore), reinterpret_cast< void ** >(&ps));
+	if (FAILED(hr)) {
+		qFatal("TaskList: Failed to get property store");
+		goto cleanup;
 	}
 
-	[m_synthesizerHelper appendMessage:message];
-	[message release];
+	InitPropVariantFromString(name.toStdWString().c_str(), &pt);
+	hr = ps->SetValue(PKEY_Title, pt);
+	PropVariantClear(&pt);
 
-	if (![[m_synthesizerHelper synthesizer] isSpeaking]) {
-		[m_synthesizerHelper processSpeech];
+	if (FAILED(hr)) {
+		qFatal("TaskList: Failed to set title");
+		goto cleanup;
 	}
-}
 
-void TextToSpeechPrivate::setVolume(int volume) {
-	// Check for setVolume: availability. It's only available on 10.5+.
-	if ([[m_synthesizerHelper synthesizer] respondsToSelector:@selector(setVolume:)]) {
-		[[m_synthesizerHelper synthesizer] setVolume:volume / 100.0];
+	hr = ps->Commit();
+	if (FAILED(hr)) {
+		qFatal("TaskList: Failed commit");
+		goto cleanup;
 	}
-}
 
-TextToSpeech::TextToSpeech(QObject *) {
-	enabled = true;
-	d = new TextToSpeechPrivate();
-}
+	SHAddToRecentDocs(SHARD_LINK, link);
 
-TextToSpeech::~TextToSpeech() {
-	delete d;
-}
-
-void TextToSpeech::say(const QString &text) {
-	if (d && enabled)
-		d->say(text);
-}
-
-void TextToSpeech::setEnabled(bool e) {
-	enabled = e;
-}
-
-void TextToSpeech::setVolume(int volume) {
-	if (d && enabled)
-		d->setVolume(volume);
-}
-
-bool TextToSpeech::isEnabled() const {
-	return enabled;
+cleanup:
+	if (ps)
+		ps->Release();
+	if (link)
+		link->Release();
 }
