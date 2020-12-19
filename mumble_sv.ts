b7486b1e1c07f334d86c81f4c7e786c174195f21
Jan Klass <kissaki@posteo.de>
2020-12-19 19:05:31 +0100

    New translations OverlayConfig.h (Swedish)

--- mumble_sv.ts
+++ mumble_sv.ts
@@ -3,48 +3,100 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_OVERLAYEDITOR_H_
-#define MUMBLE_MUMBLE_OVERLAYEDITOR_H_
+#ifndef MUMBLE_MUMBLE_OVERLAYCONFIG_H_
+#define MUMBLE_MUMBLE_OVERLAYCONFIG_H_
 
-#include "OverlayEditorScene.h"
-#include "ui_OverlayEditor.h"
+#include "ConfigWidget.h"
+#include "OverlayText.h"
 
-struct OverlaySettings;
+#include "ui_Overlay.h"
 
-class OverlayEditor : public QDialog, public Ui::OverlayEditor {
+class OverlayUserGroup;
+struct OverlayAppInfo;
+class OverlayPositionableItem;
+
+class OverlayConfig : public ConfigWidget, public Ui::OverlayConfig {
 private:
 	Q_OBJECT
-	Q_DISABLE_COPY(OverlayEditor)
-protected:
-	QGraphicsItem *qgiPromote;
-	OverlayEditorScene oes;
-	OverlaySettings *os;
+	Q_DISABLE_COPY(OverlayConfig)
 
-	void enterEvent(QEvent *e) Q_DECL_OVERRIDE;
-	void leaveEvent(QEvent *e) Q_DECL_OVERRIDE;
+	void initDisplay();
+	void initDisplayFps();
+	void initDisplayClock();
+	void refreshFpsDemo();
+	void refreshFpsLive();
+	void refreshTimeLive();
+	void addWhitelistPath(const QString &path);
+
+protected:
+	QPixmap qpScreen;
+	QGraphicsPixmapItem *qgpiScreen;
+	QGraphicsScene qgs;
+	QGraphicsScene qgsFpsPreview;
+	BasepointPixmap bpFpsDemo;
+	BasepointPixmap bpTimeDemo;
+	QGraphicsPixmapItem *qgpiFpsDemo;
+	OverlayPositionableItem *qgpiFpsLive;
+	OverlayPositionableItem *qgpiTimeLive;
+	OverlayUserGroup *oug;
+	QGraphicsTextItem *qgtiInstructions;
+
+	float fViewScale;
+
+	bool eventFilter(QObject *, QEvent *) Q_DECL_OVERRIDE;
+
+	bool supportsInstallableOverlay();
+	bool isInstalled();
+	bool needsUpgrade();
+	bool installFiles();
+	bool uninstallFiles();
+	bool supportsCertificates();
+	bool installerIsValid();
+	void showCertificates();
+
+	void updateOverlayExclusionModeState();
+protected slots:
+	void on_qpbInstall_clicked();
+	void on_qpbUninstall_clicked();
+
+	void on_qcbOverlayExclusionMode_currentIndexChanged(int);
+
+	void on_qlwLaunchers_itemSelectionChanged();
+	void on_qpbLaunchersAdd_clicked();
+	void on_qpbLaunchersRemove_clicked();
+
+	void on_qlwWhitelist_itemSelectionChanged();
+	void on_qpbWhitelistAdd_clicked();
+	void on_qpbWhitelistRemove_clicked();
+
+	void on_qlwPaths_itemSelectionChanged();
+	void on_qpbPathsAdd_clicked();
+	void on_qpbPathsRemove_clicked();
+
+	void on_qlwBlacklist_itemSelectionChanged();
+	void on_qpbBlacklistAdd_clicked();
+	void on_qpbBlacklistRemove_clicked();
+
+	void on_qcbEnable_stateChanged(int);
+	void on_qcbShowFps_stateChanged(int);
+	void on_qcbShowTime_stateChanged(int);
+	void on_qpbFpsFont_clicked();
+	void on_qpbFpsColor_clicked();
+	void on_qpbLoadPreset_clicked();
+	void on_qpbSavePreset_clicked();
+	void resizeScene(bool force = false);
 
 public:
-	OverlayEditor(QWidget *p = nullptr, QGraphicsItem *qgi = nullptr, OverlaySettings *osptr = nullptr);
-	~OverlayEditor() Q_DECL_OVERRIDE;
-signals:
-	void applySettings();
+	/// The uniqe name of this ConfigWidget
+	static const QString name;
+	OverlayConfig(Settings &st);
+	virtual QString title() const Q_DECL_OVERRIDE;
+	virtual const QString &getName() const Q_DECL_OVERRIDE;
+	virtual QIcon icon() const Q_DECL_OVERRIDE;
 public slots:
-	void reset();
-	void apply();
-	void accept() Q_DECL_OVERRIDE;
-
-	void on_qrbPassive_clicked();
-	void on_qrbTalking_clicked();
-	void on_qrbWhisper_clicked();
-	void on_qrbShout_clicked();
-
-	void on_qcbAvatar_clicked();
-	void on_qcbUser_clicked();
-	void on_qcbChannel_clicked();
-	void on_qcbMutedDeafened_clicked();
-	void on_qcbBox_clicked();
-
-	void on_qsZoom_valueChanged(int);
+	void accept() const Q_DECL_OVERRIDE;
+	void save() const Q_DECL_OVERRIDE;
+	void load(const Settings &r) Q_DECL_OVERRIDE;
 };
 
 #endif
