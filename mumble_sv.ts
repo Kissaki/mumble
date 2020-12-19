b2f4f6f8577651809dae4360a2e8b35029d95832
Jan Klass <kissaki@posteo.de>
2020-12-19 19:22:04 +0100

    New translations mumble.qrc (Swedish)

--- mumble_sv.ts
+++ mumble_sv.ts
@@ -1,37 +1,21 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>CFBundleExecutable</key>
-	<string>Mumble</string>
-	<key>CFBundleGetInfoString</key>
-	<string>An open source, low-latency, high quality voice chat software primarily intended for use while gaming.</string>
-	<key>CFBundleIconFile</key>
-	<string>mumble.icns</string>
-	<key>CFBundleIdentifier</key>
-	<string>net.sourceforge.mumble.Mumble</string>
-	<key>CFBundlePackageType</key>
-	<string>APPL</string>
-	<key>CFBundleSignature</key>
-	<string>MBLE</string>
-	<key>CFBundleURLTypes</key>
-	<array>
-		<dict>
-			<key>CFBundleURLName</key>
-			<string>Mumble Server URL</string>
-			<key>CFBundleURLSchemes</key>
-			<array>
-				<string>mumble</string>
-			</array>
-		</dict>
-	</array>
-	<key>CFBundleVersion</key>
-	<string>@PROJECT_VERSION_MAJOR@.@PROJECT_VERSION_MINOR@.@PROJECT_VERSION_PATCH@</string>
-	<key>NSHumanReadableCopyright</key>
-	<string>Copyright (c) 2005-@MUMBLE_BUILD_YEAR@ The Mumble Developers</string>
-	<key>NSPrincipalClass</key>
-	<string>NSApplication</string>
-	<key>NSHighResolutionCapable</key>
-	<true/>
-</dict>
-</plist>
+<RCC>
+    <qresource prefix="/">
+        <file alias="rec.svg">../../icons/rec.svg</file>
+        <file alias="mumble.svg">../../icons/mumble.svg</file>
+        <file alias="mumble.icns">../../icons/mumble.icns</file>
+        <file alias="wb_male.oga">../../samples/wb_male.oga</file>
+        <file alias="on.ogg">../../samples/on.ogg</file>
+        <file alias="off.ogg">../../samples/off.ogg</file>
+        <file alias="Critical.ogg">../../samples/Critical.ogg</file>
+        <file alias="PermissionDenied.ogg">../../samples/PermissionDenied.ogg</file>
+        <file alias="SelfMutedDeafened.ogg">../../samples/SelfMutedDeafened.ogg</file>
+        <file alias="ServerConnected.ogg">../../samples/ServerConnected.ogg</file>
+        <file alias="ServerDisconnected.ogg">../../samples/ServerDisconnected.ogg</file>
+        <file alias="TextMessage.ogg">../../samples/TextMessage.ogg</file>
+        <file alias="UserJoinedChannel.ogg">../../samples/UserJoinedChannel.ogg</file>
+        <file alias="UserKickedYouOrByYou.ogg">../../samples/UserKickedYouOrByYou.ogg</file>
+        <file alias="UserLeftChannel.ogg">../../samples/UserLeftChannel.ogg</file>
+        <file alias="UserMutedYouOrByYou.ogg">../../samples/UserMutedYouOrByYou.ogg</file>
+        <file alias="RecordingStateChanged.ogg">../../samples/RecordingStateChanged.ogg</file>
+    </qresource>
+</RCC>
