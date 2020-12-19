9976d8cb28a964d7c7fe153308e5e79a9b121f38
Jan Klass <kissaki@posteo.de>
2020-12-19 18:52:36 +0100

    New translations OpusCodec.h (Korean)

--- mumble_ko.ts
+++ mumble_ko.ts
@@ -3,31 +3,49 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_OVERLAYPOSITIONABLEITEM_H
-#define MUMBLE_MUMBLE_OVERLAYPOSITIONABLEITEM_H
+#ifndef MUMBLE_MUMBLE_OPUSCODEC_H_
+#define MUMBLE_MUMBLE_OPUSCODEC_H_
 
-#include <QtCore/QtGlobal>
-#include <QtWidgets/QGraphicsItem>
+#include <opus.h>
 
-class OverlayPositionableItem : public QObject, public QGraphicsPixmapItem {
-	Q_OBJECT
-	Q_DISABLE_COPY(OverlayPositionableItem);
+#include <QtCore/QLibrary>
+
+#ifndef Q_OS_WIN
+#	define __cdecl
+#endif
+
+/// Loads Opus from a shared library and acts as a wrapper for its functions.
+class OpusCodec {
+private:
+	Q_DISABLE_COPY(OpusCodec)
+protected:
+	QLibrary qlOpus;
+	bool bValid;
 
 public:
-	OverlayPositionableItem(QRectF *posPtr, const bool isPositionable = false);
-	virtual ~OverlayPositionableItem();
-	void updateRender();
-	void setItemVisible(const bool &visible);
+	OpusCodec();
+	virtual ~OpusCodec();
 
-private:
-	/// Float value between 0 and 1 where 0,0 is top left, and 1,1 is bottom right
-	QRectF *m_position;
-	const bool m_isPositionEditable;
-	QGraphicsEllipseItem *m_qgeiHandle;
-	void createPositioningHandle();
-	bool sceneEventFilter(QGraphicsItem *, QEvent *) Q_DECL_OVERRIDE;
-private slots:
-	void onMove();
+	bool isValid() const;
+	void report() const;
+
+	const char *(__cdecl *opus_get_version_string)();
+
+	OpusEncoder *(__cdecl *opus_encoder_create)(opus_int32 Fs, int channels, int application, int *error);
+	int(__cdecl *opus_encoder_ctl)(OpusEncoder *st, int request, ...);
+	void(__cdecl *opus_encoder_destroy)(OpusEncoder *st);
+	OpusDecoder *(__cdecl *opus_decoder_create)(opus_int32 Fs, int channels, int *error);
+	int(__cdecl *opus_decoder_ctl)(OpusDecoder *st, int request, ...);
+	void(__cdecl *opus_decoder_destroy)(OpusDecoder *st);
+
+	int(__cdecl *opus_encode)(OpusEncoder *st, const opus_int16 *pcm, int frame_size, unsigned char *compressed,
+							  int nbCompressedBytes);
+	int(__cdecl *opus_decode_float)(OpusDecoder *st, const unsigned char *data, opus_int32 len, float *pcm,
+									int frame_size, int decode_fec);
+
+	int(__cdecl *opus_decoder_get_nb_samples)(OpusDecoder *st, const unsigned char packet[], opus_int32 len);
+
+	int(__cdecl *opus_packet_get_samples_per_frame)(const unsigned char *data, opus_int32 Fs);
 };
 
-#endif
+#endif // OPUSCODEC_H_
