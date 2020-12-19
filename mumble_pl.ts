7ba54fac4fa11946777bf6a4fb53e3f52645257a
Jan Klass <kissaki@posteo.de>
2020-12-19 19:09:59 +0100

    New translations OverlayPositionableItem.cpp (Polish)

--- mumble_pl.ts
+++ mumble_pl.ts
@@ -3,32 +3,85 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include "Overlay.h"
-#include "OverlayConfig.h"
+#include "OverlayPositionableItem.h"
 
-void Overlay::platformInit() {
-	d = nullptr;
+#include "Utils.h"
+
+#include <QtCore/QEvent>
+#include <QtGui/QPen>
+#include <QtWidgets/QGraphicsScene>
+
+OverlayPositionableItem::OverlayPositionableItem(QRectF *posPtr, const bool isPositionable)
+	: m_position(posPtr), m_isPositionEditable(isPositionable), m_qgeiHandle(nullptr) {
 }
 
-void Overlay::setActiveInternal(bool) {
+OverlayPositionableItem::~OverlayPositionableItem() {
+	delete m_qgeiHandle;
+	m_qgeiHandle = nullptr;
 }
 
-bool OverlayConfig::supportsInstallableOverlay() {
-	return false;
+void OverlayPositionableItem::createPositioningHandle() {
+	m_qgeiHandle = new QGraphicsEllipseItem(QRectF(-4.0f, -4.0f, 8.0f, 8.0f));
+	m_qgeiHandle->setPen(QPen(Qt::darkRed, 0.0f));
+	m_qgeiHandle->setBrush(Qt::red);
+	m_qgeiHandle->setZValue(0.5f);
+	m_qgeiHandle->setFlag(QGraphicsItem::ItemIsMovable);
+	m_qgeiHandle->setFlag(QGraphicsItem::ItemIsSelectable);
+	scene()->addItem(m_qgeiHandle);
+	m_qgeiHandle->installSceneEventFilter(this);
 }
 
-bool OverlayConfig::isInstalled() {
-	return true;
+bool OverlayPositionableItem::sceneEventFilter(QGraphicsItem *watched, QEvent *e) {
+	switch (e->type()) {
+		case QEvent::GraphicsSceneMouseMove:
+		case QEvent::GraphicsSceneMouseRelease:
+			QMetaObject::invokeMethod(this, "onMove", Qt::QueuedConnection);
+			break;
+		default:
+			break;
+	}
+	return QGraphicsItem::sceneEventFilter(watched, e);
 }
 
-bool OverlayConfig::needsUpgrade() {
-	return false;
+void OverlayPositionableItem::onMove() {
+	if (!m_qgeiHandle) {
+		return;
+	}
+
+	const QRectF &sr = scene()->sceneRect();
+	const QPointF &p = m_qgeiHandle->pos();
+
+	m_position->setX(qBound< qreal >(0.0f, p.x() / sr.width(), 1.0f));
+	m_position->setY(qBound< qreal >(0.0f, p.y() / sr.height(), 1.0f));
+
+	m_qgeiHandle->setPos(m_position->x() * sr.width(), m_position->y() * sr.height());
+
+	updateRender();
 }
 
-bool OverlayConfig::installFiles() {
-	return false;
+void OverlayPositionableItem::updateRender() {
+	const QRectF &sr = scene()->sceneRect();
+	// Translate the 0..1 float position to the real scene coordinates (relative to absolute position)
+	QPoint absPos(iroundf(sr.width() * m_position->x() + 0.5f), iroundf(sr.height() * m_position->y() + 0.5f));
+
+	if (m_isPositionEditable) {
+		if (!m_qgeiHandle) {
+			createPositioningHandle();
+		}
+		m_qgeiHandle->setPos(absPos.x(), absPos.y());
+	}
+
+	QRectF br = boundingRect();
+	// Limit the position by the elements width (to make sure it is right-/bottom-bound rather than outside of the scene
+	QPoint maxPos(iroundf(sr.width() - br.width() + 0.5f), iroundf(sr.height() - br.height() + 0.5f));
+	int basex = qBound< int >(0, absPos.x(), maxPos.x());
+	int basey = qBound< int >(0, absPos.y(), maxPos.y());
+	setPos(basex, basey);
 }
 
-bool OverlayConfig::uninstallFiles() {
-	return false;
+void OverlayPositionableItem::setItemVisible(const bool &visible) {
+	setVisible(visible);
+	if (m_qgeiHandle) {
+		m_qgeiHandle->setVisible(visible);
+	}
 }
