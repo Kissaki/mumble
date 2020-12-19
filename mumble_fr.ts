1eb643778ffb448487ba3be5c0451b7f82491195
Jan Klass <kissaki@posteo.de>
2020-12-19 17:57:57 +0100

    New translations Plugins.h (French)

--- mumble_fr.ts
+++ mumble_fr.ts
@@ -3,38 +3,89 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_PATHLISTWIDGET_H_
-#define MUMBLE_MUMBLE_PATHLISTWIDGET_H_
+#ifndef MUMBLE_MUMBLE_PLUGINS_H_
+#define MUMBLE_MUMBLE_PLUGINS_H_
 
-#include <QListWidget>
+#include "ConfigDialog.h"
 
-class QGraphicsSceneDragDropEvent;
-class QDropEvent;
+#include "ui_Plugins.h"
 
-/**
- * ListWidget that adds functionality for being able to drop files or folders to add them to the list.
- *
- * It makes use of OverlayAppInfo to display file/app information (e.g. the appropriate icon).
- */
-class PathListWidget : public QListWidget {
+#ifdef Q_OS_WIN
+#	include "win.h"
+#endif
+
+#include <QtCore/QMutex>
+#include <QtCore/QObject>
+#include <QtCore/QReadWriteLock>
+#include <QtCore/QUrl>
+
+struct PluginInfo;
+
+class PluginConfig : public ConfigWidget, public Ui::PluginConfig {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(PluginConfig)
+protected:
+	void refillPluginList();
+	PluginInfo *pluginForItem(QTreeWidgetItem *) const;
+
 public:
-	enum PathType { FILE_EXE, FOLDER };
+	/// The unique name of this ConfigWidget
+	static const QString name;
+	PluginConfig(Settings &st);
+	virtual QString title() const Q_DECL_OVERRIDE;
+	const QString &getName() const Q_DECL_OVERRIDE;
+	virtual QIcon icon() const Q_DECL_OVERRIDE;
+public slots:
+	void save() const Q_DECL_OVERRIDE;
+	void load(const Settings &r) Q_DECL_OVERRIDE;
+	void on_qpbConfig_clicked();
+	void on_qpbAbout_clicked();
+	void on_qpbReload_clicked();
+	void on_qtwPlugins_currentItemChanged(QTreeWidgetItem *, QTreeWidgetItem *);
+};
 
-	PathListWidget(QWidget *parent = 0);
-	void setPathType(PathType type);
-	virtual void dragEnterEvent(QDragEnterEvent *event) Q_DECL_OVERRIDE;
-	virtual void dragMoveEvent(QDragMoveEvent *event) Q_DECL_OVERRIDE;
-	virtual void dropEvent(QDropEvent *event) Q_DECL_OVERRIDE;
+struct PluginFetchMeta;
+
+class Plugins : public QObject {
+	friend class PluginConfig;
 
 private:
 	Q_OBJECT
-	Q_DISABLE_COPY(PathListWidget)
+	Q_DISABLE_COPY(Plugins)
+protected:
+	QReadWriteLock qrwlPlugins;
+	QMutex qmPluginStrings;
+	QList< PluginInfo * > qlPlugins;
+	PluginInfo *locked;
+	PluginInfo *prevlocked;
+	void clearPlugins();
+	int iPluginTry;
+	QMap< QString, PluginFetchMeta > qmPluginFetchMeta;
+	QString qsSystemPlugins;
+	QString qsUserPlugins;
+#ifdef Q_OS_WIN
+	HANDLE hToken;
+	TOKEN_PRIVILEGES tpPrevious;
+	DWORD cbPrevious;
+#endif
+public:
+	std::string ssContext, ssContextSent;
+	std::wstring swsIdentity, swsIdentitySent;
+	bool bValid;
+	bool bUnlink;
+	float fPosition[3], fFront[3], fTop[3];
+	float fCameraPosition[3], fCameraFront[3], fCameraTop[3];
 
-	PathType pathType;
-
-	void addFilePath(const QString &path);
-	void addFolderPath(const QString &path);
-	void checkAcceptDragEvent(QDropEvent *event, bool store);
+	Plugins(QObject *p = nullptr);
+	~Plugins() Q_DECL_OVERRIDE;
+public slots:
+	void on_Timer_timeout();
+	void rescanPlugins();
+	bool fetch();
+	void checkUpdates();
+	void fetchedUpdatePAPlugins(QByteArray, QUrl);
+	void fetchedPAPluginDL(QByteArray, QUrl);
 };
 
 #endif
