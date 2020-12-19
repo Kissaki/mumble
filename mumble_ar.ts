80dee1fe789dc49b37caa4d484aea4d0a4b5dbea
Jan Klass <kissaki@posteo.de>
2020-12-19 18:25:40 +0100

    New translations TalkingUI.h (Arabic)

--- mumble_ar.ts
+++ mumble_ar.ts
@@ -1,68 +1,129 @@
-// Copyright 2005-2020 The Mumble Developers. All rights reserved.
+// Copyright 2020 The Mumble Developers. All rights reserved.
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_THEMES_H_
-#define MUMBLE_MUMBLE_THEMES_H_
+#ifndef MUMBLE_MUMBLE_TALKINGUI_H_
+#define MUMBLE_MUMBLE_TALKINGUI_H_
 
-#include <Settings.h>
-#include <ThemeInfo.h>
-#ifndef Q_MOC_RUN
-#	include <boost/optional.hpp>
-#endif
+#include <QWidget>
+#include <QtCore/QHash>
+#include <QtCore/QSet>
+#include <QtGui/QIcon>
 
-class Themes {
-public:
-	/// Returns the style configured in the given settings structure
-	static boost::optional< ThemeInfo::StyleInfo > getConfiguredStyle(const Settings &settings);
+#include <memory>
+#include <vector>
 
-	/// Updates the given settings object to be configured to the given style
-	///
-	/// @note Does not apply the theme @see apply
-	///
-	/// @param settings Settings object to update
-	/// @param style Style to set
-	/// @param outChanged Will be set to true if the style in settings actually changed. Will not be changed otherwise.
-	static void setConfiguredStyle(Settings &settings, boost::optional< ThemeInfo::StyleInfo > style, bool &outChanged);
+#include "Settings.h"
+#include "TalkingUIContainer.h"
+#include "TalkingUIEntry.h"
+#include "TalkingUISelection.h"
 
-	/// Applies the theme
-	///
-	/// @note Can only apply a theme before MainWindow etc. is opened
-	static bool apply();
+class QLabel;
+class QGroupBox;
+class QTimer;
+class QMouseEvent;
 
-	/// Return a theme name to theme map
-	static ThemeMap getThemes();
+class Channel;
+class ClientUser;
+class TalkingUIComponent;
 
-	/// Returns the per user themes directory
-	static QDir getUserThemesDirectory();
+/// The talking UI is a widget that will display the users you are currently
+/// hearing to you.
+class TalkingUI : public QWidget {
+	friend class TalkingUIUser;
 
 private:
-	/// Applies the fallback stylesheet
-	static void applyFallback();
+	Q_OBJECT
+	Q_DISABLE_COPY(TalkingUI);
 
-	/// Tries to apply the configured theme.
-	/// @return True on success. False on failure.
-	static bool applyConfigured();
+	std::vector< std::unique_ptr< TalkingUIContainer > > m_containers;
+	/// The Entry corresponding to the currently selected user
+	std::unique_ptr< TalkingUISelection > m_currentSelection;
 
-	// Sets the theme to a QSS theme
-	static void setTheme(QString &themeQss, QStringList &skinPaths);
+	/// The current line height of an entry in the TalkingUI
+	int m_currentLineHeight;
 
-	/// Returns list of theme search directories ordered ascending by priorty (lowest first)
-	static QVector< QDir > getSearchDirectories();
+	int findContainer(int associatedChannelID, ContainerType type) const;
+	std::unique_ptr< TalkingUIContainer > removeContainer(const TalkingUIContainer &container);
+	std::unique_ptr< TalkingUIContainer > removeContainer(int associatedChannelID, ContainerType type);
+	std::unique_ptr< TalkingUIContainer > removeIfSuperfluous(const TalkingUIContainer &container);
 
-	/// Returns default style-sheet used for fall-backs
-	static QString getDefaultStylesheet();
+	void sortContainers();
 
-	/// userStylesheetPath returns the absolute path to the
-	/// user.qss file.
-	static QString userStylesheetPath();
+	TalkingUIUser *findUser(unsigned int userSession);
+	void removeUser(unsigned int userSession);
 
-	/// readStylesheet fills stylesheetContent with the content
-	/// of the file at stylesheetFn, if available.
-	/// If a the file is is available, the function returns true.
-	/// If no file is available, it returns false.
-	static bool readStylesheet(const QString &stylesheetFn, QString &stylesheetContent);
+	void addListener(const ClientUser *user, const Channel *channel);
+	TalkingUIChannelListener *findListener(unsigned int userSession, int channelID);
+	void removeListener(unsigned int userSession, int channelID);
+	void removeAllListeners();
+
+	/// Sets up the UI components
+	void setupUI();
+	/// Hides an user
+	///
+	/// @param session The session ID of the user that shall be hidden
+	void hideUser(unsigned int session);
+	/// Adds an UI entry for the given Channel, if none exists yet.
+	///
+	/// @param channel A pointer to the channel that shall be added
+	void addChannel(const Channel *channel);
+	;
+	/// Adds an UI entry for the given User, if none exists yet.
+	///
+	/// @param channel A pointer to the user that shall be added
+	/// @returns The pointer to the respective user entry in the TalkingUI
+	/// (may be nullptr in case of an error)
+	TalkingUIUser *findOrAddUser(const ClientUser *user);
+	/// Moves the given user into the given channel
+	///
+	/// @paam userSession The session ID of the user
+	/// @param channelID The channel ID of the channel
+	void moveUserToChannel(unsigned int userSession, int channelID);
+
+	/// Update (resize) the UI to its content
+	void updateUI();
+
+	/// Sets the font size according to the settings
+	///
+	/// @param widget a pointer to the widget to set the font size for
+	void setFontSize(QWidget *widget);
+
+	/// Updates the user's status icons (reflecting e.g. its mut-state)
+	///
+	/// @param user A pointer to the user that shall be processed
+	void updateStatusIcons(const ClientUser *user);
+
+	/// Set the current selection
+	///
+	/// @param selection The new selection
+	void setSelection(const TalkingUISelection &selection);
+
+	bool isSelected(const TalkingUIComponent &component) const;
+
+	void mousePressEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
+
+public:
+	TalkingUI(QWidget *parent = nullptr);
+
+	void setVisible(bool visible) Q_DECL_OVERRIDE;
+	QSize sizeHint() const Q_DECL_OVERRIDE;
+	QSize minimumSizeHint() const Q_DECL_OVERRIDE;
+
+public slots:
+	void on_talkingStateChanged();
+	void on_mainWindowSelectionChanged(const QModelIndex &current, const QModelIndex &previous);
+	void on_serverSynchronized();
+	void on_serverDisconnected();
+	void on_channelChanged(QObject *user);
+	void on_settingsChanged();
+	void on_clientDisconnected(unsigned int userSession);
+	void on_muteDeafStateChanged();
+	void on_userLocalVolumeAdjustmentsChanged(float newAdjustment, float oldAdjustment);
+	void on_channelListenerAdded(const ClientUser *user, const Channel *channel);
+	void on_channelListenerRemoved(const ClientUser *user, const Channel *channel);
+	void on_channelListenerLocalVolumeAdjustmentChanged(int channelID, float newAdjustment, float oldAdjustment);
 };
 
-#endif // MUMBLE_MUMBLE_THEMES_H_
+#endif // MUMBLE_MUMBLE_TALKINGUI_H_
