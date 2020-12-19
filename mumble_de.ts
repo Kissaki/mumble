c914b4ad62198d4059dfe6a0f914466780ceef4e
Jan Klass <kissaki@posteo.de>
2020-12-19 18:08:32 +0100

    New translations G15LCDEngine_unix.h (German)

--- mumble_de.ts
+++ mumble_de.ts
@@ -3,77 +3,42 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include <G15LCDEngine_unix.h>
+#ifndef MUMBLE_MUMBLE_G15LCDENGINE_UNIX_H_
+#	define MUMBLE_MUMBLE_G15LCDENGINE_UNIX_H_
+#	include "LCD.h"
 
-static LCDEngine *G15LCDEngineNew() {
-	return new G15LCDEngineUnix();
-}
+#	include <g15daemon_client.h>
 
-static LCDEngineRegistrar registrar(G15LCDEngineNew);
+class G15LCDDeviceUnix;
 
-G15LCDEngineUnix::G15LCDEngineUnix() {
-	sock = new_g15_screen(G15_PIXELBUF);
-	if (sock < 0) {
-		qWarning("G15LCDEngineUnix: Unable to connect to G15Daemon.");
-		return;
-	}
+class G15LCDEngineUnix : public LCDEngine {
+	friend class G15LCDDeviceUnix;
 
-	qlDevices << new G15LCDDeviceUnix(this);
-}
+protected:
+	int sock;
 
-G15LCDEngineUnix::~G15LCDEngineUnix() {
-}
+public:
+	G15LCDEngineUnix();
+	~G15LCDEngineUnix() Q_DECL_OVERRIDE;
+	QList< LCDDevice * > devices() const Q_DECL_OVERRIDE;
+};
 
-QList< LCDDevice * > G15LCDEngineUnix::devices() const {
-	return qlDevices;
-}
+class G15LCDDeviceUnix : public LCDDevice {
+protected:
+	bool bEnabled;
+	G15LCDEngineUnix *engine;
 
-/* -- */
+public:
+	G15LCDDeviceUnix(G15LCDEngineUnix *e);
+	~G15LCDDeviceUnix() Q_DECL_OVERRIDE;
+	bool enabled() Q_DECL_OVERRIDE;
+	void setEnabled(bool e) Q_DECL_OVERRIDE;
+	void blitImage(QImage *img, bool alert) Q_DECL_OVERRIDE;
+	QString name() const Q_DECL_OVERRIDE;
+	QSize size() const Q_DECL_OVERRIDE;
+};
 
-G15LCDDeviceUnix::G15LCDDeviceUnix(G15LCDEngineUnix *e) : LCDDevice() {
-	bEnabled = false;
-	engine   = e;
-}
-
-G15LCDDeviceUnix::~G15LCDDeviceUnix() {
-}
-
-bool G15LCDDeviceUnix::enabled() {
-	return bEnabled;
-}
-
-void G15LCDDeviceUnix::setEnabled(bool b) {
-	bEnabled = b;
-}
-
-void G15LCDDeviceUnix::blitImage(QImage *img, bool) {
-	Q_ASSERT(img);
-
-	const unsigned int len = 6880;
-	uchar buf[len];
-	uchar *tmp = img->bits();
-
-	for (unsigned int i = 0; i < len / 8; ++i) {
-		unsigned int idx = i * 8;
-		buf[idx + 7]     = tmp[i] & 0x80 ? 1 : 0;
-		buf[idx + 6]     = tmp[i] & 0x40 ? 1 : 0;
-		buf[idx + 5]     = tmp[i] & 0x20 ? 1 : 0;
-		buf[idx + 4]     = tmp[i] & 0x10 ? 1 : 0;
-		buf[idx + 3]     = tmp[i] & 0x08 ? 1 : 0;
-		buf[idx + 2]     = tmp[i] & 0x04 ? 1 : 0;
-		buf[idx + 1]     = tmp[i] & 0x02 ? 1 : 0;
-		buf[idx + 0]     = tmp[i] & 0x01 ? 1 : 0;
-	}
-
-	int ret = g15_send(engine->sock, reinterpret_cast< char * >(buf), len);
-	if (ret < 0)
-		qWarning("G15LCDDeviceUnix: Unable to g15_send().");
-}
-
-QString G15LCDDeviceUnix::name() const {
-	return QString::fromLatin1("Logitech Gamepanel (G15Daemon)");
-}
-
-QSize G15LCDDeviceUnix::size() const {
-	return QSize(G15_WIDTH, G15_HEIGHT);
-}
+#else
+class G15LCDEngineUnix;
+class G15LCDDeviceUnix;
+#endif
