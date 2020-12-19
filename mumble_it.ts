7a7237520a3e75d3761ab7c424b900618bdc1452
Jan Klass <kissaki@posteo.de>
2020-12-19 18:29:22 +0100

    New translations OverlayConfig.h (Italian)

--- mumble_it.ts
+++ mumble_it.ts
@@ -3,35 +3,100 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_OSS_H_
-#define MUMBLE_MUMBLE_OSS_H_
+#ifndef MUMBLE_MUMBLE_OVERLAYCONFIG_H_
+#define MUMBLE_MUMBLE_OVERLAYCONFIG_H_
 
-#include "AudioInput.h"
-#include "AudioOutput.h"
+#include "ConfigWidget.h"
+#include "OverlayText.h"
 
-class OSSInput : public AudioInput {
+#include "ui_Overlay.h"
+
+class OverlayUserGroup;
+struct OverlayAppInfo;
+class OverlayPositionableItem;
+
+class OverlayConfig : public ConfigWidget, public Ui::OverlayConfig {
 private:
 	Q_OBJECT
-	Q_DISABLE_COPY(OSSInput)
+	Q_DISABLE_COPY(OverlayConfig)
+
+	void initDisplay();
+	void initDisplayFps();
+	void initDisplayClock();
+	void refreshFpsDemo();
+	void refreshFpsLive();
+	void refreshTimeLive();
+	void addWhitelistPath(const QString &path);
+
 protected:
-	void release();
+	QPixmap qpScreen;
+	QGraphicsPixmapItem *qgpiScreen;
+	QGraphicsScene qgs;
+	QGraphicsScene qgsFpsPreview;
+	BasepointPixmap bpFpsDemo;
+	BasepointPixmap bpTimeDemo;
+	QGraphicsPixmapItem *qgpiFpsDemo;
+	OverlayPositionableItem *qgpiFpsLive;
+	OverlayPositionableItem *qgpiTimeLive;
+	OverlayUserGroup *oug;
+	QGraphicsTextItem *qgtiInstructions;
+
+	float fViewScale;
+
+	bool eventFilter(QObject *, QEvent *) Q_DECL_OVERRIDE;
+
+	bool supportsInstallableOverlay();
+	bool isInstalled();
+	bool needsUpgrade();
+	bool installFiles();
+	bool uninstallFiles();
+	bool supportsCertificates();
+	bool installerIsValid();
+	void showCertificates();
+
+	void updateOverlayExclusionModeState();
+protected slots:
+	void on_qpbInstall_clicked();
+	void on_qpbUninstall_clicked();
+
+	void on_qcbOverlayExclusionMode_currentIndexChanged(int);
+
+	void on_qlwLaunchers_itemSelectionChanged();
+	void on_qpbLaunchersAdd_clicked();
+	void on_qpbLaunchersRemove_clicked();
+
+	void on_qlwWhitelist_itemSelectionChanged();
+	void on_qpbWhitelistAdd_clicked();
+	void on_qpbWhitelistRemove_clicked();
+
+	void on_qlwPaths_itemSelectionChanged();
+	void on_qpbPathsAdd_clicked();
+	void on_qpbPathsRemove_clicked();
+
+	void on_qlwBlacklist_itemSelectionChanged();
+	void on_qpbBlacklistAdd_clicked();
+	void on_qpbBlacklistRemove_clicked();
+
+	void on_qcbEnable_stateChanged(int);
+	void on_qcbShowFps_stateChanged(int);
+	void on_qcbShowTime_stateChanged(int);
+	void on_qpbFpsFont_clicked();
+	void on_qpbFpsColor_clicked();
+	void on_qpbLoadPreset_clicked();
+	void on_qpbSavePreset_clicked();
+	void resizeScene(bool force = false);
 
 public:
-	OSSInput();
-	~OSSInput() Q_DECL_OVERRIDE;
-	void run() Q_DECL_OVERRIDE;
-};
-
-class OSSOutput : public AudioOutput {
-	friend class OSSUser;
-
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(OSSOutput)
-public:
-	OSSOutput();
-	~OSSOutput() Q_DECL_OVERRIDE;
-	void run() Q_DECL_OVERRIDE;
+	/// The uniqe name of this ConfigWidget
+	static const QString name;
+	OverlayConfig(Settings &st);
+	virtual QString title() const Q_DECL_OVERRIDE;
+	virtual const QString &getName() const Q_DECL_OVERRIDE;
+	virtual QIcon icon() const Q_DECL_OVERRIDE;
+public slots:
+	void accept() const Q_DECL_OVERRIDE;
+	void save() const Q_DECL_OVERRIDE;
+	void load(const Settings &r) Q_DECL_OVERRIDE;
 };
 
 #endif
