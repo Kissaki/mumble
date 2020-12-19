1a5b5ef5a4a60e8e06fce913b2093c371406a7f1
Jan Klass <kissaki@posteo.de>
2020-12-19 18:13:07 +0100

    New translations Usage.h (German)

--- mumble_de.ts
+++ mumble_de.ts
@@ -3,77 +3,31 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_USEREDIT_H_
-#define MUMBLE_MUMBLE_USEREDIT_H_
+#ifndef MUMBLE_MUMBLE_USAGE_H_
+#define MUMBLE_MUMBLE_USAGE_H_
 
-#include "Message.h"
-#include "User.h"
-#include "ui_UserEdit.h"
+#include <QtCore/QBuffer>
+#include <QtCore/QDataStream>
+#include <QtCore/QObject>
 
-#include <QSortFilterProxyModel>
+class ClientUser;
 
-class UserListModel;
-class UserListFilterProxyModel;
-
-namespace MumbleProto {
-class UserList;
-class UserList_User;
-} // namespace MumbleProto
-
+/// This class is used to send usage information to the mumble.info website.
 ///
-/// Dialog used for server-side registered user list editing.
-///
-class UserEdit : public QDialog, public Ui::UserEdit {
+/// During instantiation of this class a ten minute timer will be started. After that
+/// the object's registerUsage function will be called that sends the respective
+/// information to the server.
+class Usage : public QObject {
 	Q_OBJECT
-	Q_DISABLE_COPY(UserEdit)
+protected:
+	QBuffer qbReport;
+	QDataStream qdsReport;
+
 public:
-	/// Constructs a dialog for editing the given userList.
-	UserEdit(const MumbleProto::UserList &userList, QWidget *p = nullptr);
-
+	Usage(QObject *p = nullptr);
 public slots:
-	void accept() Q_DECL_OVERRIDE;
-
-	void on_qlSearch_textChanged(QString);
-	void on_qpbRemove_clicked();
-	void on_qpbRename_clicked();
-	void on_qtvUserList_customContextMenuRequested(const QPoint &);
-	void onSelectionChanged(const QItemSelection & /*selected*/, const QItemSelection & /*deselected*/);
-	void onCurrentRowChanged(const QModelIndex &current, const QModelIndex & /*previous*/);
-	void on_qsbInactive_valueChanged(int);
-	void on_qcbInactive_currentIndexChanged(int);
-
-private:
-	enum TimespanUnits { TU_DAYS, TU_WEEKS, TU_MONTHS, TU_YEARS, COUNT_TU };
-
-	/// Polls the inactive-filter controls for their current value and updates the model filter.
-	void updateInactiveDaysFilter();
-
-	UserListModel *m_model;
-	UserListFilterProxyModel *m_filter;
+	/// Performs the actual registration.
+	void registerUsage();
 };
 
-///
-/// Provides filtering and sorting capabilities for UserListModel instances to UserEdit.
-/// @see UserEdit
-/// @see UserListModel
-///
-class UserListFilterProxyModel : public QSortFilterProxyModel {
-	Q_OBJECT
-public:
-	explicit UserListFilterProxyModel(QObject *parent = nullptr);
-
-	bool filterAcceptsRow(int source_row, const QModelIndex &source_parent) const Q_DECL_OVERRIDE;
-
-public slots:
-	/// Sets the amount of inactive days below which rows will get filterd by the proxy
-	void setFilterMinimumInactiveDays(int minimumInactiveDays);
-	/// Helper function for removing all rows involved in a given selection (must include COL_NICK).
-	void removeRowsInSelection(const QItemSelection &selection);
-
-private:
-	/// Every row with less inactive days will be filtered.
-	int m_minimumInactiveDays;
-};
-
-
-#endif // MUMBLE_MUMBLE_USEREDIT_H_
+#endif
