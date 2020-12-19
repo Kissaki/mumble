aafec03359392cc69ced0ea99b9c7f6ba77a3ef5
Jan Klass <kissaki@posteo.de>
2020-12-19 19:13:28 +0100

    New translations SocketRPC.h (Russian)

--- mumble_ru.ts
+++ mumble_ru.ts
@@ -3,95 +3,52 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include "SharedMemory.h"
+#ifndef MUMBLE_MUMBLE_SOCKETRPC_H_
+#define MUMBLE_MUMBLE_SOCKETRPC_H_
 
-#include "win.h"
+#include <QtCore/QObject>
+#include <QtCore/QString>
+#include <QtCore/QVariant>
+#include <QtCore/QXmlStreamReader>
+#include <QtNetwork/QLocalSocket>
 
-#include <QtCore/QDebug>
+class QBuffer;
+class QLocalServer;
 
-struct SharedMemory2Private {
-	HANDLE hMemory;
+class SocketRPCClient : public QObject {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(SocketRPCClient)
+protected:
+	QLocalSocket *qlsSocket;
+	QXmlStreamReader qxsrReader;
+	QXmlStreamWriter qxswWriter;
+	QBuffer *qbBuffer;
+	QByteArray qbaOutput;
+
+	void processXml();
+
+public:
+	SocketRPCClient(QLocalSocket *s, QObject *p = nullptr);
+public slots:
+	void disconnected();
+	void error(QLocalSocket::LocalSocketError);
+	void readyRead();
 };
 
-SharedMemory2::SharedMemory2(QObject *p, unsigned int minsize, const QString &memname) : QObject(p) {
-	a_ucData = nullptr;
+class SocketRPC : public QObject {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(SocketRPC)
+protected:
+	QLocalServer *qlsServer;
 
-	d          = new SharedMemory2Private();
-	d->hMemory = nullptr;
+public:
+	typedef QMap< QString, QVariant > ParameterMap;
+	SocketRPC(const QString &basename, QObject *p = nullptr);
+	static bool send(const QString &basename, const QString &request, const ParameterMap &param = ParameterMap());
+public slots:
+	void newConnection();
+};
 
-	if (memname.isEmpty()) {
-		// Create a new segment
-
-		for (int i = 0; i < 100; ++i) {
-			qsName     = QString::fromLatin1("Local\\MumbleOverlayMemory%1").arg(++uiIndex);
-			d->hMemory = CreateFileMappingW(INVALID_HANDLE_VALUE, nullptr, PAGE_READWRITE, 0, minsize,
-											qsName.toStdWString().c_str());
-			if (d->hMemory && GetLastError() != ERROR_ALREADY_EXISTS) {
-				break;
-			}
-
-			if (d->hMemory)
-				CloseHandle(d->hMemory);
-			d->hMemory = nullptr;
-		}
-	} else {
-		// Open existing segment
-
-		qsName     = memname;
-		d->hMemory = CreateFileMappingW(INVALID_HANDLE_VALUE, nullptr, PAGE_READWRITE, 0, minsize,
-										qsName.toStdWString().c_str());
-		qWarning("%p %lx", d->hMemory, GetLastError());
-		if (GetLastError() != ERROR_ALREADY_EXISTS) {
-			qWarning() << "SharedMemory2: Memory doesn't exist" << qsName;
-			if (d->hMemory) {
-				CloseHandle(d->hMemory);
-				d->hMemory = nullptr;
-			}
-		}
-	}
-
-	if (!d->hMemory) {
-		qWarning() << "SharedMemory2: CreateFileMapping failed for" << qsName;
-	} else {
-		a_ucData = reinterpret_cast< unsigned char * >(MapViewOfFile(d->hMemory, FILE_MAP_ALL_ACCESS, 0, 0, 0));
-
-		if (!a_ucData) {
-			qWarning() << "SharedMemory2: Failed to map memory" << qsName;
-		} else {
-			MEMORY_BASIC_INFORMATION mbi;
-			memset(&mbi, 0, sizeof(mbi));
-			if ((VirtualQuery(a_ucData, &mbi, sizeof(mbi)) == 0) || (mbi.RegionSize < minsize)) {
-				qWarning() << "SharedMemory2: Memory too small" << qsName << mbi.RegionSize;
-			} else {
-				uiSize = mbi.RegionSize;
-				return;
-			}
-		}
-	}
-
-	if (a_ucData) {
-		UnmapViewOfFile(a_ucData);
-		a_ucData = nullptr;
-	}
-	if (d->hMemory) {
-		CloseHandle(d->hMemory);
-		d->hMemory = nullptr;
-	}
-}
-
-SharedMemory2::~SharedMemory2() {
-	if (a_ucData)
-		UnmapViewOfFile(a_ucData);
-	if (d->hMemory)
-		CloseHandle(d->hMemory);
-
-	delete d;
-}
-
-void SharedMemory2::systemRelease() {
-	// This doesn't really do anything on Win32, since it has delete-on-close semantics anyway
-	if (d->hMemory) {
-		CloseHandle(d->hMemory);
-		d->hMemory = nullptr;
-	}
-}
+#endif
