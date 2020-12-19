89b9a1ed87c0410a752789c371a4c664a0ae3f4a
Jan Klass <kissaki@posteo.de>
2020-12-19 18:15:56 +0100

    New translations AudioOutputUser.h (Arabic)

--- mumble_ar.ts
+++ mumble_ar.ts
@@ -3,69 +3,34 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_AUDIOSTATS_H_
-#	define MUMBLE_MUMBLE_AUDIOSTATS_H_
+#ifndef MUMBLE_MUMBLE_AUDIOOUTPUTUSER_H_
+#define MUMBLE_MUMBLE_AUDIOOUTPUTUSER_H_
 
-#	include <QtCore/QList>
-#	include <QtCore/QTimer>
-#	include <QtCore/QtGlobal>
-#	include <QtWidgets/QWidget>
+#include <QtCore/QObject>
 
-class AudioBar : public QWidget {
+class AudioOutputUser : public QObject {
 private:
 	Q_OBJECT
-	Q_DISABLE_COPY(AudioBar)
+	Q_DISABLE_COPY(AudioOutputUser)
 protected:
-	void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
+	unsigned int iBufferSize;
+
+	/// Used to resize the buffer.
+	/// WARNING:
+	///          Audio callback is a dedicated place that can be executed
+	///          in a special thread or interrupt handler. Allocating
+	///          memory will probably crash the program!
+	void resizeBuffer(unsigned int newsize);
 
 public:
-	AudioBar(QWidget *parent = nullptr);
-	int iMin, iMax;
-	int iBelow, iAbove;
-	int iValue, iPeak;
-	QColor qcBelow, qcInside, qcAbove;
-
-	QList< QColor > qlReplacableColors;
-	QList< Qt::BrushStyle > qlReplacementBrushes;
+	AudioOutputUser(const QString &name);
+	~AudioOutputUser() Q_DECL_OVERRIDE;
+	const QString qsName;
+	float *pfBuffer = nullptr;
+	float *pfVolume = nullptr;
+	float fPos[3]   = { 0.0, 0.0, 0.0 };
+	bool bStereo;
+	virtual bool prepareSampleBuffer(unsigned int snum) = 0;
 };
 
-class AudioEchoWidget : public QWidget {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(AudioEchoWidget)
-public:
-	AudioEchoWidget(QWidget *parent);
-protected slots:
-	void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
-};
-
-class AudioNoiseWidget : public QWidget {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(AudioNoiseWidget)
-public:
-	AudioNoiseWidget(QWidget *parent);
-protected slots:
-	void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
-};
-
-#	include "ui_AudioStats.h"
-
-class AudioStats : public QDialog, public Ui::AudioStats {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(AudioStats)
-protected:
-	QTimer *qtTick;
-	bool bTalking;
-
-public:
-	AudioStats(QWidget *parent);
-	~AudioStats() Q_DECL_OVERRIDE;
-public slots:
-	void on_Tick_timeout();
-};
-
-#else
-class AudioStats;
-#endif
+#endif // AUDIOOUTPUTUSER_H_
