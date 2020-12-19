8ab360ec461a65f8e66e6a0f019b93720dab3cdb
Jan Klass <kissaki@posteo.de>
2020-12-19 18:23:38 +0100

    New translations Tokens.h (Arabic)

--- mumble_ar.ts
+++ mumble_ar.ts
@@ -3,70 +3,25 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include "Usage.h"
+#ifndef MUMBLE_MUMBLE_TOKENS_H_
+#define MUMBLE_MUMBLE_TOKENS_H_
 
-#include "ClientUser.h"
-#include "LCD.h"
-#include "NetworkConfig.h"
-#include "OSInfo.h"
-#include "Version.h"
-#include "Global.h"
+#include "ui_Tokens.h"
 
-#include <QtCore/QTimer>
-#include <QtNetwork/QHostAddress>
-#include <QtNetwork/QNetworkAccessManager>
-#include <QtNetwork/QNetworkReply>
-#include <QtXml/QDomElement>
+class Tokens : public QDialog, public Ui::Tokens {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(Tokens)
+protected:
+	QByteArray qbaDigest;
 
-Usage::Usage(QObject *p) : QObject(p) {
-	qbReport.open(QBuffer::ReadWrite);
-	qdsReport.setDevice(&qbReport);
-	qdsReport.setVersion(QDataStream::Qt_4_4);
-	qdsReport << static_cast< unsigned int >(2);
+public:
+	Tokens(QWidget *p = nullptr);
+public slots:
+	void accept() Q_DECL_OVERRIDE;
+public slots:
+	void on_qpbAdd_clicked();
+	void on_qpbRemove_clicked();
+};
 
-	// Wait 10 minutes (so we know they're actually using this), then...
-	QTimer::singleShot(60 * 10 * 1000, this, SLOT(registerUsage()));
-}
-
-void Usage::registerUsage() {
-	if (!g.s.bUsage
-		|| g.s.uiUpdateCounter == 0) // Only register usage if allowed by the user and first wizard run has finished
-		return;
-
-	QDomDocument doc;
-	QDomElement root = doc.createElement(QLatin1String("usage"));
-	doc.appendChild(root);
-
-	QDomElement tag;
-	QDomText t;
-
-	OSInfo::fillXml(doc, root);
-
-	tag = doc.createElement(QLatin1String("in"));
-	root.appendChild(tag);
-	t = doc.createTextNode(g.s.qsAudioInput);
-	tag.appendChild(t);
-
-	tag = doc.createElement(QLatin1String("out"));
-	root.appendChild(tag);
-	t = doc.createTextNode(g.s.qsAudioOutput);
-	tag.appendChild(t);
-
-	tag = doc.createElement(QLatin1String("lcd"));
-	root.appendChild(tag);
-	t = doc.createTextNode(QString::number(g.lcd->hasDevices() ? 1 : 0));
-	tag.appendChild(t);
-
-	QBuffer *qb = new QBuffer();
-	qb->setData(doc.toString().toUtf8());
-	qb->open(QIODevice::ReadOnly);
-
-	QNetworkRequest req(QUrl(QLatin1String("https://usage-report.mumble.info/v1/report")));
-	Network::prepareRequest(req);
-	req.setHeader(QNetworkRequest::ContentTypeHeader, QLatin1String("text/xml"));
-
-	QNetworkReply *rep = g.nam->post(req, qb);
-	qb->setParent(rep);
-
-	connect(rep, SIGNAL(finished()), rep, SLOT(deleteLater()));
-}
+#endif
