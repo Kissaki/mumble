dd7e90e3f2ae0708d01f7f1d3c90118279778502
Jan Klass <kissaki@posteo.de>
2020-12-19 18:14:31 +0100

    New translations XboxInput.cpp (German)

--- mumble_de.ts
+++ mumble_de.ts
@@ -1,199 +1,69 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>VoiceRecorderDialog</class>
- <widget class="QDialog" name="VoiceRecorderDialog">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>450</width>
-    <height>255</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>Recorder</string>
-  </property>
-  <property name="windowIcon">
-   <iconset>
-    <normaloff>:/actions/media-record.svg</normaloff>:/actions/media-record.svg</iconset>
-  </property>
-  <layout class="QGridLayout" name="gridLayout_2">
-   <item row="0" column="0" colspan="2">
-    <widget class="QGroupBox" name="qgbControl">
-     <property name="title">
-      <string>Control</string>
-     </property>
-     <layout class="QHBoxLayout" name="horizontalLayout_2">
-      <item>
-       <widget class="QLabel" name="qlTime">
-        <property name="font">
-         <font>
-          <pointsize>20</pointsize>
-          <weight>50</weight>
-          <bold>false</bold>
-          <underline>false</underline>
-         </font>
-        </property>
-        <property name="text">
-         <string>00:00:00</string>
-        </property>
-        <property name="alignment">
-         <set>Qt::AlignCenter</set>
-        </property>
-       </widget>
-      </item>
-      <item>
-       <spacer name="horizontalSpacer">
-        <property name="orientation">
-         <enum>Qt::Horizontal</enum>
-        </property>
-        <property name="sizeType">
-         <enum>QSizePolicy::Minimum</enum>
-        </property>
-        <property name="sizeHint" stdset="0">
-         <size>
-          <width>20</width>
-          <height>20</height>
-         </size>
-        </property>
-       </spacer>
-      </item>
-      <item>
-       <widget class="QPushButton" name="qpbStart">
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Expanding" vsizetype="Fixed">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-        <property name="text">
-         <string>&amp;Start</string>
-        </property>
-       </widget>
-      </item>
-      <item>
-       <widget class="QPushButton" name="qpbStop">
-        <property name="enabled">
-         <bool>false</bool>
-        </property>
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Expanding" vsizetype="Fixed">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-        <property name="text">
-         <string>S&amp;top</string>
-        </property>
-       </widget>
-      </item>
-     </layout>
-    </widget>
-   </item>
-   <item row="1" column="1">
-    <widget class="QGroupBox" name="qgbMode">
-     <property name="title">
-      <string>Mode</string>
-     </property>
-     <layout class="QVBoxLayout" name="verticalLayout">
-      <item>
-       <widget class="QRadioButton" name="qrbDownmix">
-        <property name="enabled">
-         <bool>true</bool>
-        </property>
-        <property name="text">
-         <string>Downmix</string>
-        </property>
-        <property name="checkable">
-         <bool>true</bool>
-        </property>
-        <property name="checked">
-         <bool>true</bool>
-        </property>
-       </widget>
-      </item>
-      <item>
-       <widget class="QRadioButton" name="qrbMultichannel">
-        <property name="text">
-         <string>Multichannel</string>
-        </property>
-       </widget>
-      </item>
-      <item>
-       <spacer name="verticalSpacer">
-        <property name="orientation">
-         <enum>Qt::Vertical</enum>
-        </property>
-        <property name="sizeHint" stdset="0">
-         <size>
-          <width>20</width>
-          <height>40</height>
-         </size>
-        </property>
-       </spacer>
-      </item>
-     </layout>
-    </widget>
-   </item>
-   <item row="1" column="0">
-    <widget class="QGroupBox" name="qgbOutput">
-     <property name="title">
-      <string>Output</string>
-     </property>
-     <layout class="QFormLayout" name="formLayout">
-      <item row="0" column="0">
-       <widget class="QLabel" name="qlOutputFormat">
-        <property name="text">
-         <string>Output format</string>
-        </property>
-       </widget>
-      </item>
-      <item row="0" column="1">
-       <widget class="MUComboBox" name="qcbFormat"/>
-      </item>
-      <item row="2" column="0">
-       <widget class="QLabel" name="qlTargetDirectory">
-        <property name="text">
-         <string>Target directory</string>
-        </property>
-       </widget>
-      </item>
-      <item row="4" column="0">
-       <widget class="QLabel" name="qlFilename">
-        <property name="text">
-         <string>Filename</string>
-        </property>
-       </widget>
-      </item>
-      <item row="2" column="1">
-       <layout class="QHBoxLayout" name="qhblTargetDirectory">
-        <item>
-         <widget class="QLineEdit" name="qleTargetDirectory"/>
-        </item>
-        <item>
-         <widget class="QPushButton" name="qpbTargetDirectoryBrowse">
-          <property name="text">
-           <string>&amp;Browse...</string>
-          </property>
-         </widget>
-        </item>
-       </layout>
-      </item>
-      <item row="4" column="1">
-       <widget class="QLineEdit" name="qleFilename"/>
-      </item>
-     </layout>
-    </widget>
-   </item>
-  </layout>
- </widget>
- <customwidgets>
-  <customwidget>
-   <class>MUComboBox</class>
-   <extends>QComboBox</extends>
-   <header>widgets/MUComboBox.h</header>
-  </customwidget>
- </customwidgets>
- <resources/>
- <connections/>
-</ui>
+// Copyright 2005-2020 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#include "XboxInput.h"
+
+#include <QtCore/QStringList>
+
+const QUuid XboxInput::s_XboxInputGuid = QUuid(QString::fromLatin1("ca3937e3-640c-4d9e-9ef3-903f8b4fbcab"));
+
+XboxInput::XboxInput()
+	: GetState(nullptr), m_getStateFunc(nullptr), m_getStateExFunc(nullptr), m_xinputlib(nullptr), m_valid(false) {
+	// Load the most suitable XInput DLL available.
+	//
+	// We prefer 1_4 and 1_3 over the others because they provide
+	// XInputGetStateEx(), which allows us to query the state of
+	// the guide button.
+	//
+	// See https://msdn.microsoft.com/en-us/library/windows/desktop/hh405051(v=vs.85).aspx
+	// for more information.
+	QStringList alternatives;
+	alternatives << QLatin1String("XInput1_4.dll");
+	alternatives << QLatin1String("xinput1_3.dll");
+	alternatives << QLatin1String("XInput9_1_0.dll");
+	alternatives << QLatin1String("xinput1_2.dll");
+	alternatives << QLatin1String("xinput1_1.dll");
+
+	foreach (const QString &lib, alternatives) {
+		m_xinputlib = LoadLibraryW(reinterpret_cast< const wchar_t * >(lib.utf16()));
+		if (m_xinputlib) {
+			qWarning("XboxInput: using XInput DLL '%s'", qPrintable(lib));
+			m_valid = true;
+			break;
+		}
+	}
+
+	if (!m_valid) {
+		qWarning("XboxInput: no valid XInput DLL was found on the system.");
+		return;
+	}
+
+	m_getStateFunc = reinterpret_cast< XboxInputGetStateFunc >(GetProcAddress(m_xinputlib, "XInputGetState"));
+	// Undocumented XInputGetStateEx -- ordinal 100. It is available in XInput 1.3 and greater.
+	// It provides access to the state of the guide button.
+	// For reference, see SDL's XInput support: http://www.libsdl.org/tmp/SDL/src/core/windows/SDL_xinput.c
+	m_getStateExFunc = reinterpret_cast< XboxInputGetStateFunc >(GetProcAddress(m_xinputlib, (char *) 100));
+
+	if (m_getStateExFunc) {
+		GetState = m_getStateExFunc;
+		qWarning("XboxInput: using XInputGetStateEx() as querying function.");
+	} else if (m_getStateFunc) {
+		GetState = m_getStateFunc;
+		qWarning("XboxInput: using XInputGetState() as querying function.");
+	} else {
+		m_valid = false;
+		qWarning("XboxInput: no valid querying function found.");
+	}
+}
+
+XboxInput::~XboxInput() {
+	if (m_xinputlib) {
+		FreeLibrary(m_xinputlib);
+	}
+}
+
+bool XboxInput::isValid() const {
+	return m_valid;
+}
