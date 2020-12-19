912527a3150828472e51ffe5d95dd705613cf45f
Jan Klass <kissaki@posteo.de>
2020-12-19 18:33:23 +0100

    New translations OverlayText.h (Basque)

--- mumble_eu.ts
+++ mumble_eu.ts
@@ -3,29 +3,63 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include "PTTButtonWidget.h"
+#ifndef MUMBLE_MUMBLE_OVERLAYTEXT_H_
+#define MUMBLE_MUMBLE_OVERLAYTEXT_H_
 
-#include "Global.h"
+#include <QtGui/QFont>
+#include <QtGui/QPainterPath>
+#include <QtGui/QPixmap>
 
-PTTButtonWidget::PTTButtonWidget(QWidget *p) : QWidget(p) {
-	setupUi(this);
+//! Annotated QPixmap supplying a basepoint.
+class BasepointPixmap : public QPixmap {
+public:
+	//! Local coordinates of the base point.
+	QPoint qpBasePoint;
+	//@{
+	/**
+	 * Font ascent and descent.
+	 * The pixmap may exceed those font metrics, so if you need to
+	 * transform rendered text properly, use these attributes.
+	 */
+	int iAscent;
+	int iDescent;
+	//@}
 
-	setWindowFlags(Qt::Tool | Qt::WindowStaysOnTopHint);
+	BasepointPixmap();
+	//! Create from QPixmap, basepoint is bottom left.
+	BasepointPixmap(const QPixmap &);
+	//! Empty pixmap, basepoint is bottom left.
+	BasepointPixmap(int, int);
+	//! Empty pixmap with specified basepoint.
+	BasepointPixmap(int, int, const QPoint &);
+};
 
-	if (!g.s.qbaPTTButtonWindowGeometry.isEmpty()) {
-		restoreGeometry(g.s.qbaPTTButtonWindowGeometry);
-	}
-}
+class OverlayTextLine {
+private:
+	const float fEdgeFactor;
 
-void PTTButtonWidget::closeEvent(QCloseEvent *e) {
-	g.s.qbaPTTButtonWindowGeometry = saveGeometry();
-	QWidget::closeEvent(e);
-}
+	QString qsText;
+	QFont qfFont;
+	QPainterPath qpp;
+	float fAscent, fDescent;
+	float fXCorrection, fYCorrection;
+	int iCurWidth, iCurHeight;
+	float fEdge;
+	float fBaseliningThreshold;
+	bool bElided;
 
-void PTTButtonWidget::on_qpbPushToTalk_pressed() {
-	emit triggered(true, QVariant());
-}
+	BasepointPixmap render(int, int, const QColor &, const QPoint &) const;
 
-void PTTButtonWidget::on_qpbPushToTalk_released() {
-	emit triggered(false, QVariant());
-}
+public:
+	OverlayTextLine(const QString &, const QFont &);
+
+	void setFont(const QFont &);
+	void setEdge(float);
+
+	//! Render text with current font.
+	BasepointPixmap createPixmap(QColor col);
+	//! Render text to fit a bounding box.
+	BasepointPixmap createPixmap(unsigned int maxwidth, unsigned int height, QColor col);
+};
+
+#endif //_OVERLAYTEXT_H
