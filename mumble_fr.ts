c62f74e1d8bd54e4985fcdf32cd50a5c97bbe6ca
Jan Klass <kissaki@posteo.de>
2020-12-19 17:59:41 +0100

    New translations UserLockFile_win.cpp (French)

--- mumble_fr.ts
+++ mumble_fr.ts
@@ -3,48 +3,35 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_LOCKFILE_H_
-#define MUMBLE_MUMBLE_LOCKFILE_H_
+#include "UserLockFile.h"
 
-#include <QtCore/QtGlobal>
+UserLockFile::UserLockFile(const QString &lockFilePath) : m_handle(0), m_path(lockFilePath) {
+}
 
-#ifdef Q_OS_WIN
-#	include "win.h"
-#endif
+UserLockFile::~UserLockFile() {
+	release();
+}
 
-#include <QtCore/QString>
+QString UserLockFile::path() const {
+	return m_path;
+}
 
-/// UserLockFile implements an atomic lock file
-/// that can be used as a mutex between different
-/// processes run by the same user.
-class UserLockFile {
-#if defined(Q_OS_WIN)
-	HANDLE m_handle;
-#endif
-	QString m_path;
+bool UserLockFile::acquire() {
+	if (m_handle != 0) {
+		return false;
+	}
 
-public:
-	/// Constructs a LockFile at path.
-	/// The path should be somewhere
-	/// owned by the current user, such
-	/// as inside the home directory of
-	/// the user. This is to avoid clashing
-	/// with other lock files.
-	UserLockFile(const QString &path);
+	m_handle = CreateFile(reinterpret_cast< const wchar_t * >(m_path.utf16()), GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS,
+						  FILE_ATTRIBUTE_HIDDEN, nullptr);
+	if (m_handle == INVALID_HANDLE_VALUE && GetLastError() == ERROR_SHARING_VIOLATION) {
+		return false;
+	}
+	return true;
+}
 
-	/// Destroys the LockFile, and ensures
-	/// that it is released.
-	~UserLockFile();
-
-	/// Returns the path that the lock file
-	/// exists at.
-	QString path() const;
-
-	/// Acquires the lock file.
-	bool acquire();
-
-	/// Releases the lock file.
-	void release();
-};
-
-#endif
+void UserLockFile::release() {
+	if (m_handle != 0) {
+		CloseHandle(m_handle);
+		m_handle = 0;
+	}
+}
