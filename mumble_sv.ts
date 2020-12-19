91d69d77c1730f3d47762365d3eab7f2cdc98fad
Jan Klass <kissaki@posteo.de>
2020-12-19 19:01:29 +0100

    New translations Themes.h (Swedish)

--- mumble_sv.ts
+++ mumble_sv.ts
@@ -3,23 +3,66 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_SVGICON_H
-#define MUMBLE_MUMBLE_SVGICON_H
+#ifndef MUMBLE_MUMBLE_THEMES_H_
+#define MUMBLE_MUMBLE_THEMES_H_
 
-#include <QIcon>
-#include <QString>
+#include <Settings.h>
+#include <ThemeInfo.h>
+#ifndef Q_MOC_RUN
+#	include <boost/optional.hpp>
+#endif
 
-class SvgIcon {
+class Themes {
 public:
-	/// addSvgPixmapsToIcon renders the SVG file at |fn| in various
-	/// common sizes from 8x8 up to 256x256 and adds the resulting
-	/// rasterized pixmaps to |icon|.
+	/// Returns the style configured in the given settings structure
+	static boost::optional< ThemeInfo::StyleInfo > getConfiguredStyle(const Settings &settings);
+
+	/// Updates the given settings object to be configured to the given style
 	///
-	/// This method is only needed due to an odd interaction between
-	/// Qt and Plasma/KStatusNotifierItem. See
-	/// https://github.com/mumble-voip/mumble/issues/3374
-	/// for more information.
-	static void addSvgPixmapsToIcon(QIcon &icon, QString fn);
+	/// @note Does not apply the theme @see apply
+	///
+	/// @param settings Settings object to update
+	/// @param style Style to set
+	/// @param outChanged Will be set to true if the style in settings actually changed. Will not be changed otherwise.
+	static void setConfiguredStyle(Settings &settings, boost::optional< ThemeInfo::StyleInfo > style, bool &outChanged);
+
+	/// Applies the theme
+	///
+	/// @note Can only apply a theme before MainWindow etc. is opened
+	static bool apply();
+
+	/// Return a theme name to theme map
+	static ThemeMap getThemes();
+
+	/// Returns the per user themes directory
+	static QDir getUserThemesDirectory();
+
+private:
+	/// Applies the fallback stylesheet
+	static void applyFallback();
+
+	/// Tries to apply the configured theme.
+	/// @return True on success. False on failure.
+	static bool applyConfigured();
+
+	// Sets the theme to a QSS theme
+	static void setTheme(QString &themeQss, QStringList &skinPaths);
+
+	/// Returns list of theme search directories ordered ascending by priorty (lowest first)
+	static QVector< QDir > getSearchDirectories();
+
+	/// Returns default style-sheet used for fall-backs
+	static QString getDefaultStylesheet();
+
+	/// userStylesheetPath returns the absolute path to the
+	/// user.qss file.
+	static QString userStylesheetPath();
+
+	/// readStylesheet fills stylesheetContent with the content
+	/// of the file at stylesheetFn, if available.
+	/// If a the file is is available, the function returns true.
+	/// If no file is available, it returns false.
+	static bool readStylesheet(const QString &stylesheetFn, QString &stylesheetContent);
 };
 
-#endif
+#endif // MUMBLE_MUMBLE_THEMES_H_
