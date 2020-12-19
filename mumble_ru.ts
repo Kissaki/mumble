4eecbc03fc5a9f3bc3489f43636e0ce6eb7a2f88
Jan Klass <kissaki@posteo.de>
2020-12-19 19:17:03 +0100

    New translations DBus.h (Russian)

--- mumble_ru.ts
+++ mumble_ru.ts
@@ -3,96 +3,44 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_CUSTOMELEMENTS_H_
-#define MUMBLE_MUMBLE_CUSTOMELEMENTS_H_
+#ifndef MUMBLE_MUMBLE_DBUS_H_
+#define MUMBLE_MUMBLE_DBUS_H_
 
-#include <QtCore/QObject>
-#include <QtWidgets/QLabel>
-#include <QtWidgets/QTextBrowser>
-#include <QtWidgets/QTextEdit>
+#include <QtDBus/QDBusAbstractAdaptor>
 
-class LogTextBrowser : public QTextBrowser {
+class QDBusMessage;
+
+class MumbleDBus : public QDBusAbstractAdaptor {
 private:
 	Q_OBJECT
-	Q_DISABLE_COPY(LogTextBrowser)
-protected:
-	void resizeEvent(QResizeEvent *e) Q_DECL_OVERRIDE;
-	bool event(QEvent *e) Q_DECL_OVERRIDE;
-
+	Q_CLASSINFO("D-Bus Interface", "net.sourceforge.mumble.Mumble")
+	Q_DISABLE_COPY(MumbleDBus)
+	Q_PROPERTY(bool mute READ isSelfMuted WRITE setSelfMuted)
+	Q_PROPERTY(bool deaf READ isSelfDeaf WRITE setSelfDeaf)
 public:
-	LogTextBrowser(QWidget *p = nullptr);
-
-	int getLogScroll();
-	int getLogScrollMaximum();
-	void setLogScroll(int scroll_pos);
-	void scrollLogToBottom();
-};
-
-class ChatbarTextEdit : public QTextEdit {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(ChatbarTextEdit)
-	void inFocus(bool);
-	QStringList qslHistory;
-	QString qsHistoryTemp;
-	int iHistoryIndex;
-	static const int MAX_HISTORY = 50;
-
-protected:
-	QString qsDefaultText;
-	bool bDefaultVisible;
-	void focusInEvent(QFocusEvent *) Q_DECL_OVERRIDE;
-	void focusOutEvent(QFocusEvent *) Q_DECL_OVERRIDE;
-	void contextMenuEvent(QContextMenuEvent *) Q_DECL_OVERRIDE;
-	void dragEnterEvent(QDragEnterEvent *) Q_DECL_OVERRIDE;
-	void dragMoveEvent(QDragMoveEvent *) Q_DECL_OVERRIDE;
-	void dropEvent(QDropEvent *) Q_DECL_OVERRIDE;
-	bool event(QEvent *) Q_DECL_OVERRIDE;
-	QSize minimumSizeHint() const Q_DECL_OVERRIDE;
-	QSize sizeHint() const Q_DECL_OVERRIDE;
-	void resizeEvent(QResizeEvent *e) Q_DECL_OVERRIDE;
-	void insertFromMimeData(const QMimeData *source) Q_DECL_OVERRIDE;
-	bool sendImagesFromMimeData(const QMimeData *source);
-
-public:
-	void setDefaultText(const QString &, bool = false);
-	unsigned int completeAtCursor();
-signals:
-	void tabPressed(void);
-	void backtabPressed(void);
-	void ctrlSpacePressed(void);
-	void entered(QString);
-	void pastedImage(QString);
+	MumbleDBus(QObject *parent);
 public slots:
-	void pasteAndSend_triggered();
-	void doResize();
-	void doScrollbar();
-	void addToHistory(const QString &str);
-	void historyUp();
-	void historyDown();
+	void openUrl(const QString &url, const QDBusMessage &);
+	void getCurrentUrl(const QDBusMessage &);
+	void getTalkingUsers(const QDBusMessage &);
+	void focus();
 
-public:
-	ChatbarTextEdit(QWidget *p = nullptr);
+	/// Change when Mumble transmits voice.
+	///
+	/// @param mode The new transmit mode (0 = continous, 1 = voice activity, 2 = push-to-talk)
+	void setTransmitMode(unsigned int mode, const QDBusMessage &);
+
+	/// Get the current transmit mode.
+	///
+	/// @return The current transmit mode (0 = continous, 1 = voice activity, 2 = push-to-talk)
+	unsigned int getTransmitMode();
+
+	void setSelfMuted(bool mute);
+	void setSelfDeaf(bool deafen);
+	bool isSelfMuted();
+	bool isSelfDeaf();
+	void startTalking();
+	void stopTalking();
 };
 
-class DockTitleBar : public QLabel {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(DockTitleBar)
-protected:
-	QTimer *qtTick;
-	int size;
-	int newsize;
-
-public:
-	DockTitleBar();
-	QSize sizeHint() const Q_DECL_OVERRIDE;
-	QSize minimumSizeHint() const Q_DECL_OVERRIDE;
-public slots:
-	void tick();
-
-protected:
-	bool eventFilter(QObject *, QEvent *) Q_DECL_OVERRIDE;
-};
-
-#endif // CUSTOMELEMENTS_H_
+#endif
