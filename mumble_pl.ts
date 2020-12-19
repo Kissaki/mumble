1113d0eef3694a1d405e07db50974b333755a2b0
Jan Klass <kissaki@posteo.de>
2020-12-19 18:46:21 +0100

    New translations ApplicationPaletteTemplate.h (Polish)

--- mumble_pl.ts
+++ mumble_pl.ts
@@ -3,270 +3,93 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_AUDIOINPUT_H_
-#define MUMBLE_MUMBLE_AUDIOINPUT_H_
+// See scripts/generate-ApplicationPalette-class.py
 
-#include <QtCore/QObject>
-#include <QtCore/QThread>
-#include <boost/array.hpp>
-#include <boost/shared_ptr.hpp>
-#include <fstream>
-#include <list>
-#include <mutex>
-#include <speex/speex.h>
-#include <speex/speex_echo.h>
-#include <speex/speex_preprocess.h>
-#include <speex/speex_resampler.h>
-#include <vector>
+% (warning) s
+#ifndef APPLICATIONPALETTE_H
+#define APPLICATIONPALETTE_H
 
-#include "Audio.h"
-#include "Message.h"
-#include "Settings.h"
-#include "Timer.h"
-
-class AudioInput;
-class CELTCodec;
-class OpusCodec;
-struct CELTEncoder;
-struct OpusEncoder;
-struct DenoiseState;
-typedef boost::shared_ptr< AudioInput > AudioInputPtr;
-
-/**
- * A chunk of audio data to process
- * This struct wraps pointers to two dynamically allocated arrays, containing
- * PCM samples of microphone and speaker readback data (for echo cancellation).
- * Does not handle pointer ownership, so you'll have to deallocate them yourself.
- */
-struct AudioChunk {
-	AudioChunk() : mic(nullptr), speaker(nullptr) {}
-	explicit AudioChunk(short *mic) : mic(mic), speaker(nullptr) {}
-	AudioChunk(short *mic, short *speaker) : mic(mic), speaker(speaker) {}
-	bool empty() const { return mic == nullptr; }
-
-	short *mic;     ///< Pointer to microphone samples
-	short *speaker; ///< Pointer to speaker samples, nullptr if echo cancellation is disabled
-};
-
-/*
- * According to https://www.speex.org/docs/manual/speex-manual/node7.html
- * "It is important that, at any time, any echo that is present in the input
- * has already been sent to the echo canceller as echo_frame."
- * Thus, we artificially introduce a small lag in the microphone by means of
- * a queue, so as to be sure the speaker data always precedes the microphone.
- *
- * There are conflicting requirements for the queue:
- * - it has to be small enough not to cause a noticeable lag in the voice
- * - it has to be large enough not to force us to drop packets frequently
- *   when the addMic() and addEcho() callbacks are called in a jittery way
- * - its fill level must be controlled so it does not operate towards zero
- *   elements size, as this would not provide the lag required for the
- *   echo canceller to work properly.
- *
- * The current implementation uses a 5 elements queue, with a control
- * statemachine that introduces packet drops to control the fill level
- * to at least 2 (plus or minus one) and less than 4 elements.
- * With a 10ms chunk, this queue should introduce a ~20ms lag to the voice.
- */
-class Resynchronizer {
-public:
-	/**
-	 * Add a microphone sample to the resynchronizer queue
-	 * The resynchronizer may decide to drop the sample, and in that case
-	 * the pointer will be deallocated not lo leak memory
-	 *
-	 * \param mic pointer to a dynamically allocated  array with PCM data
-	 */
-	void addMic(short *mic);
-
-	/**
-	 * Add a speaker sample to the resynchronizer
-	 * The resynchronizer may decide to drop the sample, and in that case
-	 * the pointer will be deallocated not lo leak memory
-	 *
-	 * \param mic pointer to a dynamically allocated array with PCM data
-	 * \return If microphone data is available, the resynchronizer will return a
-	 * valid audio chunk to encode, otherwise an empty chunk will be returned
-	 */
-	AudioChunk addSpeaker(short *speaker);
-
-	/**
-	 * Reinitialize the resynchronizer, emptying the queue in the process.
-	 */
-	void reset();
-
-	/**
-	 * \return the nominal lag that the resynchronizer tries to enforce on the
-	 * microphone data, in order to make sure the speaker data is always passed
-	 * first to the echo canceller
-	 */
-	int getNominalLag() const { return 2; }
-
-	~Resynchronizer();
-
-	bool bDebugPrintQueue = false; ///< Enables printing queue fill level stats
-
-private:
-	/**
-	 * Print queue level stats for debugging purposes
-	 * \param mic used to distinguish between addMic() and addSpeaker()
-	 */
-	void printQueue(char who);
-
-	// TODO: there was a mutex (qmEcho), but can the callbacks be called concurrently?
-	mutable std::mutex m;
-	std::list< short * > micQueue;                          ///< Queue of microphone samples
-	enum { S0, S1a, S1b, S2, S3, S4a, S4b, S5 } state = S0; ///< Queue fill control statemachine
-};
-
-class AudioInputRegistrar {
-private:
-	Q_DISABLE_COPY(AudioInputRegistrar)
-public:
-	static QMap< QString, AudioInputRegistrar * > *qmNew;
-	static QString current;
-	static AudioInputPtr newFromChoice(QString choice = QString());
-
-	const QString name;
-	int priority;
-
-	AudioInputRegistrar(const QString &n, int priority = 0);
-	virtual ~AudioInputRegistrar();
-	virtual AudioInput *create()                               = 0;
-	virtual const QList< audioDevice > getDeviceChoices()      = 0;
-	virtual void setDeviceChoice(const QVariant &, Settings &) = 0;
-	virtual bool canEcho(const QString &outputsys) const       = 0;
-	virtual bool canExclusive() const;
-};
-
-class AudioInput : public QThread {
-	friend class AudioNoiseWidget;
-	friend class AudioEchoWidget;
-	friend class AudioStats;
-	friend class AudioInputDialog;
-
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(AudioInput)
-protected:
-	typedef enum { CodecCELT, CodecSpeex } CodecFormat;
-	typedef enum { SampleShort, SampleFloat } SampleFormat;
-	typedef void (*inMixerFunc)(float *RESTRICT, const void *RESTRICT, unsigned int, unsigned int, quint64);
-
-private:
-	bool bDebugDumpInput;                           ///< When true, dump pcm data to debug the echo canceller
-	std::ofstream outMic, outSpeaker, outProcessed; ///< Files to dump raw pcm data
-
-	SpeexResamplerState *srsMic, *srsEcho;
-
-	unsigned int iMicFilled, iEchoFilled;
-	inMixerFunc imfMic, imfEcho;
-	inMixerFunc chooseMixer(const unsigned int nchan, SampleFormat sf, quint64 mask);
-	void resetAudioProcessor();
-
-	OpusCodec *oCodec;
-	OpusEncoder *opusState;
-	DenoiseState *denoiseState;
-	bool selectCodec();
-	void selectNoiseCancel();
-
-	typedef boost::array< unsigned char, 960 > EncodingOutputBuffer;
-
-	int encodeOpusFrame(short *source, int size, EncodingOutputBuffer &buffer);
-	int encodeCELTFrame(short *pSource, EncodingOutputBuffer &buffer);
-
-protected:
-	MessageHandler::UDPMessageType umtType;
-	SampleFormat eMicFormat, eEchoFormat;
-
-	unsigned int iMicChannels, iEchoChannels;
-	unsigned int iMicFreq, iEchoFreq;
-	unsigned int iMicLength, iEchoLength;
-	unsigned int iMicSampleSize, iEchoSampleSize;
-	int iEchoMCLength, iEchoFrameSize;
-	quint64 uiMicChannelMask, uiEchoChannelMask;
-
-	bool bEchoMulti;
-	Settings::NoiseCancel noiseCancel;
-	static const unsigned int iSampleRate = SAMPLE_RATE;
-	static const int iFrameSize           = SAMPLE_RATE / 100;
-
-	QMutex qmSpeex;
-	SpeexPreprocessState *sppPreprocess;
-	SpeexEchoState *sesEcho;
-
-	CELTCodec *cCodec;
-	CELTEncoder *ceEncoder;
-
-	/// bResetEncoder is a flag that notifies
-	/// our encoder functions that the encoder
-	/// needs to be reset.
-	bool bResetEncoder;
-
-	/// Encoded audio rate in bit/s
-	int iAudioQuality;
-	bool bAllowLowDelay;
-	/// Number of 10ms audio "frames" per packet (!= frames in packet)
-	int iAudioFrames;
-
-	float *pfMicInput;
-	float *pfEchoInput;
-
-	Resynchronizer resync;
-	std::vector< short > opusBuffer;
-
-	void encodeAudioFrame(AudioChunk chunk);
-	void addMic(const void *data, unsigned int nsamp);
-	void addEcho(const void *data, unsigned int nsamp);
-
-	volatile bool bRunning;
-	volatile bool bPreviousVoice;
-
-	int iFrameCounter;
-	int iSilentFrames;
-	int iHoldFrames;
-	int iBufferedFrames;
-
-	QList< QByteArray > qlFrames;
-	void flushCheck(const QByteArray &, bool terminator, int voiceTargetID);
-
-	void initializeMixer();
-
-	static void adjustBandwidth(int bitspersec, int &bitrate, int &frames, bool &allowLowDelay);
-signals:
-	void doDeaf();
-	void doMute();
-
-public:
-	typedef enum { ActivityStateIdle, ActivityStateReturnedFromIdle, ActivityStateActive } ActivityState;
-
-	ActivityState activityState;
-
-	bool bResetProcessor;
-
-	Timer tIdle;
-
-	int iBitrate;
-	float dPeakSpeaker, dPeakSignal, dMaxMic, dPeakMic, dPeakCleanMic;
-	float fSpeechProb;
-
-	static int getNetworkBandwidth(int bitrate, int frames);
-	static void setMaxBandwidth(int bitspersec);
-
-	/// Construct an AudioInput.
-	///
-	/// This constructor is only ever called by Audio::startInput(), and is guaranteed
-	/// to be called on the application's main thread.
-	AudioInput();
-
-	/// Destroy an AudioInput.
-	///
-	/// This destructor is only ever called by Audio::stopInput() and Audio::stop(),
-	/// and is guaranteed to be called on the application's main thread.
-	~AudioInput() Q_DECL_OVERRIDE;
-	void run() Q_DECL_OVERRIDE = 0;
-	virtual bool isAlive() const;
-	bool isTransmitting() const;
-};
-
+#include <QTimer>
+#include <QWidget>
+#ifndef Q_MOC_RUN
+#	include <boost/optional.hpp>
 #endif
+#include <QApplication>
+#include <QDebug>
+
+	///
+	/// Class enabling theming of QApplication::palette from stylesheets.
+	///
+	/// QPalette cannot be styled which creates issues as not all
+	/// GUI elements in Qt can be styled. This class works around
+	/// that by offering a QPROPERTY for each color role and group
+	/// combination in QPalette. As you can set custom QPROPERTYs
+	/// from stylesheet this allows the user to set all relevant
+	/// palette brushes from the stylesheet.
+	///
+	/// Due to restrictions on allowed property names as well as a
+	/// mandatory prefix the attributes are exposed as lower cased:
+	/// "qproperty-<role>_<group>".
+	///
+	/// So a group of QPalette::Active and QPalette::Text role
+	/// would be styled by:
+	///
+	/// ApplicationPalette {
+	///     qproperty-text_active: #ff0000; /* Set color for active group */
+	/// }
+	///
+	/// See http://qt-project.org/doc/qt-4.8/qpalette.html#ColorGroup-enum
+	/// for the available groups and roles.
+	///
+	/// You can also use the shorthand "qproperty-<role>" to set all groups
+	/// to the same brush.
+	///
+	/// The class will automatically pick up style changes on itself
+	/// and update the application palette accordingly. To use the class
+	/// simply instantiate it before setting the theme and keep it around
+	/// till the application terminates.
+	///
+	class ApplicationPalette : public QWidget {
+	Q_OBJECT
+		% (properties) s public : explicit ApplicationPalette(QWidget *p = 0)
+		: QWidget(p),
+	m_originalPalette(QApplication::palette()){
+		// Empty
+	}
+
+		% (getterssetters) s
+
+		private slots : void updateApplicationPalette() {
+		qWarning() << "Updating application palette";
+
+		QPalette newPalette = m_originalPalette; // Do not re-use potentially already styled palette. Might not pick up
+												 // system style changes though.
+
+		% (paletteupdates) s
+
+				QApplication::setPalette(newPalette);
+		resetAllProperties();
+	}
+
+	void resetAllProperties() { % (propertyresets) s }
+
+protected:
+	bool event(QEvent *e) Q_DECL_OVERRIDE {
+		bool result = QWidget::event(e);
+
+		if (e->type() == QEvent::StyleChange) {
+			// Update global palette. Have to defer it
+			// as property updates are also signals.
+			QTimer::singleShot(0, this, SLOT(updateApplicationPalette()));
+		}
+
+		return result;
+	}
+
+private:
+	const QPalette m_originalPalette;
+
+	% (variables) s
+};
+
+#endif // APPLICATIONPALETTE_H
