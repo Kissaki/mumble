6a20b7261dedee7b4d494951137119a287fe5c83
Jan Klass <kissaki@posteo.de>
2020-12-19 18:32:56 +0100

    New translations MumbleApplication.cpp (Basque)

--- mumble_eu.ts
+++ mumble_eu.ts
@@ -3,60 +3,99 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_MUMBLEAPPLICATION_H
-#define MUMBLE_MUMBLE_MUMBLEAPPLICATION_H
+#include "MumbleApplication.h"
 
-#include <QApplication>
-#include <QUrl>
+#include "EnvUtils.h"
+#include "MainWindow.h"
+#include "GlobalShortcut.h"
 
-#ifdef Q_OS_WIN
-#	include <QAbstractNativeEventFilter>
+#if defined(Q_OS_WIN)
+#	include "GlobalShortcut_win.h"
 #endif
 
-/**
- * @brief Implements custom system shutdown behavior as well as event filtering.
- */
+#include <QtGui/QFileOpenEvent>
+
+// We define a global macro called 'g'. This can lead to issues when included code uses 'g' as a type or parameter name
+// (like protobuf 3.7 does). As such, for now, we have to make this our last include.
+#include "Global.h"
+
+MumbleApplication *MumbleApplication::instance() {
+	return static_cast< MumbleApplication * >(QCoreApplication::instance());
+}
+
+MumbleApplication::MumbleApplication(int &pargc, char **pargv) : QApplication(pargc, pargv) {
+	connect(this, SIGNAL(commitDataRequest(QSessionManager &)), SLOT(onCommitDataRequest(QSessionManager &)),
+			Qt::DirectConnection);
+}
+
+QString MumbleApplication::applicationVersionRootPath() {
+	QString versionRoot = EnvUtils::getenv(QLatin1String("MUMBLE_VERSION_ROOT"));
+	if (versionRoot.count() > 0) {
+		return versionRoot;
+	}
+	return this->applicationDirPath();
+}
+
+void MumbleApplication::onCommitDataRequest(QSessionManager &) {
+	// Make sure the config is saved and supress the ask on quit message
+	if (g.mw) {
+		g.s.save();
+		g.mw->bSuppressAskOnQuit = true;
+		qWarning() << "Session likely ending. Suppressing ask on quit";
+	}
+}
+
+bool MumbleApplication::event(QEvent *e) {
+	if (e->type() == QEvent::FileOpen) {
+		QFileOpenEvent *foe = static_cast< QFileOpenEvent * >(e);
+		if (!g.mw) {
+			this->quLaunchURL = foe->url();
+		} else {
+			g.mw->openUrl(foe->url());
+		}
+		return true;
+	}
+	return QApplication::event(e);
+}
+
 #ifdef Q_OS_WIN
-class MumbleApplication : public QApplication, public QAbstractNativeEventFilter {
-#else
-class MumbleApplication : public QApplication {
+/// gswForward forwards a native Windows keyboard/mouse message
+/// into GlobalShortcutWin's event stream.
+///
+/// @return  Returns true if the forwarded event was suppressed
+///          by GlobalShortcutWin. Otherwise, returns false.
+static bool gswForward(MSG *msg) {
+	GlobalShortcutWin *gsw = static_cast< GlobalShortcutWin * >(GlobalShortcutEngine::engine);
+	if (!gsw) {
+		return false;
+	}
+	switch (msg->message) {
+		case WM_LBUTTONDOWN:
+		case WM_LBUTTONUP:
+		case WM_RBUTTONDOWN:
+		case WM_RBUTTONUP:
+		case WM_MBUTTONDOWN:
+		case WM_MBUTTONUP:
+		case WM_XBUTTONDOWN:
+		case WM_XBUTTONUP:
+			return gsw->injectMouseMessage(msg);
+		case WM_KEYDOWN:
+		case WM_KEYUP:
+		case WM_SYSKEYDOWN:
+		case WM_SYSKEYUP:
+			return gsw->injectKeyboardMessage(msg);
+	}
+	return false;
+}
+
+bool MumbleApplication::nativeEventFilter(const QByteArray &, void *message, long *) {
+	MSG *msg = reinterpret_cast< MSG * >(message);
+	if (QThread::currentThread() == thread()) {
+		bool suppress = gswForward(msg);
+		if (suppress) {
+			return true;
+		}
+	}
+	return false;
+}
 #endif
-	Q_OBJECT
-public:
-	/// The instance function returns an instance
-	/// of the MumbleApplication singleton.
-	static MumbleApplication *instance();
-
-	MumbleApplication(int &pargc, char **pargv);
-
-	/// applicationVersionRootPath returns
-	/// Mumble's "versioned root"-path.
-	///
-	/// This is a version-specific path that contains
-	/// supplementary binaries and other products
-	/// that Mumble needs to function.
-	///
-	/// In the current implementation, the versioned
-	/// root path is set by the MUMBLE_VERSION_ROOT
-	/// environment variable. This environment variable
-	/// is set in the mumble.exe launcher.
-	///
-	/// If a versioned root path has not been
-	/// configured in the environment, the function
-	/// returns the same path as Qt's own
-	/// QApplication::applicationDirPath().
-	QString applicationVersionRootPath();
-
-	bool event(QEvent *e) Q_DECL_OVERRIDE;
-#ifdef Q_OS_WIN
-	bool nativeEventFilter(const QByteArray &eventType, void *message, long *result) Q_DECL_OVERRIDE;
-#endif
-
-	QUrl quLaunchURL;
-
-public slots:
-	/// Saves state and suppresses ask on quit before system shutdown.
-	void onCommitDataRequest(QSessionManager &);
-};
-
-#endif // MUMBLE_MUMBLE_MUMBLEAPPLICATION_H
