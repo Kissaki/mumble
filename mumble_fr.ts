1fff49377828a171c05d7387e968966eaf19824a
Jan Klass <kissaki@posteo.de>
2020-12-19 18:18:49 +0100

    New translations VersionCheck.h (French)

--- mumble_fr.ts
+++ mumble_fr.ts
@@ -3,69 +3,28 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_WASAPI_H_
-#define MUMBLE_MUMBLE_WASAPI_H_
+#ifndef MUMBLE_MUMBLE_VERSIONCHECK_H_
+#define MUMBLE_MUMBLE_VERSIONCHECK_H_
 
-#include "AudioInput.h"
-#include "AudioOutput.h"
-
-#include "win.h"
-
+#include <QFutureWatcher>
+#include <QtCore/QByteArray>
 #include <QtCore/QObject>
-#include <QtCore/QUuid>
+#include <QtCore/QUrl>
 
-#include <audioclient.h>
-#include <avrt.h>
-#include <functiondiscoverykeys.h>
-#include <ksmedia.h>
-#include <mmdeviceapi.h>
-#include <mmreg.h>
-#include <strsafe.h>
-#ifdef _INC_FUNCTIONDISCOVERYKEYS
-#	undef _INC_FUNCTIONDISCOVERYKEYS
-#endif
-#include <audiopolicy.h>
-#include <functiondiscoverykeys_devpkey.h>
-#include <propidl.h>
-
-class WASAPISystem : public QObject {
+class VersionCheck : public QObject {
 private:
 	Q_OBJECT
-	Q_DISABLE_COPY(WASAPISystem)
-public:
-	static const QHash< QString, QString > getDevices(EDataFlow dataflow);
-	static const QHash< QString, QString > getInputDevices();
-	static const QHash< QString, QString > getOutputDevices();
-	static const QList< audioDevice > mapToDevice(const QHash< QString, QString > &, const QString &);
-};
+	Q_DISABLE_COPY(VersionCheck)
 
-class WASAPIInput : public AudioInput {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(WASAPIInput)
-public:
-	WASAPIInput();
-	~WASAPIInput() Q_DECL_OVERRIDE;
-	void run() Q_DECL_OVERRIDE;
-};
-
-class WASAPIOutput : public AudioOutput {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(WASAPIOutput)
-
-	bool setVolumeForSessionControl(IAudioSessionControl *control, const DWORD mumblePID, QSet< QUuid > &seen);
-	bool setVolumeForSessionControl2(IAudioSessionControl2 *control2, const DWORD mumblePID, QSet< QUuid > &seen);
-
-protected:
-	typedef QPair< float, float > VolumePair;
-	QMap< ISimpleAudioVolume *, VolumePair > qmVolumes;
-	void setVolumes(IMMDevice *, bool talking);
+	QFutureWatcher< void > m_preparationWatcher;
+	QUrl m_requestURL;
+protected slots:
+	void performRequest();
+public slots:
+	void fetched(QByteArray data, QUrl url);
 
 public:
-	WASAPIOutput();
-	~WASAPIOutput() Q_DECL_OVERRIDE;
-	void run() Q_DECL_OVERRIDE;
+	VersionCheck(bool autocheck, QObject *parent = nullptr, bool focus = false);
 };
 
 #endif
