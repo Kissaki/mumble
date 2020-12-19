3781e0c4c465713ab7330981b9aeeef0b5712617
Jan Klass <kissaki@posteo.de>
2020-12-19 18:25:43 +0100

    New translations PathListWidget.h (Arabic)

--- mumble_ar.ts
+++ mumble_ar.ts
@@ -3,23 +3,38 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_SVGICON_H
-#define MUMBLE_MUMBLE_SVGICON_H
+#ifndef MUMBLE_MUMBLE_PATHLISTWIDGET_H_
+#define MUMBLE_MUMBLE_PATHLISTWIDGET_H_
 
-#include <QIcon>
-#include <QString>
+#include <QListWidget>
 
-class SvgIcon {
+class QGraphicsSceneDragDropEvent;
+class QDropEvent;
+
+/**
+ * ListWidget that adds functionality for being able to drop files or folders to add them to the list.
+ *
+ * It makes use of OverlayAppInfo to display file/app information (e.g. the appropriate icon).
+ */
+class PathListWidget : public QListWidget {
 public:
-	/// addSvgPixmapsToIcon renders the SVG file at |fn| in various
-	/// common sizes from 8x8 up to 256x256 and adds the resulting
-	/// rasterized pixmaps to |icon|.
-	///
-	/// This method is only needed due to an odd interaction between
-	/// Qt and Plasma/KStatusNotifierItem. See
-	/// https://github.com/mumble-voip/mumble/issues/3374
-	/// for more information.
-	static void addSvgPixmapsToIcon(QIcon &icon, QString fn);
+	enum PathType { FILE_EXE, FOLDER };
+
+	PathListWidget(QWidget *parent = 0);
+	void setPathType(PathType type);
+	virtual void dragEnterEvent(QDragEnterEvent *event) Q_DECL_OVERRIDE;
+	virtual void dragMoveEvent(QDragMoveEvent *event) Q_DECL_OVERRIDE;
+	virtual void dropEvent(QDropEvent *event) Q_DECL_OVERRIDE;
+
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(PathListWidget)
+
+	PathType pathType;
+
+	void addFilePath(const QString &path);
+	void addFolderPath(const QString &path);
+	void checkAcceptDragEvent(QDropEvent *event, bool store);
 };
 
 #endif
