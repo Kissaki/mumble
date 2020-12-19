9c686dda419d0ec2747f00c0afe3ebaafeee99b6
Jan Klass <kissaki@posteo.de>
2020-12-19 18:51:40 +0100

    New translations DeveloperConsole.cpp (Korean)

--- mumble_ko.ts
+++ mumble_ko.ts
@@ -3,77 +3,48 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include <G15LCDEngine_unix.h>
+#include "DeveloperConsole.h"
 
-static LCDEngine *G15LCDEngineNew() {
-	return new G15LCDEngineUnix();
+#include "LogEmitter.h"
+
+#include <QtWidgets/QTextBrowser>
+
+// We define a global macro called 'g'. This can lead to issues when included code uses 'g' as a type or parameter name
+// (like protobuf 3.7 does). As such, for now, we have to make this our last include.
+#include "Global.h"
+
+DeveloperConsole::DeveloperConsole(QObject *parent) : QObject(parent) {
+	connect(g.le.data(), SIGNAL(newLogEntry(const QString &)), this, SLOT(addLogMessage(const QString &)));
 }
 
-static LCDEngineRegistrar registrar(G15LCDEngineNew);
+DeveloperConsole::~DeveloperConsole() {
+	QMainWindow *mw = m_window.data();
+	delete mw;
+}
 
-G15LCDEngineUnix::G15LCDEngineUnix() {
-	sock = new_g15_screen(G15_PIXELBUF);
-	if (sock < 0) {
-		qWarning("G15LCDEngineUnix: Unable to connect to G15Daemon.");
-		return;
+void DeveloperConsole::show() {
+	if (m_window.isNull()) {
+		QMainWindow *mw = new QMainWindow();
+		mw->setAttribute(Qt::WA_DeleteOnClose);
+		QTextBrowser *tb = new QTextBrowser();
+		mw->resize(675, 300);
+		mw->setCentralWidget(tb);
+		mw->setWindowTitle(tr("Developer Console"));
+
+		connect(g.le.data(), SIGNAL(newLogEntry(const QString &)), tb, SLOT(append(const QString &)));
+
+		foreach (const QString &m, m_logEntries)
+			tb->append(m);
+		m_window = QPointer< QMainWindow >(mw);
 	}
 
-	qlDevices << new G15LCDDeviceUnix(this);
+	m_window.data()->show();
+	m_window.data()->activateWindow();
 }
 
-G15LCDEngineUnix::~G15LCDEngineUnix() {
-}
+void DeveloperConsole::addLogMessage(const QString &msg) {
+	if (m_logEntries.count() >= 1000)
+		m_logEntries.removeFirst();
 
-QList< LCDDevice * > G15LCDEngineUnix::devices() const {
-	return qlDevices;
-}
-
-/* -- */
-
-G15LCDDeviceUnix::G15LCDDeviceUnix(G15LCDEngineUnix *e) : LCDDevice() {
-	bEnabled = false;
-	engine   = e;
-}
-
-G15LCDDeviceUnix::~G15LCDDeviceUnix() {
-}
-
-bool G15LCDDeviceUnix::enabled() {
-	return bEnabled;
-}
-
-void G15LCDDeviceUnix::setEnabled(bool b) {
-	bEnabled = b;
-}
-
-void G15LCDDeviceUnix::blitImage(QImage *img, bool) {
-	Q_ASSERT(img);
-
-	const unsigned int len = 6880;
-	uchar buf[len];
-	uchar *tmp = img->bits();
-
-	for (unsigned int i = 0; i < len / 8; ++i) {
-		unsigned int idx = i * 8;
-		buf[idx + 7]     = tmp[i] & 0x80 ? 1 : 0;
-		buf[idx + 6]     = tmp[i] & 0x40 ? 1 : 0;
-		buf[idx + 5]     = tmp[i] & 0x20 ? 1 : 0;
-		buf[idx + 4]     = tmp[i] & 0x10 ? 1 : 0;
-		buf[idx + 3]     = tmp[i] & 0x08 ? 1 : 0;
-		buf[idx + 2]     = tmp[i] & 0x04 ? 1 : 0;
-		buf[idx + 1]     = tmp[i] & 0x02 ? 1 : 0;
-		buf[idx + 0]     = tmp[i] & 0x01 ? 1 : 0;
-	}
-
-	int ret = g15_send(engine->sock, reinterpret_cast< char * >(buf), len);
-	if (ret < 0)
-		qWarning("G15LCDDeviceUnix: Unable to g15_send().");
-}
-
-QString G15LCDDeviceUnix::name() const {
-	return QString::fromLatin1("Logitech Gamepanel (G15Daemon)");
-}
-
-QSize G15LCDDeviceUnix::size() const {
-	return QSize(G15_WIDTH, G15_HEIGHT);
+	m_logEntries.append(msg);
 }
