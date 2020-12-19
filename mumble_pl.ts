c68d4f278138d49274b066f2ce0ea109974ba10d
Jan Klass <kissaki@posteo.de>
2020-12-19 19:06:53 +0100

    New translations XboxInput.h (Polish)

--- mumble_pl.ts
+++ mumble_pl.ts
@@ -3,47 +3,82 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include <winver.h>
+#ifndef MUMBLE_MUMBLE_XBOXINPUT_H_
+#define MUMBLE_MUMBLE_XBOXINPUT_H_
 
-#ifndef DEBUG
-#define VER_DEBUG                   0L
-#else
-#define VER_DEBUG                   VS_FF_DEBUG
-#endif
+#include "win.h"
 
-#ifdef SNAPSHOT_BUILD
-#define	VER_RELEASE	VS_FF_SPECIALBUILD|VS_FF_PRERELEASE
-#else
-#define VER_RELEASE 0L
-#endif
+#include <QUuid>
 
-VS_VERSION_INFO VERSIONINFO
-	FILEVERSION		@PROJECT_VERSION_MAJOR@,@PROJECT_VERSION_MINOR@,@PROJECT_VERSION_PATCH@,0
-	PRODUCTVERSION	@PROJECT_VERSION_MAJOR@,@PROJECT_VERSION_MINOR@,@PROJECT_VERSION_PATCH@,0
-	FILEFLAGSMASK	VS_FFI_FILEFLAGSMASK
-	FILEFLAGS		(VER_DEBUG|VER_RELEASE)
-	FILEOS			VOS_NT_WINDOWS32
-	FILETYPE		VFT_DLL
-	FILESUBTYPE		0L
-	BEGIN
-		BLOCK "StringFileInfo"
-		BEGIN
-			BLOCK "040904E4"
-			BEGIN
-				VALUE "CompanyName", "The Mumble Developers"
-				VALUE "FileDescription", "Mumble - Low-latency VoIP client"
-				VALUE "FileVersion", "@PROJECT_VERSION@"
-				VALUE "ProductVersion", "@PROJECT_VERSION@"
-				VALUE "LegalCopyright", "Copyright (c) 2005-@MUMBLE_BUILD_YEAR@ The Mumble Developers"
-				VALUE "OriginalFilename", "mumble_app.dll"
-				VALUE "ProductName", "Mumble"
-#ifdef SNAPSHOT_BUILD
-				VALUE "SpecialBuild", "Snapshot development release"
+#include <stdint.h>
+
+/// XBOXINPUT_MAX_DEVICES defines the maximum
+/// number of devices that can be connected
+/// to the system at once.
+///
+/// In typical operation, one calls GetState()
+/// in a loop bounded by XBOXINPUT_MAX_DEVICES.
+#define XBOXINPUT_MAX_DEVICES 4
+
+/// XBOXINPUT_TRIGGER_THRESHOLD defines the thresold
+/// that an analog trigger (leftTrigger and
+/// rightTrigger of XboxInputState) must have exceeded
+/// in order to count as a button press.
+#define XBOXINPUT_TRIGGER_THRESHOLD 30
+
+/// XboxInputState represents the state of an
+/// Xbox controller as returned by GetState().
+struct XboxInputState {
+	uint32_t packetNumber;
+	uint16_t buttons;
+	uint8_t leftTrigger;
+	uint8_t rightTrigger;
+	uint16_t leftThumbX;
+	uint16_t leftThumbY;
+	uint16_t rightThumbY;
+	uint16_t rightThumbX;
+	uint32_t paddingReserved; // Required for XInputGetStateEx. Not required for XInputGetState.
+};
+
+typedef uint32_t(WINAPI *XboxInputGetStateFunc)(uint32_t deviceIndex, XboxInputState *state);
+
+/// XboxInput is an XInput wrapper that dynamically loads an appropriate
+/// xinput*.dll on construction and provides access to its GetState(Ex)
+/// function.
+class XboxInput {
+public:
+	XboxInput();
+	virtual ~XboxInput();
+
+	/// s_XboxInputGuid is the GUID used by GlobalShortcut_win
+	/// to distinguish XboxInputLibrary's events from other event
+	/// soures.
+	static const QUuid s_XboxInputGuid;
+
+	/// isValid determines wheter the XboxInputLibrary
+	/// is usable.
+	bool isValid() const;
+
+	/// Query the state of the Xbox controller at deviceIndex.
+	/// If the function succeeds, it returns 0 (Windows's ERROR_SUCCESS).
+	/// If no device is connected, it returns 0x48F (Windows's ERROR_DEVICE_NOT_CONNECTED).
+	XboxInputGetStateFunc GetState;
+
+protected:
+	/// m_getStateFunc represents XInputGetState from the XInput DLL.
+	XboxInputGetStateFunc m_getStateFunc;
+
+	/// m_getStateFuncEx represents XInputGetStateEx, which is optionally
+	/// available in the XInput DLL.
+	XboxInputGetStateFunc m_getStateExFunc;
+
+	/// m_xinputlib is the handle to the XInput DLL as returned by
+	/// LoadLibrary.
+	HMODULE m_xinputlib;
+
+	/// m_valid determines whether or not the XboxInputLibrary
+	/// is valid for use.
+	bool m_valid;
+};
+
 #endif
-			END
-		END
-		BLOCK "VarFileInfo"
-		BEGIN
-			VALUE "Translation", 0x409, 1252
-		END
-	END
