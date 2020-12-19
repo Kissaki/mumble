8aac1a136fe91fbe6572bd5afc4dfaaf5b7ed915
Jan Klass <kissaki@posteo.de>
2020-12-19 18:33:04 +0100

    New translations RichTextEditor.h (Basque)

--- mumble_eu.ts
+++ mumble_eu.ts
@@ -3,24 +3,82 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_PTTBUTTONWIDGET_H_
-#define MUMBLE_MUMBLE_PTTBUTTONWIDGET_H_
+#ifndef MUMBLE_MUMBLE_RICHTEXTEDITOR_H_
+#define MUMBLE_MUMBLE_RICHTEXTEDITOR_H_
 
-#include "ui_PTTButtonWidget.h"
+#include <QtCore/QtGlobal>
+#include <QtWidgets/QTextEdit>
 
-class PTTButtonWidget : public QWidget, public Ui::qwPTTButtonWidget {
+class LogDocument;
+
+class RichTextHtmlEdit : public QTextEdit {
+private:
 	Q_OBJECT
-	Q_DISABLE_COPY(PTTButtonWidget)
+	Q_DISABLE_COPY(RichTextHtmlEdit)
 protected:
-	void closeEvent(QCloseEvent *e) Q_DECL_OVERRIDE;
+	void insertFromMimeData(const QMimeData *source);
 
 public:
-	PTTButtonWidget(QWidget *p = 0);
-public slots:
-	void on_qpbPushToTalk_pressed();
-	void on_qpbPushToTalk_released();
-signals:
-	void triggered(bool checked, QVariant);
+	RichTextHtmlEdit(QWidget *p);
+
+private:
+	LogDocument *m_document;
 };
 
-#endif // PTTBUTTONWIDGET_H_
+#include "ui_RichTextEditor.h"
+#include "ui_RichTextEditorLink.h"
+
+
+class RichTextEditorLink : public QDialog, Ui::RichTextEditorLink {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(RichTextEditorLink)
+public:
+	RichTextEditorLink(const QString &text = QString(), QWidget *p = nullptr);
+	QString text() const;
+};
+
+class RichTextEditor : public QTabWidget, Ui::RichTextEditor {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(RichTextEditor)
+protected:
+	bool bModified;
+	bool bChanged;
+	bool bReadOnly;
+	void richToPlain();
+	QColor qcColor;
+	bool eventFilter(QObject *obj, QEvent *event) Q_DECL_OVERRIDE;
+
+public:
+	RichTextEditor(QWidget *p = nullptr);
+	QString text();
+	bool isModified() const;
+signals:
+	/// The accept signal is emitted when Ctrl-Enter is pressed inside the RichTextEditor.
+	void accept();
+public slots:
+	void setText(const QString &text, bool readonly = false);
+	void updateColor(const QColor &);
+	void updateActions();
+protected slots:
+	void on_qaBold_triggered(bool);
+	void on_qaItalic_triggered(bool);
+	void on_qaUnderline_triggered(bool);
+	void on_qaColor_triggered();
+	void on_qaLink_triggered();
+	void on_qaImage_triggered();
+
+	void on_qptePlainText_textChanged();
+	void on_qteRichText_textChanged();
+	void on_qteRichText_cursorPositionChanged();
+	void on_qteRichText_currentCharFormatChanged();
+	void onCurrentChanged(int);
+};
+
+class RichTextImage {
+public:
+	static bool isValidImage(const QByteArray &buf, QByteArray &fmt);
+};
+
+#endif
