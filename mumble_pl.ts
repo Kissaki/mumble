5d008cbcc606a6b9f0b17fa40b7f9ba80ff68214
Jan Klass <kissaki@posteo.de>
2020-12-19 19:10:58 +0100

    New translations SharedMemory.h (Polish)

--- mumble_pl.ts
+++ mumble_pl.ts
@@ -3,34 +3,35 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_TEXTTOSPEECH_H_
-#define MUMBLE_MUMBLE_TEXTTOSPEECH_H_
+#ifndef MUMBLE_MUMBLE_SHAREDMEMORY_H_
+#define MUMBLE_MUMBLE_SHAREDMEMORY_H_
 
 #include <QtCore/QObject>
+#include <QtCore/QString>
 
-class TextToSpeechPrivate;
-
-class TextToSpeech : public QObject {
-	friend class TextToSpeechPrivate;
-
+struct SharedMemory2Private;
+class SharedMemory2 : QObject {
 private:
+	Q_DISABLE_COPY(SharedMemory2)
 	Q_OBJECT
-	Q_DISABLE_COPY(TextToSpeech)
-	Q_PROPERTY(bool enabled READ isEnabled WRITE setEnabled)
 protected:
-	bool enabled;
+	QString qsName;
+	SharedMemory2Private *d;
+	unsigned char *a_ucData;
+	unsigned int uiSize;
+	static unsigned int uiIndex;
 
 public:
-	TextToSpeech(QObject *parent = nullptr);
-	~TextToSpeech() Q_DECL_OVERRIDE;
-	bool isEnabled() const;
-public slots:
-	void say(const QString &text);
-	void setEnabled(bool ena);
-	void setVolume(int volume);
+	SharedMemory2(QObject *p, unsigned int minsize, const QString &name = QString());
+	~SharedMemory2() Q_DECL_OVERRIDE;
 
-private:
-	TextToSpeechPrivate *d;
+	void erase();
+	void systemRelease();
+
+	QString name() const;
+	unsigned int size() const;
+	unsigned char *data();
+	const unsigned char *data() const;
 };
 
 #endif
