a77e258f18c11ea9633faa5f734eb4ecd1575c19
Jan Klass <kissaki@posteo.de>
2020-12-19 18:12:09 +0100

    New translations AudioOutputUser.cpp (Basque)

--- mumble_eu.ts
+++ mumble_eu.ts
@@ -3,79 +3,24 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_AUDIOOUTPUTSPEECH_H_
-#define MUMBLE_MUMBLE_AUDIOOUTPUTSPEECH_H_
-
-#include <celt.h>
-#include <speex/speex.h>
-#include <speex/speex_jitter.h>
-#include <speex/speex_resampler.h>
-
-#include <QtCore/QMutex>
-
 #include "AudioOutputUser.h"
-#include "Message.h"
 
-class CELTCodec;
-class OpusCodec;
-class ClientUser;
-struct OpusDecoder;
+AudioOutputUser::AudioOutputUser(const QString &name) : qsName(name) {
+}
 
-class AudioOutputSpeech : public AudioOutputUser {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(AudioOutputSpeech)
-protected:
-	unsigned int iAudioBufferSize;
-	unsigned int iBufferOffset;
-	unsigned int iBufferFilled;
-	unsigned int iOutputSize;
-	unsigned int iLastConsume;
-	unsigned int iFrameSize;
-	unsigned int iFrameSizePerChannel;
-	unsigned int iSampleRate;
-	unsigned int iMixerFreq;
-	bool bLastAlive;
-	bool bHasTerminator;
+AudioOutputUser::~AudioOutputUser() {
+	delete[] pfBuffer;
+	delete[] pfVolume;
+}
 
-	float *fFadeIn;
-	float *fFadeOut;
-	float *fResamplerBuffer;
-
-	SpeexResamplerState *srs;
-
-	QMutex qmJitter;
-	JitterBuffer *jbJitter;
-	int iMissCount;
-
-	CELTCodec *cCodec;
-	CELTDecoder *cdDecoder;
-
-	OpusCodec *oCodec;
-	OpusDecoder *opusState;
-
-	SpeexBits sbBits;
-	void *dsSpeex;
-
-	QList< QByteArray > qlFrames;
-
-public:
-	unsigned char ucFlags;
-	MessageHandler::UDPMessageType umtType;
-	int iMissedFrames;
-	ClientUser *p;
-
-	/// Fetch and decode frames from the jitter buffer. Called in mix().
-	///
-	/// @param frameCount Number of frames to decode. frame means a bundle of one sample from each channel.
-	virtual bool prepareSampleBuffer(unsigned int frameCount) Q_DECL_OVERRIDE;
-
-	void addFrameToBuffer(const QByteArray &, unsigned int iBaseSeq);
-
-	/// @param systemMaxBufferSize maximum number of samples the system audio play back may request each time
-	AudioOutputSpeech(ClientUser *, unsigned int freq, MessageHandler::UDPMessageType type,
-					  unsigned int systemMaxBufferSize);
-	~AudioOutputSpeech() Q_DECL_OVERRIDE;
-};
-
-#endif // AUDIOOUTPUTSPEECH_H_
+void AudioOutputUser::resizeBuffer(unsigned int newsize) {
+	if (newsize > iBufferSize) {
+		float *n = new float[newsize];
+		if (pfBuffer) {
+			memcpy(n, pfBuffer, sizeof(float) * iBufferSize);
+			delete[] pfBuffer;
+		}
+		pfBuffer    = n;
+		iBufferSize = newsize;
+	}
+}
