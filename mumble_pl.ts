e8a70bf9c59b16b26e4a5de5098826c32bdd303f
Jan Klass <kissaki@posteo.de>
2020-12-19 19:10:21 +0100

    New translations Usage.cpp (Polish)

--- mumble_pl.ts
+++ mumble_pl.ts
@@ -3,65 +3,70 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-/* Copyright (C) 2015, Fredrik Nordin <freedick@ludd.ltu.se>
-
-   All rights reserved.
-
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions
-   are met:
-
-   - Redistributions of source code must retain the above copyright notice,
-	 this list of conditions and the following disclaimer.
-   - Redistributions in binary form must reproduce the above copyright notice,
-	 this list of conditions and the following disclaimer in the documentation
-	 and/or other materials provided with the distribution.
-   - Neither the name of the Mumble Developers nor the names of its
-	 contributors may be used to endorse or promote products derived from this
-	 software without specific prior written permission.
-
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR
-   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
-#ifndef MUMBLE_MUMBLE_USERVOLUME_H_
-#define MUMBLE_MUMBLE_USERVOLUME_H_
-
-#include <QMap>
+#include "Usage.h"
 
 #include "ClientUser.h"
-#include "ui_UserLocalVolumeDialog.h"
+#include "LCD.h"
+#include "NetworkConfig.h"
+#include "OSInfo.h"
+#include "Version.h"
+#include "Global.h"
 
-class UserLocalVolumeDialog : public QDialog, private Ui::UserLocalVolumeDialog {
-	Q_OBJECT
-	Q_DISABLE_COPY(UserLocalVolumeDialog);
+#include <QtCore/QTimer>
+#include <QtNetwork/QHostAddress>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkReply>
+#include <QtXml/QDomElement>
 
-	/// The session ID for the user that the dialog is changing the volume for.
-	unsigned int m_clientSession;
+Usage::Usage(QObject *p) : QObject(p) {
+	qbReport.open(QBuffer::ReadWrite);
+	qdsReport.setDevice(&qbReport);
+	qdsReport.setVersion(QDataStream::Qt_4_4);
+	qdsReport << static_cast< unsigned int >(2);
 
-	/// The user's original adjustment (in dB) when entering the dialog.
-	int m_originalVolumeAdjustmentDecibel;
-	QMap< unsigned int, UserLocalVolumeDialog * > *m_qmUserVolTracker;
+	// Wait 10 minutes (so we know they're actually using this), then...
+	QTimer::singleShot(60 * 10 * 1000, this, SLOT(registerUsage()));
+}
 
-public slots:
-	void closeEvent(QCloseEvent *event);
-	void on_qsUserLocalVolume_valueChanged(int value);
-	void on_qsbUserLocalVolume_valueChanged(int value);
-	void on_qbbUserLocalVolume_clicked(QAbstractButton *b);
-	void reject();
+void Usage::registerUsage() {
+	if (!g.s.bUsage
+		|| g.s.uiUpdateCounter == 0) // Only register usage if allowed by the user and first wizard run has finished
+		return;
 
-public:
-	static void present(unsigned int sessionId, QMap< unsigned int, UserLocalVolumeDialog * > *qmUserVolTracker);
-	UserLocalVolumeDialog(unsigned int sessionId, QMap< unsigned int, UserLocalVolumeDialog * > *qmUserVolTracker);
-};
+	QDomDocument doc;
+	QDomElement root = doc.createElement(QLatin1String("usage"));
+	doc.appendChild(root);
 
-#endif
+	QDomElement tag;
+	QDomText t;
+
+	OSInfo::fillXml(doc, root);
+
+	tag = doc.createElement(QLatin1String("in"));
+	root.appendChild(tag);
+	t = doc.createTextNode(g.s.qsAudioInput);
+	tag.appendChild(t);
+
+	tag = doc.createElement(QLatin1String("out"));
+	root.appendChild(tag);
+	t = doc.createTextNode(g.s.qsAudioOutput);
+	tag.appendChild(t);
+
+	tag = doc.createElement(QLatin1String("lcd"));
+	root.appendChild(tag);
+	t = doc.createTextNode(QString::number(g.lcd->hasDevices() ? 1 : 0));
+	tag.appendChild(t);
+
+	QBuffer *qb = new QBuffer();
+	qb->setData(doc.toString().toUtf8());
+	qb->open(QIODevice::ReadOnly);
+
+	QNetworkRequest req(QUrl(QLatin1String("https://usage-report.mumble.info/v1/report")));
+	Network::prepareRequest(req);
+	req.setHeader(QNetworkRequest::ContentTypeHeader, QLatin1String("text/xml"));
+
+	QNetworkReply *rep = g.nam->post(req, qb);
+	qb->setParent(rep);
+
+	connect(rep, SIGNAL(finished()), rep, SLOT(deleteLater()));
+}
