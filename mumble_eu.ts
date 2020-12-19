15a0088f2ce063f46926d3228ae2fbb937d9f649
Jan Klass <kissaki@posteo.de>
2020-12-19 18:37:29 +0100

    New translations SvgIcon.cpp (Basque)

--- mumble_eu.ts
+++ mumble_eu.ts
@@ -3,14 +3,37 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_TASK_LIST_H_
-#define MUMBLE_MUMBLE_TASK_LIST_H_
+#include "SvgIcon.h"
 
-class QString;
+#include <QPainter>
+#include <QSvgRenderer>
 
-class TaskList {
-public:
-	static void addToRecentList(QString name, QString user, QString host, int port);
-};
+void SvgIcon::addSvgPixmapsToIcon(QIcon &icon, QString fn) {
+	QSvgRenderer svg(fn);
 
-#endif // TASK_LIST_H_
+	QList< QSize > commonSizes;
+	commonSizes << QSize(8, 8);
+	commonSizes << QSize(16, 16);
+	commonSizes << QSize(22, 22); // Plasma notification area size
+	commonSizes << QSize(24, 24);
+	commonSizes << QSize(32, 32);
+	commonSizes << QSize(44, 44); // Plasma notification area size @x2
+	commonSizes << QSize(48, 48);
+	commonSizes << QSize(64, 64);
+	commonSizes << QSize(96, 96);
+	commonSizes << QSize(128, 128);
+	commonSizes << QSize(256, 256);
+
+	foreach (QSize size, commonSizes) {
+		QPixmap pm(size);
+		pm.fill(Qt::transparent);
+
+		QPainter p(&pm);
+		p.setRenderHint(QPainter::Antialiasing);
+		p.setRenderHint(QPainter::TextAntialiasing);
+		p.setRenderHint(QPainter::SmoothPixmapTransform);
+		svg.render(&p);
+
+		icon.addPixmap(pm);
+	}
+}
