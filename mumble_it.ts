eac52dab6d61c132b07cf7fe2bf707c460c1cbc3
Jan Klass <kissaki@posteo.de>
2020-12-19 18:31:52 +0100

    New translations ListenerLocalVolumeDialog.cpp (Italian)

--- mumble_it.ts
+++ mumble_it.ts
@@ -3,40 +3,61 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_LISTENERVOLUME_H_
-#define MUMBLE_MUMBLE_LISTENERVOLUME_H_
+#include "ListenerLocalVolumeDialog.h"
+#include "Channel.h"
+#include "ChannelListener.h"
+#include "ClientUser.h"
 
-#include "ui_UserLocalVolumeDialog.h"
+#include <QtWidgets/QPushButton>
 
-class ClientUser;
-class Channel;
+#include <cmath>
 
-/// The dialog to configure the local volume adjustment for a "listener proxy". Therefore
-/// this dialog can be used to tune the volume of audio streams one receivs via the listening
-/// feature.
-/// It actually uses the same UI frontend as UserLocalVolumeDialog as it only needs backend
-/// changes.
-class ListenerLocalVolumeDialog : public QDialog, private Ui::UserLocalVolumeDialog {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(ListenerLocalVolumeDialog);
+// We define a global macro called 'g'. This can lead to issues when included code uses 'g' as a type or parameter name
+// (like protobuf 3.7 does). As such, for now, we have to make this our last include.
+#include "Global.h"
 
-protected:
-	/// The user belonging to the listener proxy this dialog has been invoked on
-	ClientUser *m_user;
-	/// The channel of the listener proxy this dialog has been invoked on
-	Channel *m_channel;
-	/// The volume adjustment that was set before this dialog opened
-	float m_initialAdjustemt;
+ListenerLocalVolumeDialog::ListenerLocalVolumeDialog(ClientUser *user, Channel *channel, QWidget *parent)
+	: QDialog(parent), m_user(user), m_channel(channel) {
+	setupUi(this);
 
-public slots:
-	void on_qsUserLocalVolume_valueChanged(int value);
-	void on_qsbUserLocalVolume_valueChanged(int value);
-	void on_qbbUserLocalVolume_clicked(QAbstractButton *b);
-	void reject();
+	m_initialAdjustemt = ChannelListener::getListenerLocalVolumeAdjustment(m_channel);
 
-public:
-	ListenerLocalVolumeDialog(ClientUser *user, Channel *channel, QWidget *parent = nullptr);
-};
+	// Decibel formula: +6db = *2
+	// Calculate the db-shift from the set volume-faactor
+	float fdbShift = log2f(m_initialAdjustemt) * 6;
+	qsUserLocalVolume->setValue(static_cast< int >(roundf(fdbShift)));
 
-#endif
+	setWindowTitle(tr("Adjusting local volume for listening to %1").arg(channel->qsName));
+}
+
+void ListenerLocalVolumeDialog::on_qsUserLocalVolume_valueChanged(int value) {
+	qsbUserLocalVolume->setValue(value);
+}
+
+void ListenerLocalVolumeDialog::on_qsbUserLocalVolume_valueChanged(int value) {
+	qsUserLocalVolume->setValue(value);
+
+	// Decibel formula: +6db = *2
+	// Calculate the volume-factor for the set db-shift
+	ChannelListener::setListenerLocalVolumeAdjustment(m_channel,
+													  static_cast< float >(pow(2.0, qsUserLocalVolume->value() / 6.0)));
+}
+
+void ListenerLocalVolumeDialog::on_qbbUserLocalVolume_clicked(QAbstractButton *button) {
+	if (button == qbbUserLocalVolume->button(QDialogButtonBox::Reset)) {
+		qsUserLocalVolume->setValue(0);
+	}
+	if (button == qbbUserLocalVolume->button(QDialogButtonBox::Ok)) {
+		ListenerLocalVolumeDialog::accept();
+	}
+	if (button == qbbUserLocalVolume->button(QDialogButtonBox::Cancel)) {
+		ListenerLocalVolumeDialog::close();
+	}
+}
+
+void ListenerLocalVolumeDialog::reject() {
+	// Restore to what has been set before the dialog
+	ChannelListener::setListenerLocalVolumeAdjustment(m_channel, m_initialAdjustemt);
+
+	QDialog::reject();
+}
