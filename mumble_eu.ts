c7b6f580931033e21677f4e7c5df50a2181d413b
Jan Klass <kissaki@posteo.de>
2020-12-19 18:10:13 +0100

    New translations AppNap.h (Basque)

--- mumble_eu.ts
+++ mumble_eu.ts
@@ -3,19 +3,10 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_ABOUT_H_
-#define MUMBLE_MUMBLE_ABOUT_H_
+#ifndef MUMBLE_MUMBLE_APPNAP_H_
+#define MUMBLE_MUMBLE_APPNAP_H_
 
-#include <QtCore/QObject>
-#include <QtCore/QtGlobal>
-#include <QtWidgets/QDialog>
-
-class AboutDialog : public QDialog {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(AboutDialog)
-public:
-	AboutDialog(QWidget *parent);
-};
+/// MUSuppressAppNap suppresses OS X's App Nap feature.
+void MUSuppressAppNap(bool suppress);
 
 #endif
