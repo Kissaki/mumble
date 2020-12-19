13cb7cfba9c16ae37aab709abe870e0de3e42e42
Jan Klass <kissaki@posteo.de>
2020-12-19 19:17:32 +0100

    New translations LookConfig.h (Portuguese, Brazilian)

--- mumble_pt.ts
+++ mumble_pt.ts
@@ -1,63 +1,46 @@
-// Copyright 2020 The Mumble Developers. All rights reserved.
+// Copyright 2005-2020 The Mumble Developers. All rights reserved.
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include "ListenerLocalVolumeDialog.h"
-#include "Channel.h"
-#include "ChannelListener.h"
-#include "ClientUser.h"
+#ifndef MUMBLE_MUMBLE_LOOKCONFIG_H_
+#define MUMBLE_MUMBLE_LOOKCONFIG_H_
 
-#include <QtWidgets/QPushButton>
+#include "ConfigDialog.h"
+#include "ThemeInfo.h"
 
-#include <cmath>
+#include "ui_LookConfig.h"
 
-// We define a global macro called 'g'. This can lead to issues when included code uses 'g' as a type or parameter name
-// (like protobuf 3.7 does). As such, for now, we have to make this our last include.
-#include "Global.h"
+class QFileSystemWatcher;
+class QTimer;
 
-ListenerLocalVolumeDialog::ListenerLocalVolumeDialog(ClientUser *user, Channel *channel, QWidget *parent)
-	: QDialog(parent), m_user(user), m_channel(channel) {
-	setupUi(this);
+class LookConfig : public ConfigWidget, Ui::LookConfig {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(LookConfig)
+public:
+	/// The unique name of this ConfigWidget
+	static const QString name;
+	LookConfig(Settings &st);
+	virtual QString title() const Q_DECL_OVERRIDE;
+	virtual const QString &getName() const Q_DECL_OVERRIDE;
+	virtual QIcon icon() const Q_DECL_OVERRIDE;
 
-	m_initialAdjustemt = ChannelListener::getListenerLocalVolumeAdjustment(m_channel);
+public slots:
+	void accept() const Q_DECL_OVERRIDE;
+	void save() const Q_DECL_OVERRIDE;
+	void load(const Settings &r) Q_DECL_OVERRIDE;
+	void themeDirectoryChanged();
+	void on_qcbAbbreviateChannelNames_stateChanged(int state);
 
-	// Decibel formula: +6db = *2
-	// Calculate the db-shift from the set volume-faactor
-	float fdbShift = log2f(m_initialAdjustemt) * 6;
-	qsUserLocalVolume->setValue(static_cast< int >(roundf(fdbShift)));
+private:
+	/// Reload themes combobox and select given configuredStyle in it
+	void reloadThemes(const boost::optional< ThemeInfo::StyleInfo > configuredStyle);
 
-	setWindowTitle(tr("Adjusting local volume for listening to %1").arg(channel->qsName));
-}
+	/// Timer to prevent change event floods from triggering theme reloads
+	QTimer *m_themeDirectoryDebouncer;
+	/// Watcher to keep theme list up to date with user theme directory
+	QFileSystemWatcher *m_themeDirectoryWatcher;
+};
 
-void ListenerLocalVolumeDialog::on_qsUserLocalVolume_valueChanged(int value) {
-	qsbUserLocalVolume->setValue(value);
-}
-
-void ListenerLocalVolumeDialog::on_qsbUserLocalVolume_valueChanged(int value) {
-	qsUserLocalVolume->setValue(value);
-
-	// Decibel formula: +6db = *2
-	// Calculate the volume-factor for the set db-shift
-	ChannelListener::setListenerLocalVolumeAdjustment(m_channel,
-													  static_cast< float >(pow(2.0, qsUserLocalVolume->value() / 6.0)));
-}
-
-void ListenerLocalVolumeDialog::on_qbbUserLocalVolume_clicked(QAbstractButton *button) {
-	if (button == qbbUserLocalVolume->button(QDialogButtonBox::Reset)) {
-		qsUserLocalVolume->setValue(0);
-	}
-	if (button == qbbUserLocalVolume->button(QDialogButtonBox::Ok)) {
-		ListenerLocalVolumeDialog::accept();
-	}
-	if (button == qbbUserLocalVolume->button(QDialogButtonBox::Cancel)) {
-		ListenerLocalVolumeDialog::close();
-	}
-}
-
-void ListenerLocalVolumeDialog::reject() {
-	// Restore to what has been set before the dialog
-	ChannelListener::setListenerLocalVolumeAdjustment(m_channel, m_initialAdjustemt);
-
-	QDialog::reject();
-}
+#endif
