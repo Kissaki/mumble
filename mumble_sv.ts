c4431d9f8e6e289fe6d4c07cd6bea060cc49af5b
Jan Klass <kissaki@posteo.de>
2020-12-19 19:03:04 +0100

    New translations UserInformation.cpp (Swedish)

--- mumble_sv.ts
+++ mumble_sv.ts
@@ -1,236 +1,219 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>UserEdit</class>
- <widget class="QDialog" name="UserEdit">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>542</width>
-    <height>470</height>
-   </rect>
-  </property>
-  <property name="minimumSize">
-   <size>
-    <width>381</width>
-    <height>470</height>
-   </size>
-  </property>
-  <property name="windowTitle">
-   <string/>
-  </property>
-  <layout class="QGridLayout" name="gridLayout_2">
-   <item row="1" column="0">
-    <widget class="QDialogButtonBox" name="qbbButtons">
-     <property name="sizePolicy">
-      <sizepolicy hsizetype="Expanding" vsizetype="Minimum">
-       <horstretch>0</horstretch>
-       <verstretch>0</verstretch>
-      </sizepolicy>
-     </property>
-     <property name="orientation">
-      <enum>Qt::Horizontal</enum>
-     </property>
-     <property name="standardButtons">
-      <set>QDialogButtonBox::Cancel|QDialogButtonBox::Ok</set>
-     </property>
-    </widget>
-   </item>
-   <item row="0" column="0">
-    <widget class="QGroupBox" name="qgbUserList">
-     <property name="title">
-      <string>Registered Users</string>
-     </property>
-     <layout class="QGridLayout" name="gridLayout">
-      <item row="2" column="0">
-       <widget class="QPushButton" name="qpbRename">
-        <property name="enabled">
-         <bool>false</bool>
-        </property>
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Minimum" vsizetype="Fixed">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-        <property name="text">
-         <string>Rename</string>
-        </property>
-       </widget>
-      </item>
-      <item row="2" column="1">
-       <widget class="QPushButton" name="qpbRemove">
-        <property name="enabled">
-         <bool>false</bool>
-        </property>
-        <property name="text">
-         <string>Remove</string>
-        </property>
-        <property name="flat">
-         <bool>false</bool>
-        </property>
-       </widget>
-      </item>
-      <item row="1" column="0" colspan="6">
-       <widget class="QLineEdit" name="qlSearch">
-        <property name="font">
-         <font>
-          <italic>true</italic>
-         </font>
-        </property>
-        <property name="text">
-         <string/>
-        </property>
-        <property name="alignment">
-         <set>Qt::AlignCenter</set>
-        </property>
-        <property name="placeholderText">
-         <string>Who are you looking for?</string>
-        </property>
-       </widget>
-      </item>
-      <item row="2" column="5">
-       <widget class="MUComboBox" name="qcbInactive">
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Minimum" vsizetype="Fixed">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-        <item>
-         <property name="text">
-          <string>Days</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>Weeks</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>Months</string>
-         </property>
-        </item>
-        <item>
-         <property name="text">
-          <string>Years</string>
-         </property>
-        </item>
-       </widget>
-      </item>
-      <item row="2" column="2">
-       <spacer name="horizontalSpacer">
-        <property name="orientation">
-         <enum>Qt::Horizontal</enum>
-        </property>
-        <property name="sizeHint" stdset="0">
-         <size>
-          <width>40</width>
-          <height>20</height>
-         </size>
-        </property>
-       </spacer>
-      </item>
-      <item row="2" column="4">
-       <widget class="QSpinBox" name="qsbInactive">
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="MinimumExpanding" vsizetype="Fixed">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-        <property name="maximumSize">
-         <size>
-          <width>48</width>
-          <height>16777215</height>
-         </size>
-        </property>
-        <property name="frame">
-         <bool>true</bool>
-        </property>
-        <property name="maximum">
-         <number>9999</number>
-        </property>
-       </widget>
-      </item>
-      <item row="2" column="3">
-       <widget class="QLabel" name="qlInactive">
-        <property name="sizePolicy">
-         <sizepolicy hsizetype="Minimum" vsizetype="Preferred">
-          <horstretch>0</horstretch>
-          <verstretch>0</verstretch>
-         </sizepolicy>
-        </property>
-        <property name="text">
-         <string>Inactive for</string>
-        </property>
-       </widget>
-      </item>
-      <item row="0" column="0" colspan="6">
-       <widget class="QTreeView" name="qtvUserList">
-        <property name="alternatingRowColors">
-         <bool>true</bool>
-        </property>
-        <property name="selectionMode">
-         <enum>QAbstractItemView::ExtendedSelection</enum>
-        </property>
-        <property name="rootIsDecorated">
-         <bool>false</bool>
-        </property>
-        <property name="itemsExpandable">
-         <bool>false</bool>
-        </property>
-        <property name="sortingEnabled">
-         <bool>true</bool>
-        </property>
-       </widget>
-      </item>
-     </layout>
-    </widget>
-   </item>
-  </layout>
- </widget>
- <resources/>
- <connections>
-  <connection>
-   <sender>qbbButtons</sender>
-   <signal>accepted()</signal>
-   <receiver>UserEdit</receiver>
-   <slot>accept()</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>248</x>
-     <y>254</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>157</x>
-     <y>274</y>
-    </hint>
-   </hints>
-  </connection>
-  <connection>
-   <sender>qbbButtons</sender>
-   <signal>rejected()</signal>
-   <receiver>UserEdit</receiver>
-   <slot>reject()</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>316</x>
-     <y>260</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>286</x>
-     <y>274</y>
-    </hint>
-   </hints>
-  </connection>
- </connections>
- <customwidgets>
-  <customwidget>
-   <class>MUComboBox</class>
-   <extends>QComboBox</extends>
-   <header>widgets/MUComboBox.h</header>
-  </customwidget>
- </customwidgets>
-</ui>
+// Copyright 2005-2020 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#include "UserInformation.h"
+
+#include "Audio.h"
+#include "CELTCodec.h"
+#include "HostAddress.h"
+#include "ServerHandler.h"
+#include "ViewCert.h"
+
+#include <QtCore/QUrl>
+
+// We define a global macro called 'g'. This can lead to issues when included code uses 'g' as a type or parameter name
+// (like protobuf 3.7 does). As such, for now, we have to make this our last include.
+#include "Global.h"
+
+static QString decode_utf8_qssl_string(const QString &input) {
+	QString i = input;
+	return QUrl::fromPercentEncoding(i.replace(QLatin1String("\\x"), QLatin1String("%")).toLatin1());
+}
+
+static QString decode_utf8_qssl_string(const QStringList &list) {
+	if (list.count() > 0) {
+		return decode_utf8_qssl_string(list.at(0));
+	}
+	return QString();
+}
+
+UserInformation::UserInformation(const MumbleProto::UserStats &msg, QWidget *p) : QDialog(p) {
+	setupUi(this);
+
+	uiSession = msg.session();
+
+	qtTimer = new QTimer(this);
+	connect(qtTimer, SIGNAL(timeout()), this, SLOT(tick()));
+	qtTimer->start(6000);
+
+	qgbConnection->setVisible(false);
+
+	qlOpus->setText(tr("Not Reported"));
+
+	update(msg);
+	resize(sizeHint());
+
+	qfCertificateFont = qlCertificate->font();
+}
+
+unsigned int UserInformation::session() const {
+	return uiSession;
+}
+
+void UserInformation::tick() {
+	if (bRequested)
+		return;
+
+	bRequested = true;
+
+	g.sh->requestUserStats(uiSession, true);
+}
+
+void UserInformation::on_qpbCertificate_clicked() {
+	ViewCert *vc = new ViewCert(qlCerts, this);
+	vc->setWindowModality(Qt::WindowModal);
+	vc->setAttribute(Qt::WA_DeleteOnClose, true);
+	vc->show();
+}
+
+QString UserInformation::secsToString(unsigned int secs) {
+	QStringList qsl;
+
+	int weeks   = secs / (60 * 60 * 24 * 7);
+	secs        = secs % (60 * 60 * 24 * 7);
+	int days    = secs / (60 * 60 * 24);
+	secs        = secs % (60 * 60 * 24);
+	int hours   = secs / (60 * 60);
+	secs        = secs % (60 * 60);
+	int minutes = secs / 60;
+	int seconds = secs % 60;
+
+	if (weeks)
+		qsl << tr("%1w").arg(weeks);
+	if (days)
+		qsl << tr("%1d").arg(days);
+	if (hours)
+		qsl << tr("%1h").arg(hours);
+	if (minutes || hours)
+		qsl << tr("%1m").arg(minutes);
+	qsl << tr("%1s").arg(seconds);
+
+	return qsl.join(QLatin1String(" "));
+}
+
+void UserInformation::update(const MumbleProto::UserStats &msg) {
+	bRequested = false;
+
+	bool showcon = false;
+
+	ClientUser *cu = ClientUser::get(uiSession);
+	if (cu)
+		setWindowTitle(cu->qsName);
+
+	if (msg.certificates_size() > 0) {
+		showcon = true;
+		qlCerts.clear();
+		for (int i = 0; i < msg.certificates_size(); ++i) {
+			const std::string &s = msg.certificates(i);
+			QList< QSslCertificate > certs =
+				QSslCertificate::fromData(QByteArray(s.data(), static_cast< int >(s.length())), QSsl::Der);
+			qlCerts << certs;
+		}
+		if (!qlCerts.isEmpty()) {
+			qpbCertificate->setEnabled(true);
+
+			const QSslCertificate &cert                                      = qlCerts.last();
+			const QMultiMap< QSsl::AlternativeNameEntryType, QString > &alts = cert.subjectAlternativeNames();
+			if (alts.contains(QSsl::EmailEntry))
+				qlCertificate->setText(QStringList(alts.values(QSsl::EmailEntry)).join(tr(", ")));
+			else
+				qlCertificate->setText(decode_utf8_qssl_string(cert.subjectInfo(QSslCertificate::CommonName)));
+
+			if (msg.strong_certificate()) {
+				QFont f = qfCertificateFont;
+				f.setBold(true);
+				qlCertificate->setFont(f);
+			} else {
+				qlCertificate->setFont(qfCertificateFont);
+			}
+		} else {
+			qpbCertificate->setEnabled(false);
+			qlCertificate->setText(QString());
+		}
+	}
+	if (msg.has_address()) {
+		showcon = true;
+		HostAddress ha(msg.address());
+		qlAddress->setText(ha.toString());
+	}
+	if (msg.has_version()) {
+		showcon = true;
+
+		const MumbleProto::Version &mpv = msg.version();
+
+		qlVersion->setText(tr("%1 (%2)").arg(MumbleVersion::toString(mpv.version())).arg(u8(mpv.release())));
+		qlOS->setText(tr("%1 (%2)").arg(u8(mpv.os())).arg(u8(mpv.os_version())));
+	}
+	if (msg.celt_versions_size() > 0) {
+		QStringList qsl;
+		for (int i = 0; i < msg.celt_versions_size(); ++i) {
+			int v         = msg.celt_versions(i);
+			CELTCodec *cc = g.qmCodecs.value(v);
+			if (cc)
+				qsl << cc->version();
+			else
+				qsl << QString::number(v, 16);
+		}
+		qlCELT->setText(qsl.join(tr(", ")));
+	}
+	if (msg.has_opus()) {
+		qlOpus->setText(msg.opus() ? tr("Supported") : tr("Not Supported"));
+	}
+	if (showcon)
+		qgbConnection->setVisible(true);
+
+	qlTCPCount->setText(QString::number(msg.tcp_packets()));
+	qlUDPCount->setText(QString::number(msg.udp_packets()));
+
+	qlTCPAvg->setText(QString::number(msg.tcp_ping_avg(), 'f', 2));
+	qlUDPAvg->setText(QString::number(msg.udp_ping_avg(), 'f', 2));
+
+	qlTCPVar->setText(QString::number(msg.tcp_ping_var() > 0.0f ? sqrtf(msg.tcp_ping_var()) : 0.0f, 'f', 2));
+	qlUDPVar->setText(QString::number(msg.udp_ping_var() > 0.0f ? sqrtf(msg.udp_ping_var()) : 0.0f, 'f', 2));
+
+	if (msg.has_from_client() && msg.has_from_server()) {
+		qgbUDP->setVisible(true);
+		const MumbleProto::UserStats_Stats &from = msg.from_client();
+		qlFromGood->setText(QString::number(from.good()));
+		qlFromLate->setText(QString::number(from.late()));
+		qlFromLost->setText(QString::number(from.lost()));
+		qlFromResync->setText(QString::number(from.resync()));
+
+		const MumbleProto::UserStats_Stats &to = msg.from_server();
+		qlToGood->setText(QString::number(to.good()));
+		qlToLate->setText(QString::number(to.late()));
+		qlToLost->setText(QString::number(to.lost()));
+		qlToResync->setText(QString::number(to.resync()));
+
+		quint32 allFromPackets = from.good() + from.late() + from.lost();
+		qlFromLatePercent->setText(
+			QString::number(allFromPackets > 0 ? from.late() * 100.0 / allFromPackets : 0., 'f', 2));
+		qlFromLostPercent->setText(
+			QString::number(allFromPackets > 0 ? from.lost() * 100.0 / allFromPackets : 0., 'f', 2));
+
+		quint32 allToPackets = to.good() + to.late() + to.lost();
+		qlToLatePercent->setText(QString::number(allToPackets > 0 ? to.late() * 100.0 / allToPackets : 0., 'f', 2));
+		qlToLostPercent->setText(QString::number(allToPackets > 0 ? to.lost() * 100.0 / allToPackets : 0., 'f', 2));
+	} else {
+		qgbUDP->setVisible(false);
+	}
+
+	if (msg.has_onlinesecs()) {
+		if (msg.has_idlesecs())
+			qlTime->setText(
+				tr("%1 online (%2 idle)").arg(secsToString(msg.onlinesecs()), secsToString(msg.idlesecs())));
+		else
+			qlTime->setText(tr("%1 online").arg(secsToString(msg.onlinesecs())));
+	}
+	if (msg.has_bandwidth()) {
+		qlBandwidth->setVisible(true);
+		qliBandwidth->setVisible(true);
+		qlBandwidth->setText(tr("%1 kbit/s").arg(msg.bandwidth() / 125.0, 0, 'f', 1));
+	} else {
+		qlBandwidth->setVisible(false);
+		qliBandwidth->setVisible(false);
+		qlBandwidth->setText(QString());
+	}
+}
