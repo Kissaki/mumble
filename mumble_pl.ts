d8101a0f263b94bbf71a61b807ee74c3bb9ddff2
Jan Klass <kissaki@posteo.de>
2020-12-19 18:45:43 +0100

    New translations AppNap.mm (Polish)

--- mumble_pl.ts
+++ mumble_pl.ts
@@ -3,19 +3,41 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_ABOUT_H_
-#define MUMBLE_MUMBLE_ABOUT_H_
+#include "MumbleApplication.h"
 
-#include <QtCore/QObject>
-#include <QtCore/QtGlobal>
-#include <QtWidgets/QDialog>
+#include <Foundation/Foundation.h>
 
-class AboutDialog : public QDialog {
-private:
-	Q_OBJECT
-	Q_DISABLE_COPY(AboutDialog)
-public:
-	AboutDialog(QWidget *parent);
-};
-
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= 1070
+static bool appNapSuppressed = false;
 #endif
+
+void MUSuppressAppNap(bool suppress) {
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= 1070
+	NSProcessInfo *processInfo = [NSProcessInfo processInfo];
+	if (![processInfo respondsToSelector:@selector(disableAutomaticTermination:)]) {
+		return;
+	}
+
+	if (suppress == appNapSuppressed) {
+		qWarning("AppNap: attempt to set AppNap suppression state to %s while already in that state.", suppress ? "true" : "false");
+		return;
+	}
+
+	QString translatedReason = QApplication::tr("Mumble is currently connected to a server");
+	NSString *reason = const_cast<NSString *>(reinterpret_cast<const NSString *>(CFStringCreateWithCharacters(kCFAllocatorDefault, reinterpret_cast<const UniChar *>(translatedReason.unicode()), translatedReason.length())));
+
+	if (suppress) {
+		[[NSProcessInfo processInfo] disableAutomaticTermination:reason];
+		qWarning("AppNap: suppressed with reason: '%s'", qPrintable(translatedReason));
+	} else {
+		[[NSProcessInfo processInfo] enableAutomaticTermination:reason];
+		qWarning("AppNap: re-enabled, was suppressed with reason: '%s'", qPrintable(translatedReason));
+	}
+
+	appNapSuppressed = suppress;
+
+	[reason release];
+#else
+	Q_UNUSED(suppress);
+#endif
+}
