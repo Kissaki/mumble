198851956825739f596016617e61cbfe4ad4e877
Jan Klass <kissaki@posteo.de>
2020-12-19 18:33:21 +0100

    New translations PTTButtonWidget.cpp (Basque)

--- mumble_eu.ts
+++ mumble_eu.ts
@@ -1,40 +1,31 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>qwPTTButtonWidget</class>
- <widget class="QWidget" name="qwPTTButtonWidget">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>100</width>
-    <height>100</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>Mumble PTT</string>
-  </property>
-  <layout class="QVBoxLayout" name="verticalLayout">
-   <property name="margin">
-    <number>0</number>
-   </property>
-   <item>
-    <widget class="QPushButton" name="qpbPushToTalk">
-     <property name="sizePolicy">
-      <sizepolicy hsizetype="Expanding" vsizetype="Expanding">
-       <horstretch>0</horstretch>
-       <verstretch>0</verstretch>
-      </sizepolicy>
-     </property>
-     <property name="text">
-      <string>Push to talk</string>
-     </property>
-     <property name="default">
-      <bool>true</bool>
-     </property>
-    </widget>
-   </item>
-  </layout>
- </widget>
- <resources/>
- <connections/>
-</ui>
+// Copyright 2005-2020 The Mumble Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file at the root of the
+// Mumble source tree or at <https://www.mumble.info/LICENSE>.
+
+#include "PTTButtonWidget.h"
+
+#include "Global.h"
+
+PTTButtonWidget::PTTButtonWidget(QWidget *p) : QWidget(p) {
+	setupUi(this);
+
+	setWindowFlags(Qt::Tool | Qt::WindowStaysOnTopHint);
+
+	if (!g.s.qbaPTTButtonWindowGeometry.isEmpty()) {
+		restoreGeometry(g.s.qbaPTTButtonWindowGeometry);
+	}
+}
+
+void PTTButtonWidget::closeEvent(QCloseEvent *e) {
+	g.s.qbaPTTButtonWindowGeometry = saveGeometry();
+	QWidget::closeEvent(e);
+}
+
+void PTTButtonWidget::on_qpbPushToTalk_pressed() {
+	emit triggered(true, QVariant());
+}
+
+void PTTButtonWidget::on_qpbPushToTalk_released() {
+	emit triggered(false, QVariant());
+}
