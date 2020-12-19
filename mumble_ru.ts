a05760a24990ceea4331aa862cf57bfc014cc076
Jan Klass <kissaki@posteo.de>
2020-12-19 19:16:15 +0100

    New translations GlobalShortcut.h (Russian)

--- mumble_ru.ts
+++ mumble_ru.ts
@@ -3,237 +3,270 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include "MumbleApplication.h"
+#ifndef MUMBLE_MUMBLE_GLOBALSHORTCUT_H_
+#define MUMBLE_MUMBLE_GLOBALSHORTCUT_H_
 
-#include <QtCore/QStandardPaths>
+#include <QtCore/QThread>
+#include <QtCore/QtGlobal>
+#include <QtWidgets/QStyledItemDelegate>
+#include <QtWidgets/QToolButton>
 
-#ifdef Q_OS_WIN
-#	include <shlobj.h>
-#endif
+#include "ConfigDialog.h"
+#include "MUComboBox.h"
+#include "Timer.h"
 
-// We define a global macro called 'g'. This can lead to issues when included code uses 'g' as a type or parameter name
-// (like protobuf 3.7 does). As such, for now, we have to make this our last include.
-#include "Global.h"
+#include "ui_GlobalShortcut.h"
+#include "ui_GlobalShortcutTarget.h"
 
-Global *Global::g_global_struct;
+class GlobalShortcut : public QObject {
+	friend class GlobalShortcutEngine;
+	friend class GlobalShortcutConfig;
 
-#ifndef Q_OS_WIN
-static void migrateDataDir() {
-#	ifdef Q_OS_MAC
-	QString olddir  = QDir::homePath() + QLatin1String("/Library/Preferences/Mumble");
-	QString newdir  = QStandardPaths::writableLocation(QStandardPaths::DataLocation);
-	QString linksTo = QFile::symLinkTarget(olddir);
-	if (!QFile::exists(newdir) && QFile::exists(olddir) && linksTo.isEmpty()) {
-		QDir d;
-		d.mkpath(newdir + QLatin1String("/.."));
-		if (d.rename(olddir, newdir)) {
-			if (d.cd(QDir::homePath() + QLatin1String("/Library/Preferences"))) {
-				if (QFile::link(d.relativeFilePath(newdir), olddir)) {
-					qWarning("Migrated application data directory from '%s' to '%s'", qPrintable(olddir),
-							 qPrintable(newdir));
-					return;
-				}
-			}
-		}
-	} else {
-		/* Data dir has already been migrated. */
-		return;
-	}
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(GlobalShortcut)
+protected:
+	QList< QVariant > qlActive;
+signals:
+	void down(QVariant);
+	void triggered(bool, QVariant);
 
-	qWarning("Application data migration failed.");
-#	endif // Q_OS_MAC
+public:
+	QString qsToolTip;
+	QString qsWhatsThis;
+	QString name;
+	QVariant qvDefault;
+	int idx;
 
-// Qt4 used another data directory on Unix-like systems, to ensure a seamless
-// transition we must first move the users data to the new directory.
-#	if defined(Q_OS_UNIX) && !defined(Q_OS_MAC)
-	QString olddir =
-		QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation) + QLatin1String("/data/Mumble");
-	QString newdir = QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation) + QLatin1String("/Mumble");
+	GlobalShortcut(QObject *parent, int index, QString qsName, QVariant def = QVariant());
+	~GlobalShortcut() Q_DECL_OVERRIDE;
 
-	if (!QFile::exists(newdir) && QFile::exists(olddir)) {
-		QDir d;
-		if (d.rename(olddir, newdir)) {
-			qWarning("Migrated application data directory from '%s' to '%s'", qPrintable(olddir), qPrintable(newdir));
-			return;
-		}
-	} else {
-		/* Data dir has already been migrated. */
-		return;
-	}
-
-	qWarning("Application data migration failed.");
-#	endif // defined(Q_OS_UNIX) && ! defined(Q_OS_MAC)
-}
-#endif // Q_OS_WIN
-
-Global::Global(const QString &qsConfigPath) {
-	mw              = 0;
-	db              = 0;
-	p               = 0;
-	nam             = 0;
-	c               = 0;
-	talkingUI       = 0;
-	uiSession       = 0;
-	uiDoublePush    = 1000000;
-	iPushToTalk     = 0;
-	iTarget         = 0;
-	iPrevTarget     = 0;
-	bPushToMute     = false;
-	bCenterPosition = false;
-	bPosTest        = false;
-	bInAudioWizard  = false;
-	iAudioPathTime  = 0;
-	iAudioBandwidth = -1;
-	iMaxBandwidth   = -1;
-
-	iCodecAlpha  = 0;
-	iCodecBeta   = 0;
-	bPreferAlpha = true;
-#ifdef USE_OPUS
-	bOpus = true;
-#else
-	bOpus = false;
-#endif
-
-	bAttenuateOthers              = false;
-	prioritySpeakerActiveOverride = false;
-
-	bAllowHTML      = true;
-	uiMessageLength = 5000;
-	uiImageLength   = 131072;
-	uiMaxUsers      = 0;
-
-	qs = nullptr;
-
-	zeroconf = nullptr;
-	lcd      = nullptr;
-	l        = nullptr;
-
-#ifdef USE_OVERLAY
-	ocIntercept = nullptr;
-	o           = nullptr;
-#endif
-
-	bHappyEaster = false;
-
-	bQuit            = false;
-	bDebugDumpInput  = false;
-	bDebugPrintQueue = false;
-
-#if defined(Q_OS_WIN)
-	QString appdata;
-	wchar_t appData[MAX_PATH];
-#endif
-
-	if (!qsConfigPath.isEmpty()) {
-		QFile inifile(qsConfigPath);
-		qs = new QSettings(inifile.fileName(), QSettings::IniFormat);
-	} else {
-		QStringList qsl;
-		qsl << QCoreApplication::instance()->applicationDirPath();
-		qsl << QStandardPaths::writableLocation(QStandardPaths::DataLocation);
-
-#if defined(Q_OS_WIN)
-		if (SUCCEEDED(SHGetFolderPath(nullptr, CSIDL_APPDATA, nullptr, SHGFP_TYPE_CURRENT, appData))) {
-			appdata = QDir::fromNativeSeparators(QString::fromWCharArray(appData));
-
-			if (!appdata.isEmpty()) {
-				appdata.append(QLatin1String("/Mumble"));
-				qsl << appdata;
-			}
-		}
-#endif
-
-		foreach (const QString &dir, qsl) {
-			QFile inifile(QString::fromLatin1("%1/mumble.ini").arg(dir));
-			if (inifile.exists() && inifile.permissions().testFlag(QFile::WriteUser)) {
-				qdBasePath.setPath(dir);
-				qs = new QSettings(inifile.fileName(), QSettings::IniFormat);
-				break;
-			}
-		}
-	}
-
-	if (!qs) {
-		qs = new QSettings();
-#if defined(Q_OS_WIN)
-		if (!appdata.isEmpty())
-			qdBasePath.setPath(appdata);
-#else
-		migrateDataDir();
-		qdBasePath.setPath(QStandardPaths::writableLocation(QStandardPaths::DataLocation));
-#endif
-		if (!qdBasePath.exists()) {
-			QDir::root().mkpath(qdBasePath.absolutePath());
-			if (!qdBasePath.exists())
-				qdBasePath = QDir::home();
-		}
-	}
-
-	if (!qdBasePath.exists(QLatin1String("Plugins")))
-		qdBasePath.mkpath(QLatin1String("Plugins"));
-	if (!qdBasePath.exists(QLatin1String("Overlay")))
-		qdBasePath.mkpath(QLatin1String("Overlay"));
-	if (!qdBasePath.exists(QLatin1String("Themes")))
-		qdBasePath.mkpath(QLatin1String("Themes"));
-
-	qs->setIniCodec("UTF-8");
-}
-
-Global::~Global() {
-	delete qs;
-}
-
-const char Global::ccHappyEaster[] = {
-	072,  057,  0162, 0145, 0143, 056,  0163, 0166, 0147, 0,    0117, 0160, 0145, 0156, 040,  0164, 0150, 0145, 040,
-	0160, 0157, 0144, 040,  0142, 0141, 0171, 040,  0144, 0157, 0157, 0162, 0163, 054,  040,  0110, 0101, 0114, 056,
-	0,    0111, 047,  0155, 040,  0163, 0157, 0162, 0162, 0171, 054,  040,  045,  061,  056,  040,  0111, 047,  0155,
-	040,  0141, 0146, 0162, 0141, 0151, 0144, 040,  0111, 040,  0143, 0141, 0156, 047,  0164, 040,  0144, 0157, 040,
-	0164, 0150, 0141, 0164, 056,  0,    0121, 0127, 0151, 0144, 0147, 0145, 0164, 0173, 0142, 0141, 0143, 0153, 0147,
-	0162, 0157, 0165, 0156, 0144, 055,  0143, 0157, 0154, 0157, 0162, 072,  0142, 0154, 0141, 0143, 0153, 073,  0141,
-	0154, 0164, 0145, 0162, 0156, 0141, 0164, 0145, 055,  0142, 0141, 0143, 0153, 0147, 0162, 0157, 0165, 0156, 0144,
-	055,  0143, 0157, 0154, 0157, 0162, 072,  043,  063,  063,  060,  060,  060,  060,  073,  0143, 0157, 0154, 0157,
-	0162, 072,  0167, 0150, 0151, 0164, 0145, 073,  0175, 0121, 0124, 0145, 0170, 0164, 0102, 0162, 0157, 0167, 0163,
-	0145, 0162, 0173, 0142, 0141, 0143, 0153, 0147, 0162, 0157, 0165, 0156, 0144, 055,  0151, 0155, 0141, 0147, 0145,
-	072,  0165, 0162, 0154, 050,  072,  057,  0162, 0145, 0143, 056,  0163, 0166, 0147, 051,  073,  0142, 0141, 0143,
-	0153, 0147, 0162, 0157, 0165, 0156, 0144, 055,  0160, 0157, 0163, 0151, 0164, 0151, 0157, 0156, 072,  0143, 0145,
-	0156, 0164, 0145, 0162, 0143, 0145, 0156, 0164, 0145, 0162, 073,  0142, 0141, 0143, 0153, 0147, 0162, 0157, 0165,
-	0156, 0144, 055,  0143, 0154, 0151, 0160, 072,  0160, 0141, 0144, 0144, 0151, 0156, 0147, 073,  0142, 0141, 0143,
-	0153, 0147, 0162, 0157, 0165, 0156, 0144, 055,  0157, 0162, 0151, 0147, 0151, 0156, 072,  0160, 0141, 0144, 0144,
-	0151, 0156, 0147, 073,  0142, 0141, 0143, 0153, 0147, 0162, 0157, 0165, 0156, 0144, 055,  0162, 0145, 0160, 0145,
-	0141, 0164, 072,  0156, 0157, 055,  0162, 0145, 0160, 0145, 0141, 0164, 073,  0142, 0141, 0143, 0153, 0147, 0162,
-	0157, 0165, 0156, 0144, 055,  0141, 0164, 0164, 0141, 0143, 0150, 0155, 0145, 0156, 0164, 072,  0146, 0151, 0170,
-	0145, 0144, 073,  0175, 0121, 0115, 0145, 0156, 0165, 0102, 0141, 0162, 072,  072,  0151, 0164, 0145, 0155, 0173,
-	0142, 0141, 0143, 0153, 0147, 0162, 0157, 0165, 0156, 0144, 072,  0164, 0162, 0141, 0156, 0163, 0160, 0141, 0162,
-	0145, 0156, 0164, 073,  0175, 0121, 0110, 0145, 0141, 0144, 0145, 0162, 0126, 0151, 0145, 0167, 072,  072,  0163,
-	0145, 0143, 0164, 0151, 0157, 0156, 0173, 0142, 0141, 0143, 0153, 0147, 0162, 0157, 0165, 0156, 0144, 072,  0164,
-	0162, 0141, 0156, 0163, 0160, 0141, 0162, 0145, 0156, 0164, 073,  0175, 0121, 0124, 0141, 0142, 0102, 0141, 0162,
-	072,  072,  0164, 0141, 0142, 0173, 0142, 0141, 0143, 0153, 0147, 0162, 0157, 0165, 0156, 0144, 072,  0164, 0162,
-	0141, 0156, 0163, 0160, 0141, 0162, 0145, 0156, 0164, 073,  0175, 0
+	bool active() const { return !qlActive.isEmpty(); }
 };
 
-QMultiMap< int, DeferInit * > *DeferInit::qmDeferers = nullptr;
+/**
+ * Widget used to define and key combination for a shortcut. Once it gains
+ * focus it will listen for a button combination until it looses focus.
+ */
+class ShortcutKeyWidget : public QLineEdit {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(ShortcutKeyWidget)
+	Q_PROPERTY(QList< QVariant > shortcut READ getShortcut WRITE setShortcut USER true)
+protected:
+	virtual void focusInEvent(QFocusEvent *event);
+	virtual void focusOutEvent(QFocusEvent *event);
+	virtual void mouseDoubleClickEvent(QMouseEvent *e);
+	virtual bool eventFilter(QObject *, QEvent *);
 
-void DeferInit::add(int priority) {
-	if (!qmDeferers) {
-		qmDeferers = new QMultiMap< int, DeferInit * >();
-	}
-	qmDeferers->insert(priority, this);
-}
+public:
+	QList< QVariant > qlButtons;
+	bool bModified;
+	ShortcutKeyWidget(QWidget *p = nullptr);
+	QList< QVariant > getShortcut() const;
+	void displayKeys(bool last = true);
+public slots:
+	void updateKeys(bool last);
+	void setShortcut(const QList< QVariant > &shortcut);
+signals:
+	void keySet(bool, bool);
+};
 
-DeferInit::~DeferInit() {
-}
+/**
+ * Combo box widget used to define the kind of action a shortcut triggers. Then
+ * entries get auto-generated from the GlobalShortcutEngine::qmShortcuts store.
+ *
+ * @see GlobalShortcutEngine
+ */
+class ShortcutActionWidget : public MUComboBox {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(ShortcutActionWidget)
+	Q_PROPERTY(unsigned int index READ index WRITE setIndex USER true)
+public:
+	ShortcutActionWidget(QWidget *p = nullptr);
+	unsigned int index() const;
+	void setIndex(int);
+};
 
-void DeferInit::run_initializers() {
-	if (!qmDeferers)
-		return;
-	foreach (DeferInit *d, *qmDeferers) { d->initialize(); }
-}
+class ShortcutToggleWidget : public MUComboBox {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(ShortcutToggleWidget)
+	Q_PROPERTY(int index READ index WRITE setIndex USER true)
+public:
+	ShortcutToggleWidget(QWidget *p = nullptr);
+	int index() const;
+	void setIndex(int);
+};
 
-void DeferInit::run_destroyers() {
-	if (!qmDeferers)
-		return;
-	foreach (DeferInit *d, *qmDeferers) { d->destroy(); }
-	delete qmDeferers;
-	qmDeferers = nullptr;
-}
+/**
+ * Dialog which is used to select the targets of a targeted shortcut like Whisper.
+ */
+class ShortcutTargetDialog : public QDialog, public Ui::GlobalShortcutTarget {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(ShortcutTargetDialog)
+
+	enum Target { SELECTION = 0, USERLIST = 1, CHANNEL = 2 };
+
+protected:
+	QMap< QString, QString > qmHashNames;
+	ShortcutTarget stTarget;
+
+public:
+	ShortcutTargetDialog(const ShortcutTarget &, QWidget *p = nullptr);
+	ShortcutTarget target() const;
+public slots:
+	void accept() Q_DECL_OVERRIDE;
+	void on_qcbTarget_currentIndexChanged(int index);
+	void on_qpbAdd_clicked();
+	void on_qpbRemove_clicked();
+};
+
+enum ShortcutTargetTypes {
+	SHORTCUT_TARGET_ROOT              = -1,
+	SHORTCUT_TARGET_PARENT            = -2,
+	SHORTCUT_TARGET_CURRENT           = -3,
+	SHORTCUT_TARGET_SUBCHANNEL        = -4,
+	SHORTCUT_TARGET_PARENT_SUBCHANNEL = -12
+};
+
+/**
+ * Widget used to display and change a ShortcutTarget. The widget displays a textual representation
+ * of a ShortcutTarget and enable its editing with a ShortCutTargetDialog.
+ */
+class ShortcutTargetWidget : public QFrame {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(ShortcutTargetWidget)
+	Q_PROPERTY(ShortcutTarget target READ target WRITE setTarget USER true)
+protected:
+	ShortcutTarget stTarget;
+	QLineEdit *qleTarget;
+	QToolButton *qtbEdit;
+
+public:
+	ShortcutTargetWidget(QWidget *p = nullptr);
+	ShortcutTarget target() const;
+	void setTarget(const ShortcutTarget &);
+	static QString targetString(const ShortcutTarget &);
+public slots:
+	void on_qtbEdit_clicked();
+};
+
+/**
+ * Used to get custom display and edit behaviour for the model used in GlobalShortcutConfig::qtwShortcuts.
+ * It registers custom handlers which link specific types to custom ShortcutXWidget editors and also
+ * provides a basic textual representation for them when they are not edited.
+ *
+ * @see GlobalShortcutConfig
+ * @see ShortcutKeyWidget
+ * @see ShortcutActionWidget
+ * @see ShortcutTargetWidget
+ */
+class ShortcutDelegate : public QStyledItemDelegate {
+	Q_OBJECT
+	Q_DISABLE_COPY(ShortcutDelegate)
+public:
+	ShortcutDelegate(QObject *);
+	~ShortcutDelegate() Q_DECL_OVERRIDE;
+	QString displayText(const QVariant &, const QLocale &) const Q_DECL_OVERRIDE;
+	bool helpEvent(QHelpEvent *, QAbstractItemView *, const QStyleOptionViewItem &,
+				   const QModelIndex &) Q_DECL_OVERRIDE;
+};
+
+/**
+ * Contains the Shortcut tab from the settings. This ConfigWidget provides
+ * the user with the interface to add/edit/delete global shortcuts in Mumble.
+ */
+class GlobalShortcutConfig : public ConfigWidget, public Ui::GlobalShortcut {
+	friend class ShortcutActionWidget;
+
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(GlobalShortcutConfig)
+protected:
+	QList< Shortcut > qlShortcuts;
+	QTreeWidgetItem *itemForShortcut(const Shortcut &) const;
+	bool showWarning() const;
+	bool eventFilter(QObject *, QEvent *) Q_DECL_OVERRIDE;
+
+public:
+	/// The unique name of this ConfigWidget
+	static const QString name;
+	GlobalShortcutConfig(Settings &st);
+	virtual QString title() const Q_DECL_OVERRIDE;
+	virtual const QString &getName() const Q_DECL_OVERRIDE;
+	virtual QIcon icon() const Q_DECL_OVERRIDE;
+public slots:
+	void accept() const Q_DECL_OVERRIDE;
+	void save() const Q_DECL_OVERRIDE;
+	void load(const Settings &r) Q_DECL_OVERRIDE;
+	void reload();
+	void commit();
+	void on_qcbEnableGlobalShortcuts_stateChanged(int);
+	void on_qpbAdd_clicked(bool);
+	void on_qpbRemove_clicked(bool);
+	void on_qtwShortcuts_currentItemChanged(QTreeWidgetItem *, QTreeWidgetItem *);
+	void on_qtwShortcuts_itemChanged(QTreeWidgetItem *, int);
+	void on_qpbOpenAccessibilityPrefs_clicked();
+	void on_qpbSkipWarning_clicked();
+};
+
+struct ShortcutKey {
+	Shortcut s;
+	int iNumUp;
+	GlobalShortcut *gs;
+};
+
+/**
+ * Creates a background thread which handles global shortcut behaviour. This class inherits
+ * a system unspecific interface and basic functionality to the actually used native backend
+ * classes (GlobalShortcutPlatform).
+ *
+ * @see GlobalShortcutX
+ * @see GlobalShortcutMac
+ * @see GlobalShortcutWin
+ */
+class GlobalShortcutEngine : public QThread {
+private:
+	Q_OBJECT
+	Q_DISABLE_COPY(GlobalShortcutEngine)
+public:
+	bool bNeedRemap;
+	Timer tReset;
+
+	static GlobalShortcutEngine *engine;
+	static GlobalShortcutEngine *platformInit();
+
+	QHash< int, GlobalShortcut * > qmShortcuts;
+	QList< QVariant > qlActiveButtons;
+	QList< QVariant > qlDownButtons;
+	QList< QVariant > qlSuppressed;
+
+	QList< QVariant > qlButtonList;
+	QList< QList< ShortcutKey * > > qlShortcutList;
+
+	GlobalShortcutEngine(QObject *p = nullptr);
+	~GlobalShortcutEngine() Q_DECL_OVERRIDE;
+	void resetMap();
+	void remap();
+	virtual void needRemap();
+	void run() Q_DECL_OVERRIDE;
+
+	bool handleButton(const QVariant &, bool);
+	static void add(GlobalShortcut *);
+	static void remove(GlobalShortcut *);
+	virtual QString buttonName(const QVariant &) = 0;
+	static QString buttonText(const QList< QVariant > &);
+	virtual bool canSuppress();
+
+	virtual void setEnabled(bool b);
+	virtual bool enabled();
+	virtual bool canDisable();
+signals:
+	void buttonPressed(bool last);
+};
+
+#endif
