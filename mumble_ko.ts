44e1fa3d9db3f74d4029fde3847d525f56df139e
Jan Klass <kissaki@posteo.de>
2020-12-19 18:52:39 +0100

    New translations LookConfig.h (Korean)

--- mumble_ko.ts
+++ mumble_ko.ts
@@ -3,35 +3,44 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_OSS_H_
-#define MUMBLE_MUMBLE_OSS_H_
+#ifndef MUMBLE_MUMBLE_LOOKCONFIG_H_
+#define MUMBLE_MUMBLE_LOOKCONFIG_H_
 
-#include "AudioInput.h"
-#include "AudioOutput.h"
+#include "ConfigDialog.h"
+#include "ThemeInfo.h"
 
-class OSSInput : public AudioInput {
+#include "ui_LookConfig.h"
+
+class QFileSystemWatcher;
+class QTimer;
+
+class LookConfig : public ConfigWidget, Ui::LookConfig {
 private:
 	Q_OBJECT
-	Q_DISABLE_COPY(OSSInput)
-protected:
-	void release();
-
+	Q_DISABLE_COPY(LookConfig)
 public:
-	OSSInput();
-	~OSSInput() Q_DECL_OVERRIDE;
-	void run() Q_DECL_OVERRIDE;
-};
+	/// The unique name of this ConfigWidget
+	static const QString name;
+	LookConfig(Settings &st);
+	virtual QString title() const Q_DECL_OVERRIDE;
+	virtual const QString &getName() const Q_DECL_OVERRIDE;
+	virtual QIcon icon() const Q_DECL_OVERRIDE;
 
-class OSSOutput : public AudioOutput {
-	friend class OSSUser;
+public slots:
+	void accept() const Q_DECL_OVERRIDE;
+	void save() const Q_DECL_OVERRIDE;
+	void load(const Settings &r) Q_DECL_OVERRIDE;
+	void themeDirectoryChanged();
+	void on_qcbAbbreviateChannelNames_stateChanged(int state);
 
 private:
-	Q_OBJECT
-	Q_DISABLE_COPY(OSSOutput)
-public:
-	OSSOutput();
-	~OSSOutput() Q_DECL_OVERRIDE;
-	void run() Q_DECL_OVERRIDE;
+	/// Reload themes combobox and select given configuredStyle in it
+	void reloadThemes(const boost::optional< ThemeInfo::StyleInfo > configuredStyle);
+
+	/// Timer to prevent change event floods from triggering theme reloads
+	QTimer *m_themeDirectoryDebouncer;
+	/// Watcher to keep theme list up to date with user theme directory
+	QFileSystemWatcher *m_themeDirectoryWatcher;
 };
 
 #endif
