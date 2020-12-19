e8b56d7ba7319a038bb21a0f7f6f08f1ea02ccba
Jan Klass <kissaki@posteo.de>
2020-12-19 18:49:03 +0100

    New translations TalkingUIContainer.cpp (Italian)

--- mumble_it.ts
+++ mumble_it.ts
@@ -1,159 +1,235 @@
-// Copyright 2005-2020 The Mumble Developers. All rights reserved.
+// Copyright 2020 The Mumble Developers. All rights reserved.
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include "LogEmitter.h"
-#include "MainWindow.h"
-#include "Overlay.h"
-#include "Utils.h"
+#include "TalkingUIContainer.h"
+#include "TalkingUIEntry.h"
 
-#include <CoreFoundation/CoreFoundation.h>
+#include <QGroupBox>
+#include <QVBoxLayout>
 
-// We define a global macro called 'g'. This can lead to issues when included code uses 'g' as a type or parameter name (like protobuf 3.7 does). As such, for now, we have to make this our last include.
-#include "Global.h"
+#include <algorithm>
 
-char *os_lang = nullptr;
-static FILE *fConsole = nullptr;
 
-static QSharedPointer<LogEmitter> le;
-
-#define PATH_MAX 1024
-static char crashhandler_fn[PATH_MAX];
-
-static void crashhandler_signals_restore();
-static void crashhandler_handle_crash();
-
-static void mumbleMessageOutputQString(QtMsgType type, const QString &msg) {
-	char c;
-
-	switch (type) {
-		case QtDebugMsg:
-			c='D';
-			break;
-		case QtWarningMsg:
-			c='W';
-			break;
-		case QtFatalMsg:
-			c='F';
-			break;
-		default:
-			c='X';
+struct entry_ptr_less {
+	bool operator()(const std::unique_ptr< TalkingUIEntry > &first, const std::unique_ptr< TalkingUIEntry > &second) {
+		return *first < *second;
 	}
+};
 
-#define LOG(f, msg) fprintf(f, "<%c>%s %s\n", c, \
-		qPrintable(QDateTime::currentDateTime().toString(QLatin1String("yyyy-MM-dd hh:mm:ss.zzz"))), qPrintable(msg))
 
-	QString date = QDateTime::currentDateTime().toString(QLatin1String("yyyy-MM-dd hh:mm:ss.zzz"));
-	QString fmsg = QString::fromLatin1("<%1>%2 %3").arg(c).arg(date).arg(msg);
-	fprintf(stderr, "%s\n", qPrintable(fmsg));
-	fprintf(fConsole, "%s\n", qPrintable(fmsg));
-	fflush(fConsole);
 
-	le->addLogEntry(fmsg);
-
-	if (type == QtFatalMsg) {
-		CFStringRef csMsg = CFStringCreateWithFormat(kCFAllocatorDefault, nullptr, CFSTR("%s\n\nThe error has been logged. Please submit your log file to the Mumble project if the problem persists."), qPrintable(msg));
-		CFUserNotificationDisplayAlert(0, 0, nullptr,  nullptr, nullptr, CFSTR("Mumble has encountered a fatal error"), csMsg, CFSTR("OK"), nullptr, nullptr, nullptr);
-		CFRelease(csMsg);
-		exit(0);
-	}
+TalkingUIContainer::TalkingUIContainer(int associatedChannelID, TalkingUI &talkingUI)
+	: m_associatedChannelID(associatedChannelID), m_talkingUI(talkingUI) {
 }
 
-static void mumbleMessageOutputWithContext(QtMsgType type, const QMessageLogContext &ctx, const QString &msg) {
-	Q_UNUSED(ctx);
-	mumbleMessageOutputQString(type, msg);
-}
 
-void query_language() {
-	CFPropertyListRef cfaLangs;
-	CFStringRef cfsLang;
-	static char lang[16];
-
-	cfaLangs = CFPreferencesCopyAppValue(CFSTR("AppleLanguages"), kCFPreferencesCurrentApplication);
-	cfsLang = (CFStringRef) CFArrayGetValueAtIndex((CFArrayRef)cfaLangs, 0);
-
-	if (! CFStringGetCString(cfsLang, lang, 16, kCFStringEncodingUTF8))
-		return;
-
-	os_lang = lang;
-}
-
-static void crashhandler_signal_handler(int signal) {
-	switch (signal) {
-		case SIGQUIT:
-		case SIGILL:
-		case SIGTRAP:
-		case SIGABRT:
-		case SIGEMT:
-		case SIGFPE:
-		case SIGBUS:
-		case SIGSEGV:
-		case SIGSYS:
-			crashhandler_signals_restore();
-			crashhandler_handle_crash();
-			break;
-		default:
-			break;
-	}
-}
-
-/* These are the signals that according to signal(3) produce a coredump by default. */
-int sigs[] = { SIGQUIT, SIGILL, SIGTRAP, SIGABRT, SIGEMT, SIGFPE, SIGBUS, SIGSEGV, SIGSYS };
-#define NSIGS sizeof(sigs)/sizeof(sigs[0])
-
-static void crashhandler_signals_setup() {
-	for (size_t i = 0; i < NSIGS; i++) {
-		signal(sigs[i], crashhandler_signal_handler);
-	}
-}
-
-static void crashhandler_signals_restore() {
-	for (size_t i = 0; i < NSIGS; i++) {
-		signal(sigs[i], nullptr);
-	}
-}
-
-static void crashhandler_init() {
-	QString dump = g.qdBasePath.filePath(QLatin1String("mumble.dmp"));
-	if (strncpy(crashhandler_fn, dump.toUtf8().data(), PATH_MAX)) {
-		crashhandler_signals_setup();
-		/* todo: Change the behavior of the Apple crash dialog? Maybe? */
-	}
-}
-
-static void crashhandler_handle_crash() {
-	/* Abuse mtime for figuring out which crashdump we should send. */
-	FILE *f = fopen(crashhandler_fn, "w");
-	fflush(f);
-	fclose(f);
-}
-
-void os_init() {
-	crashhandler_init();
-
-	const char *home = getenv("HOME");
-	const char *logpath = "/Library/Logs/Mumble.log";
-
-	// Make a copy of the global LogEmitter, such that
-	// os_macx.mm doesn't have to consider the deletion
-	// of the Global object and its LogEmitter object.
-	le = g.le;
-
-	if (home) {
-		size_t len = strlen(home) + strlen(logpath) + 1;
-		STACKVAR(char, buff, len);
-		memset(buff, 0, len);
-		strcat(buff, home);
-		strcat(buff, logpath);
-		fConsole = fopen(buff, "a+");
-		if (fConsole) {
-			qInstallMessageHandler(mumbleMessageOutputWithContext);
+int TalkingUIContainer::find(unsigned int associatedUserSession, EntryType type) const {
+	for (std::size_t i = 0; i < m_entries.size(); i++) {
+		const std::unique_ptr< TalkingUIEntry > &currentEntry = m_entries[i];
+		if (currentEntry->getType() == type && currentEntry->getAssociatedUserSession() == associatedUserSession) {
+			return static_cast< int >(i);
 		}
 	}
 
-	/* Query for language setting. OS X's LANG environment variable is determined from the region selected
-	 * in SystemPrefs -> International -> Formats -> Region instead of the system language. We override this
-	 * by always using the system langauge, to get rid of all sorts of nasty langauge inconsistencies. */
-	query_language();
+	return -1;
+}
+
+int TalkingUIContainer::getAssociatedChannelID() const {
+	return m_associatedChannelID;
+}
+
+void TalkingUIContainer::addEntry(std::unique_ptr< TalkingUIEntry > entry) {
+	// set container
+	entry->m_container = this;
+
+	m_entries.push_back(std::move(entry));
+}
+
+std::unique_ptr< TalkingUIEntry > TalkingUIContainer::removeEntry(const TalkingUIEntry *entry) {
+	return removeEntry(entry->getAssociatedUserSession(), entry->getType());
+}
+
+std::unique_ptr< TalkingUIEntry > TalkingUIContainer::removeEntry(unsigned int associatedUserSession, EntryType type) {
+	int index = find(associatedUserSession, type);
+
+	std::unique_ptr< TalkingUIEntry > entry(nullptr);
+
+	if (index >= 0) {
+		// Move the entry out of the vector
+		entry = std::move(m_entries[index]);
+		m_entries.erase(m_entries.begin() + index);
+
+		// reset container
+		entry->m_container = nullptr;
+	}
+
+	return entry;
+}
+
+std::vector< std::unique_ptr< TalkingUIEntry > > &TalkingUIContainer::getEntries() {
+	return m_entries;
+}
+
+const std::vector< std::unique_ptr< TalkingUIEntry > > &TalkingUIContainer::getEntries() const {
+	return m_entries;
+}
+
+bool TalkingUIContainer::contains(unsigned int associatedUserSession, EntryType type) const {
+	return find(associatedUserSession, type) >= 0;
+}
+
+std::size_t TalkingUIContainer::size() const {
+	return m_entries.size();
+}
+
+bool TalkingUIContainer::isEmpty() const {
+	return size() == 0;
+}
+
+void TalkingUIContainer::setPermanent(bool permanent) {
+	m_permanent = permanent;
+}
+
+bool TalkingUIContainer::isPermanent() const {
+	return m_permanent;
+}
+
+TalkingUIEntry *TalkingUIContainer::get(unsigned int associatedUserSession, EntryType type) {
+	int index = find(associatedUserSession, type);
+
+	if (index >= 0) {
+		return m_entries[index].get();
+	} else {
+		return nullptr;
+	}
+}
+
+bool TalkingUIContainer::operator==(const TalkingUIContainer &other) const {
+	return compare(other) == 0;
+}
+
+bool TalkingUIContainer::operator!=(const TalkingUIContainer &other) const {
+	return compare(other) != 0;
+}
+
+bool TalkingUIContainer::operator>(const TalkingUIContainer &other) const {
+	return compare(other) > 0;
+}
+
+bool TalkingUIContainer::operator>=(const TalkingUIContainer &other) const {
+	return compare(other) >= 0;
+}
+
+bool TalkingUIContainer::operator<(const TalkingUIContainer &other) const {
+	return compare(other) < 0;
+}
+
+bool TalkingUIContainer::operator<=(const TalkingUIContainer &other) const {
+	return compare(other) <= 0;
+}
+
+
+
+const int CHANNEL_LAYOUT_TOP_MARGIN = 10;
+
+TalkingUIChannel::TalkingUIChannel(int associatedChannelID, QString name, TalkingUI &talkingUI)
+	: TalkingUIContainer(associatedChannelID, talkingUI), m_channelBox(new QGroupBox()) {
+	// Set name
+	m_channelBox->setTitle(name);
+
+	// Set layout
+	QVBoxLayout *layout = new QVBoxLayout();
+	layout->setContentsMargins(0, CHANNEL_LAYOUT_TOP_MARGIN, 0, 0);
+	m_channelBox->setLayout(layout);
+}
+
+TalkingUIChannel::~TalkingUIChannel() {
+	m_channelBox->deleteLater();
+}
+
+void TalkingUIChannel::updatePriority() {
+	// First reset the priority to the lowest possible value as the loop below
+	// will only update it to be higher than the current priority
+	m_highestUserPriority = EntryPriority::LOWEST;
+
+	for (auto &currentEntry : m_entries) {
+		if (currentEntry->getPriority() > m_highestUserPriority) {
+			m_highestUserPriority = currentEntry->getPriority();
+		}
+	}
+}
+
+QString TalkingUIChannel::getName() const {
+	return m_channelBox->title();
+}
+
+void TalkingUIChannel::setName(const QString &name) {
+	m_channelBox->setTitle(name);
+}
+
+int TalkingUIChannel::compare(const TalkingUIContainer &other) const {
+	if (getType() != other.getType()) {
+		return static_cast< int >(other.getType()) - static_cast< int >(getType());
+	}
+
+	const TalkingUIChannel &otherChannel = static_cast< const TalkingUIChannel & >(other);
+
+	if (m_highestUserPriority != otherChannel.m_highestUserPriority) {
+		return m_highestUserPriority < otherChannel.m_highestUserPriority ? 1 : -1;
+	}
+
+	int res = getName().localeAwareCompare(otherChannel.getName());
+
+	if (res == 0) {
+		// Only consider channels to be the same if they have the same associated ID
+		res = otherChannel.getAssociatedChannelID() - getAssociatedChannelID();
+	}
+
+	return res;
+}
+
+QWidget *TalkingUIChannel::getWidget() {
+	return m_channelBox;
+}
+
+const QWidget *TalkingUIChannel::getWidget() const {
+	return m_channelBox;
+}
+
+ContainerType TalkingUIChannel::getType() const {
+	return ContainerType::CHANNEL;
+}
+
+void TalkingUIChannel::addEntry(std::unique_ptr< TalkingUIEntry > entry) {
+	if (entry->getPriority() > m_highestUserPriority) {
+		m_highestUserPriority = entry->getPriority();
+	}
+	TalkingUIContainer::addEntry(std::move(entry));
+
+	// Remove all entries from the layout
+	for (const auto &currentEntry : m_entries) {
+		m_channelBox->layout()->removeWidget(currentEntry->getWidget());
+	}
+
+	// Sort entries
+	std::sort(m_entries.begin(), m_entries.end(), entry_ptr_less());
+
+	// Add all entries again (including the new one) in order
+	for (auto &currentEntry : m_entries) {
+		m_channelBox->layout()->addWidget(currentEntry->getWidget());
+	}
+}
+
+std::unique_ptr< TalkingUIEntry > TalkingUIChannel::removeEntry(unsigned int associatedUserSession, EntryType type) {
+	std::unique_ptr< TalkingUIEntry > entry = TalkingUIContainer::removeEntry(associatedUserSession, type);
+
+	if (entry) {
+		m_channelBox->layout()->removeWidget(entry->getWidget());
+	}
+
+	updatePriority();
+
+	return entry;
 }
