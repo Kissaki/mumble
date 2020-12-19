df122e327e3fff8d1ed9e9cb77992f18d3970980
Jan Klass <kissaki@posteo.de>
2020-12-19 19:11:11 +0100

    New translations SvgIcon.h (Polish)

--- mumble_pl.ts
+++ mumble_pl.ts
@@ -3,37 +3,23 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#include "SvgIcon.h"
+#ifndef MUMBLE_MUMBLE_SVGICON_H
+#define MUMBLE_MUMBLE_SVGICON_H
 
-#include <QPainter>
-#include <QSvgRenderer>
+#include <QIcon>
+#include <QString>
 
-void SvgIcon::addSvgPixmapsToIcon(QIcon &icon, QString fn) {
-	QSvgRenderer svg(fn);
+class SvgIcon {
+public:
+	/// addSvgPixmapsToIcon renders the SVG file at |fn| in various
+	/// common sizes from 8x8 up to 256x256 and adds the resulting
+	/// rasterized pixmaps to |icon|.
+	///
+	/// This method is only needed due to an odd interaction between
+	/// Qt and Plasma/KStatusNotifierItem. See
+	/// https://github.com/mumble-voip/mumble/issues/3374
+	/// for more information.
+	static void addSvgPixmapsToIcon(QIcon &icon, QString fn);
+};
 
-	QList< QSize > commonSizes;
-	commonSizes << QSize(8, 8);
-	commonSizes << QSize(16, 16);
-	commonSizes << QSize(22, 22); // Plasma notification area size
-	commonSizes << QSize(24, 24);
-	commonSizes << QSize(32, 32);
-	commonSizes << QSize(44, 44); // Plasma notification area size @x2
-	commonSizes << QSize(48, 48);
-	commonSizes << QSize(64, 64);
-	commonSizes << QSize(96, 96);
-	commonSizes << QSize(128, 128);
-	commonSizes << QSize(256, 256);
-
-	foreach (QSize size, commonSizes) {
-		QPixmap pm(size);
-		pm.fill(Qt::transparent);
-
-		QPainter p(&pm);
-		p.setRenderHint(QPainter::Antialiasing);
-		p.setRenderHint(QPainter::TextAntialiasing);
-		p.setRenderHint(QPainter::SmoothPixmapTransform);
-		svg.render(&p);
-
-		icon.addPixmap(pm);
-	}
-}
+#endif
