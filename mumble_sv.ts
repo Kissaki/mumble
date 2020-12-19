7aa29599204fc65df6c171f694d91cbdcbbaaa3b
Jan Klass <kissaki@posteo.de>
2020-12-19 19:00:53 +0100

    New translations TalkingUIComponent.h (Swedish)

--- mumble_sv.ts
+++ mumble_sv.ts
@@ -1,16 +1,17 @@
-// Copyright 2005-2020 The Mumble Developers. All rights reserved.
+// Copyright 2020 The Mumble Developers. All rights reserved.
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_TASK_LIST_H_
-#define MUMBLE_MUMBLE_TASK_LIST_H_
+#ifndef MUMBLE_MUMBLE_TALKINGUICOMPONENT_H_
+#define MUMBLE_MUMBLE_TALKINGUICOMPONENT_H_
 
-class QString;
+class QWidget;
 
-class TaskList {
+class TalkingUIComponent {
 public:
-	static void addToRecentList(QString name, QString user, QString host, int port);
+	virtual QWidget *getWidget()             = 0;
+	virtual const QWidget *getWidget() const = 0;
 };
 
-#endif // TASK_LIST_H_
+#endif // MUMBLE_MUMBLE_TALKINGUICOMPONENT_H_
