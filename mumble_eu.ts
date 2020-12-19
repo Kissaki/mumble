960f508a67378c61d6b1aa3a1150255121b417bd
Jan Klass <kissaki@posteo.de>
2020-12-19 18:35:38 +0100

    New translations ViewCert.h (Basque)

--- mumble_eu.ts
+++ mumble_eu.ts
@@ -3,36 +3,30 @@
 // that can be found in the LICENSE file at the root of the
 // Mumble source tree or at <https://www.mumble.info/LICENSE>.
 
-#ifndef MUMBLE_MUMBLE_XMLTOOLS_H_
-#define MUMBLE_MUMBLE_XMLTOOLS_H_
+#ifndef MUMBLE_MUMBLE_VIEWCERT_H_
+#define MUMBLE_MUMBLE_VIEWCERT_H_
 
-#include <QMap>
-#include <QObject>
-#include <QXmlStreamReader>
-#include <QXmlStreamWriter>
+#include <QtCore/QList>
+#include <QtCore/QObject>
+#include <QtCore/QtGlobal>
+#include <QtNetwork/QSslCertificate>
+#include <QtWidgets/QDialog>
 
-class XMLTools : public QObject {
+class QListWidget;
+
+class ViewCert : public QDialog {
+private:
 	Q_OBJECT
-public:
-	/* Recursively parse and output XHTML.
-	 * This will drop <head>, <html> etc, take the contents of <body> and strip out unnecesarry styles.
-	 * It will also change <span style=""> into matching <b>, <i> or <u>.
-	 */
-	static void recurseParse(QXmlStreamReader &reader, QXmlStreamWriter &writer, int &paragraphs,
-							 const QMap< QString, QString > &opstyle, const int close = 0, bool ignore = true);
+	Q_DISABLE_COPY(ViewCert)
+protected:
+	QList< QSslCertificate > qlCerts;
+	QListWidget *qlwChain, *qlwCert;
+protected slots:
+	void on_Chain_currentRowChanged(int);
 
-	/* Iterate XML and remove close-followed-by-open.
-	 * For example, make "<b>bold with </b><b><i>italic</i></b>" into
-	 * "<b>bold with <i>italic</i></b>"
-	 *
-	 * If the input XML is or may not be valid, a "unduplicate" tag can be used as a root element,
-	 * which will be dropped and not written to writer.
-	 *
-	 * Input XML has to be valid XML.
-	 *
-	 * Works on b, i, u, and a elements.
-	 */
-	static bool unduplicateTags(QXmlStreamReader &reader, QXmlStreamWriter &writer);
+public:
+	ViewCert(QList< QSslCertificate > c, QWidget *p);
+	static QString prettifyDigest(QString);
 };
 
-#endif // XMLTOOLS_H
+#endif
