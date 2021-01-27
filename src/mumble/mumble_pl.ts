132f599b54044e158c1164f29e7100366c6ae5ba
Jan Klass <kissaki@posteo.de>
2021-01-27 22:52:58 +0100

    New translations mumble_en.ts (Polish)

--- src/mumble/mumble_pl.ts
+++ src/mumble/mumble_pl.ts
@@ -1131,6 +1131,14 @@ maksymalna liczba użytkowników wynosi tyle, co domyślny limit serwera.</trans
       <source>Idle action</source>
       <translation>Akcja bezczynności</translation>
     </message>
+    <message>
+      <source>Access to the microphone was denied. Please allow Mumble to use the microphone by changing the settings in System Preferences -&gt; Security &amp; Privacy -&gt; Privacy -&gt; Microphone.</source>
+      <translation type="unfinished">Access to the microphone was denied. Please allow Mumble to use the microphone by changing the settings in System Preferences -&gt; Security &amp; Privacy -&gt; Privacy -&gt; Microphone.</translation>
+    </message>
+    <message>
+      <source>Access to the microphone was denied. Please check that your operating system&apos;s microphone settings allow Mumble to use the microphone.</source>
+      <translation type="unfinished">Access to the microphone was denied. Please check that your operating system&apos;s microphone settings allow Mumble to use the microphone.</translation>
+    </message>
   </context>
   <context>
     <name>AudioOutput</name>
@@ -3163,13 +3171,6 @@ Label of the server. This is what the server will be named like in your server l
     <message>
       <source>&amp;Ignore</source>
       <translation>&amp;Ignoruj</translation>
-    </message>
-  </context>
-  <context>
-    <name>CoreAudioSystem</name>
-    <message>
-      <source>Default Device</source>
-      <translation>Urządzenie domyślne</translation>
     </message>
   </context>
   <context>
@@ -7998,6 +7999,22 @@ An access token is a text string, which can be used as a password for very simpl
     <message>
       <source>Cannot perform this movement automatically, please reset the numeric sorting indicators or adjust it manually.</source>
       <translation>Nie można przenieść automatycznie, prosimy o posortowanie numeryczne wskaźników lub ręczne ustawienie.</translation>
+    </message>
+    <message>
+      <source>Talking while being muted on your end</source>
+      <translation type="unfinished">Talking while being muted on your end</translation>
+    </message>
+    <message>
+      <source>This is a channel listener. The corresponding user hears everything you say in this channel.</source>
+      <translation type="unfinished">This is a channel listener. The corresponding user hears everything you say in this channel.</translation>
+    </message>
+    <message>
+      <source>Channel has access restrictions so that you can&apos;t enter it</source>
+      <translation type="unfinished">Channel has access restrictions so that you can&apos;t enter it</translation>
+    </message>
+    <message>
+      <source>Channel has access restrictions but you can enter nonetheless</source>
+      <translation type="unfinished">Channel has access restrictions but you can enter nonetheless</translation>
     </message>
   </context>
   <context>
